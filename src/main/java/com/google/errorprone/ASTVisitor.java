2dd6ec4ebb1b6e20635909cd80e4bb41981192e8
Alex Eagle <alexeagle@google.com>
2011-10-03 14:28:29 -0700

    Make ASTVisitor public so we can enter from there.

--- src/main/java/com/google/errorprone/ASTVisitor.java
+++ src/main/java/com/google/errorprone/ASTVisitor.java
@@ -16,26 +16,70 @@
 
 package com.google.errorprone;
 
-import com.google.errorprone.matchers.ErrorProducingMatcher.AstError;
-import com.sun.source.tree.*;
-import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
-import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.JCDiagnostic.DiagnosticPosition;
-import com.sun.tools.javac.util.Log;
+import static java.util.Arrays.asList;
+
 import com.google.errorprone.matchers.ErrorProducingMatcher;
+import com.google.errorprone.matchers.ErrorProducingMatcher.AstError;
 import com.google.errorprone.matchers.PreconditionsCheckNotNullMatcher;
 
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.tools.Diagnostic.Kind;
-import javax.tools.JavaFileObject;
-
-import static java.util.Arrays.asList;
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ArrayAccessTree;
+import com.sun.source.tree.ArrayTypeTree;
+import com.sun.source.tree.AssertTree;
+import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.BreakTree;
+import com.sun.source.tree.CaseTree;
+import com.sun.source.tree.CatchTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.ConditionalExpressionTree;
+import com.sun.source.tree.ContinueTree;
+import com.sun.source.tree.DoWhileLoopTree;
+import com.sun.source.tree.EmptyStatementTree;
+import com.sun.source.tree.EnhancedForLoopTree;
+import com.sun.source.tree.ErroneousTree;
+import com.sun.source.tree.ExpressionStatementTree;
+import com.sun.source.tree.ForLoopTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.IfTree;
+import com.sun.source.tree.ImportTree;
+import com.sun.source.tree.InstanceOfTree;
+import com.sun.source.tree.LabeledStatementTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.NewArrayTree;
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.ParameterizedTypeTree;
+import com.sun.source.tree.ParenthesizedTree;
+import com.sun.source.tree.PrimitiveTypeTree;
+import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.SwitchTree;
+import com.sun.source.tree.SynchronizedTree;
+import com.sun.source.tree.ThrowTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TreeVisitor;
+import com.sun.source.tree.TryTree;
+import com.sun.source.tree.TypeCastTree;
+import com.sun.source.tree.TypeParameterTree;
+import com.sun.source.tree.UnaryTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.tree.WhileLoopTree;
+import com.sun.source.tree.WildcardTree;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Log;
 
 /**
  * Visitor, following the visitor pattern, which may visit each node in the parsed AST.
  * @author Alex Eagle (alexeagle@google.com)
  */
-class ASTVisitor implements TreeVisitor<Void, VisitorState> {
+public class ASTVisitor implements TreeVisitor<Void, VisitorState> {
 
   //TODO: proper logging
   private static final Boolean DEBUG = false;
