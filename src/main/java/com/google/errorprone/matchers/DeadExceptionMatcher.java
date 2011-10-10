40ffed04ee759105c6fe0ce287b6ba4ef9420e8d
Alex Eagle <alexeagle@google.com>
2011-10-09 21:42:56 -0700

    First stab at a matcher for an exception which is created, is not thrown, and the reference is lost.

---
+++ src/main/java/com/google/errorprone/matchers/DeadExceptionMatcher.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.matchers;
+
+import com.google.errorprone.SuggestedFix;
+import com.google.errorprone.VisitorState;
+import com.sun.source.tree.NewClassTree;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCNewClass;
+import com.sun.tools.javac.util.JCDiagnostic.DiagnosticPosition;
+
+/**
+ * @author alexeagle@google.com (Alex Eagle)
+ */
+public class DeadExceptionMatcher extends ErrorProducingMatcher<NewClassTree> {
+  @Override
+  public AstError matchWithError(NewClassTree newClassTree, VisitorState state) {
+    if (state.types.isSubtype(((JCNewClass) newClassTree).type, state.symtab.exceptionType)) {
+      if (newClassTree.getEnclosingExpression() == null) {
+        DiagnosticPosition pos = ((JCTree) newClassTree).pos();
+        System.out.printf("suggestedReplace from %d to %d: \"%s\"\n",
+          pos.getStartPosition(), pos.getStartPosition(), "throw ");
+        return new AstError("Exception created but not thrown, and reference is lost", new SuggestedFix(), newClassTree);
+      }
+    }
+    return null;
+  }
+}
