c97304ffa3a826cf71122390ac42e2259d653d7c
Alex Eagle <alexeagle@google.com>
2012-05-20 15:39:27 -0700

    Create the maven compiler plugin. The compiler isn't yet found when you try to use this plugin.

---
+++ maven/src/main/java/com/google/errorprone/ErrorProneCompiler.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone;
+
+import org.codehaus.plexus.compiler.CompilerConfiguration;
+import org.codehaus.plexus.compiler.CompilerException;
+import org.codehaus.plexus.compiler.javac.JavacCompiler;
+
+import java.util.List;
+
+/**
+ * @plexus.component
+ *   role="org.codehaus.plexus.compiler.Compiler"
+ *   role-hint="error-prone"
+ *
+ * @author alexeagle@google.com (Alex Eagle)
+ */
+public class ErrorProneCompiler extends JavacCompiler {
+  @Override
+  public List compile(CompilerConfiguration config) throws CompilerException {
+    throw new UnsupportedOperationException("Uh oh!");
+  }
+}
