93f30598969d72f6c051eb5d4f77613a7ecee3ff
Eddie Aftandilian <eaftan@google.com>
2013-03-05 13:44:32 -0800

    Documentation update for PreconditionsCheckNotNullPrimitive

--- core/src/main/java/com/google/errorprone/bugpatterns/PreconditionsCheckNotNullPrimitive.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/PreconditionsCheckNotNullPrimitive.java
@@ -60,7 +60,8 @@ import com.sun.tools.javac.tree.JCTree.JCExpression;
     explanation =
         "Preconditions.checkNotNull() takes as an argument a reference that should be " +
         "non-null. Often a primitive is passed as the argument to check. The primitive " +
-        "will be autoboxed into a boxed object, which is non-null, causing the check to " +
+        "will be [http://docs.oracle.com/javase/7/docs/technotes/guides/language/autoboxing.html " +
+        "autoboxed] into a boxed object, which is non-null, causing the check to " +
         "always pass without the condition being evaluated.\n" +
         "If the intent was to ensure that the primitive met some criterion (e.g., a boolean " +
         "that should be non-null), please use Precondtions.checkState() or " +
