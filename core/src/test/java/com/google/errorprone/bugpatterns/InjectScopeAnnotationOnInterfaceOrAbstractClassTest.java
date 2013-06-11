878e653a9b8293805d82ddf9ed821a5278a3a00c
Steven Goldfeder <sgoldfed@gmail.com>
2013-06-11 10:42:50 -0700

    bugpattern for scoping annotations on interfaces or abstract classes

---
+++ core/src/test/java/com/google/errorprone/bugpatterns/InjectScopeAnnotationOnInterfaceOrAbstractClassTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * @author sgoldfeder@google.com (Steven Goldfeder)
+ */
+public class InjectScopeAnnotationOnInterfaceOrAbstractClassTest {
+
+  private CompilationTestHelper compilationHelper;
+
+  @Before
+  public void setUp() {
+    compilationHelper =
+        new CompilationTestHelper(new InjectScopeAnnotationOnInterfaceOrAbstractClass.Scanner());
+  }
+
+  @Test
+  public void testPositiveCase() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(new File(this.getClass()
+        .getResource("InjectScopeAnnotationOnInterfaceOrAbstractClassPositiveCases.java")
+        .toURI()));
+  }
+
+  @Test
+  public void testNegativeCase() throws Exception {
+    compilationHelper.assertCompileSucceeds(new File(this.getClass()
+        .getResource("InjectScopeAnnotationOnInterfaceOrAbstractClassNegativeCases.java")
+        .toURI()));
+  }
+}
