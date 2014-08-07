6312a2273468b7cdc831a088634c7b1aa4371cbc
glorioso <glorioso@google.com>
2014-08-06 17:31:12 -0700

    Create error-prone matchers for finding setUp() and tearDown() methods that look like JUnit3 overridden methods, but are actually part of a JUnit4 test class.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=72772812

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/JUnit4TearDownNotRun.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.Category.JUNIT;
+import static com.google.errorprone.BugPattern.MaturityLevel.EXPERIMENTAL;
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.JUnitMatchers.JUNIT_AFTER_ANNOTATION;
+import static com.google.errorprone.matchers.JUnitMatchers.JUNIT_AFTER_CLASS_ANNOTATION;
+import static com.google.errorprone.matchers.JUnitMatchers.JUNIT_BEFORE_ANNOTATION;
+import static com.google.errorprone.matchers.JUnitMatchers.JUNIT_BEFORE_CLASS_ANNOTATION;
+import static com.google.errorprone.matchers.JUnitMatchers.hasJUnit4AfterAnnotations;
+import static com.google.errorprone.matchers.JUnitMatchers.looksLikeJUnit3TearDown;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.not;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.matchers.Matcher;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Checks for the existence of a JUnit3 style tearDown() method in a JUnit4 test class.
+ *
+ * @author glorioso@google.com (Nick Glorioso)
+ */
+@BugPattern(name = "JUnit4TearDownNotRun",
+    summary = "tearDown() method will not be run; Please add an @After annotation",
+    explanation = "JUnit 3 provides the overridable method tearDown(), to be overridden by "
+        + " subclasses when the test needs to perform some post-test de-initialization. "
+        + " In JUnit 4, this is accomplished by annotating such a method with @After."
+        + " The method that triggered this error matches the definition of tearDown() from"
+        + " JUnit3, but was not annotated with @After and thus won't be run by the JUnit4"
+        + " runner.\n\n"
+        + " If you intend for this tearDown() method not to be run by the JUnit4 runner,"
+        + " but perhaps be manually invoked after certain test methods, please rename the method "
+        + " or mark it private.\n\n"
+        + " If the method is part of an abstract test class hierarchy"
+        + " where this class's tearDown() is invoked by a superclass method that is annotated with"
+        + " @After, then please rename the abstract method or add @After to"
+        + " the superclass's definition of tearDown()",
+    category = JUNIT, maturity = EXPERIMENTAL, severity = ERROR)
+public class JUnit4TearDownNotRun extends AbstractJUnit4InitMethodNotRun {
+  @Override
+  protected Matcher methodMatcher() {
+    return allOf(looksLikeJUnit3TearDown, not(hasJUnit4AfterAnnotations));
+  }
+
+  @Override
+  protected String correctAnnotation() {
+    return JUNIT_AFTER_ANNOTATION;
+  }
+
+  @Override
+  protected List<AnnotationReplacements> annotationReplacements() {
+    return Arrays.asList(
+        new AnnotationReplacements(JUNIT_BEFORE_ANNOTATION, JUNIT_AFTER_ANNOTATION),
+        new AnnotationReplacements(JUNIT_BEFORE_CLASS_ANNOTATION, JUNIT_AFTER_CLASS_ANNOTATION));
+  }
+}
---
+++ core/src/test/resources/com/google/errorprone/bugpatterns/JUnit4SetUpNotRunPositiveCases.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Basic class with an untagged setUp method */
+@RunWith(JUnit4.class)
+public class JUnit4SetUpNotRunPositiveCases {
+  // BUG: Diagnostic contains: @Before
+  public void setUp() {}
+}
+
+@RunWith(JUnit4.class)
+class J4PositiveCase2 {
+  // BUG: Diagnostic contains: @Before
+  protected void setUp() {}
+}
+
+/**
+ * Replace @After with @Before
+ */
+@RunWith(JUnit4.class)
+class J4AfterToBefore {
+  // BUG: Diagnostic contains: @Before
+  @After
+  protected void setUp() {}
+}
+
+/**
+ * Replace @AfterClass with @BeforeClass
+ */
+@RunWith(JUnit4.class)
+class J4AfterClassToBeforeClass {
+  // BUG: Diagnostic contains: @BeforeClass
+  @AfterClass
+  protected void setUp() {}
+}
+
+class BaseTestClass {
+  void setUp() {}
+}
+
+/**
+ * This is the ambiguous case that we want the developer to make the determination as to
+ * whether to rename setUp()
+ */
+@RunWith(JUnit4.class)
+class J4Inherit extends BaseTestClass {
+  // BUG: Diagnostic contains: @Before
+  protected void setUp() {}
+}
+
+/**
+ * setUp() method overrides parent method with @Override, but that method isn't @Before in the
+ * superclass
+ */
+@RunWith(JUnit4.class)
+class J4OverriddenSetUp extends BaseTestClass {
+  // BUG: Diagnostic contains: @Before
+  @Override protected void setUp() {}
+}
+
+@RunWith(JUnit4.class)
+class J4OverriddenSetUpPublic extends BaseTestClass {
+  // BUG: Diagnostic contains: @Before
+  @Override public void setUp() {}
+}
+
