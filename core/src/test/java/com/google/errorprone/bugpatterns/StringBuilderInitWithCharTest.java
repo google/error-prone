f0268fa8adbe11c168dd294a73624f8186df6f3e
lowasser <lowasser@google.com>
2014-08-18 15:31:59 -0700

    Add error-prone check for new StringBuilder(char). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=73575507

---
+++ core/src/test/java/com/google/errorprone/bugpatterns/StringBuilderInitWithCharTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.CompilationTestHelper.sources;
+
+import com.google.errorprone.CompilationTestHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author lowasser@google.com (Louis Wasserman)
+ */
+@RunWith(JUnit4.class)
+public class StringBuilderInitWithCharTest {
+
+  private CompilationTestHelper compilationHelper;
+
+  @Before
+  public void setUp() {
+    compilationHelper = CompilationTestHelper.newInstance(new StringBuilderInitWithChar());
+  }
+
+  @Test
+  public void testPositiveCases() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(
+        sources(getClass(), "StringBuilderInitWithCharPositiveCases.java"));
+  }
+
+  @Test
+  public void testNegativeCases() throws Exception {
+    compilationHelper.assertCompileSucceeds(
+        sources(getClass(), "StringBuilderInitWithCharNegativeCases.java"));
+  }
+}
