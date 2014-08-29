e6cd3d6c676a71af5cef5e73f94b0136962be398
cushon <cushon@google.com>
2014-08-29 10:38:37 -0700

    Still more post-jdk6 cleanup. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=74403855

--- core/src/main/java/com/google/errorprone/SearchingJavaCompiler.java
+++ core/src/main/java/com/google/errorprone/SearchingJavaCompiler.java
@@ -58,15 +58,11 @@ public class SearchingJavaCompiler extends JavaCompiler {
   public static void preRegister(final Context context) {
     final Scanner scanner = context.get(Scanner.class);
     context.put(compilerKey, new Factory<JavaCompiler>() {
-      //@Override for OpenJDK 7 only
+      @Override
       public JavaCompiler make(Context ctx) {
         // Ensure that future processing rounds continue to use the same Scanner.
         ctx.put(Scanner.class, scanner);
         return new SearchingJavaCompiler(ctx);
-      }
-      //@Override for OpenJDK 6 only
-      public JavaCompiler make() {
-        return new SearchingJavaCompiler(context);
       }
     });
   }
