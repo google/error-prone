9c66dce83df8a19748b130fcc60bec3e3e6dca73
konne <konne@google.com>
2014-09-16 16:04:37 -0700

    Added caching to dataflow ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=75667075

--- jdk8/src/main/java/com/google/errorprone/dataflow/DataFlow.java
+++ jdk8/src/main/java/com/google/errorprone/dataflow/DataFlow.java
@@ -17,6 +17,9 @@
 package com.google.errorprone.dataflow;
 
 import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 import com.sun.source.tree.BlockTree;
 import com.sun.source.tree.ClassTree;
@@ -36,10 +39,14 @@ import org.checkerframework.dataflow.cfg.CFGBuilder;
 import org.checkerframework.dataflow.cfg.ControlFlowGraph;
 import org.checkerframework.dataflow.cfg.UnderlyingAST;
 
+import java.util.Objects;
+
 import javax.annotation.processing.ProcessingEnvironment;
 
 /**
  * Provides a wrapper around {@link org.checkerframework.dataflow.analysis.Analysis}.
+ *
+ * @author konne@google.com (Konstantin Weitz)
  */
 public final class DataFlow {
   /**
@@ -51,6 +58,53 @@ public final class DataFlow {
 
     ControlFlowGraph getControlFlowGraph();
   }
+
+  /*
+   * We cache both the control flow graph and the analyses that are run on it.
+   * We tuned performance to the following assumptions (which are currently true for error-prone):
+   *
+   * <ul>
+   * <li> all dataflow analyses for a method are finished before another method is analyzed
+   * <li> multiple dataflow analyses for the same method are executed in arbitrary order
+   * </ul>
+   *
+   * TODO(user): Write a test that checks these assumptions
+   */
+  private static LoadingCache<AnalysisParams, Analysis<?, ?, ?>> analysisCache =
+      CacheBuilder.newBuilder().build(
+          new CacheLoader<AnalysisParams, Analysis<?, ?, ?>>() {
+            @Override
+            public Analysis<?, ?, ?> load(AnalysisParams key) {
+              final ProcessingEnvironment env = key.getEnvironment();
+              final ControlFlowGraph cfg = key.getCFG();
+              final TransferFunction<?, ?> transfer = key.getTransferFunction();
+
+              @SuppressWarnings({"unchecked", "rawtypes"})
+              final Analysis<?, ?, ?> analysis = new Analysis(env, transfer);
+              analysis.performAnalysis(cfg);
+              return analysis;
+            }
+          });
+
+  private static LoadingCache<CFGParams, ControlFlowGraph> cfgCache =
+      CacheBuilder.newBuilder().maximumSize(1).build(
+          new CacheLoader<CFGParams, ControlFlowGraph>() {
+            @Override
+            public ControlFlowGraph load(CFGParams key) {
+              final TreePath methodPath = key.getMethodPath();
+              final MethodTree method = (MethodTree) methodPath.getLeaf();
+              final BlockTree body = method.getBody();
+              final TreePath bodyPath = new TreePath(methodPath, body);
+              final ClassTree classTree = null;
+              final UnderlyingAST ast = new UnderlyingAST.CFGMethod(method, classTree);
+              final ProcessingEnvironment env = key.getEnvironment();
+
+              analysisCache.invalidateAll();
+              CompilationUnitTree root = bodyPath.getCompilationUnit();
+              // TODO(user), replace with faster build(bodyPath, env, ast, false, false);
+              return CFGBuilder.build(root, env, ast, false, false);
+            }
+          });
 
   // TODO(user), remove once we merge jdk8 specific's with core
   public static <T> TreePath findPathFromEnclosingNodeToTopLevel(TreePath path,
@@ -64,6 +118,12 @@ public final class DataFlow {
   /**
    * Run the {@code transfer} dataflow analysis over the method which is the leaf of the
    * {@code methodPath}.
+   *
+   * <p>For caching, we make the following assumptions:
+   * - if two paths to methods are {@code equal}, their control flow graph is the same.
+   * - if two transfer functions are {@code equal}, and are run over the same control flow graph,
+   *   the analysis result is the same.
+   * - for all contexts, the analysis result is the same.
    */
   public static <A extends AbstractValue<A>, S extends Store<S>,
                  T extends TransferFunction<A, S>> Result<A, S, T>
@@ -73,23 +133,16 @@ public final class DataFlow {
         "Leaf of methodPath must be of type MethodTree, but was %s", leaf.getClass().getName());
 
     final MethodTree method = (MethodTree) leaf;
-    final BlockTree body = method.getBody();
-    Preconditions.checkNotNull(body,
+    Preconditions.checkNotNull(method.getBody(),
         "Method to analyze must have a body. Method passed in: %s in file %s",
         method.getName(),
         methodPath.getCompilationUnit().getSourceFile().getName());
 
     final ProcessingEnvironment env = JavacProcessingEnvironment.instance(context);
-    final ClassTree classTree = null;
-    final UnderlyingAST ast = new UnderlyingAST.CFGMethod(method, classTree);
-    final TreePath bodyPath = new TreePath(methodPath, body);
-
-    CompilationUnitTree root = bodyPath.getCompilationUnit();
-    // TODO(user), replace with build(bodyPath, env, ast, false, false);
-    final ControlFlowGraph cfg = CFGBuilder.build(root, env, ast, false, false);
-
-    final Analysis<A, S, T> analysis = new Analysis<A, S, T>(env, transfer);
-    analysis.performAnalysis(cfg);
+    final ControlFlowGraph cfg = cfgCache.getUnchecked(new CFGParams(methodPath, env));
+    final AnalysisParams aparams = new AnalysisParams(transfer, cfg, env);
+    @SuppressWarnings("unchecked")
+    final Analysis<A, S, T> analysis = (Analysis<A, S, T>) analysisCache.getUnchecked(aparams);
 
     return new Result<A, S, T>() {
       @Override
@@ -125,5 +178,90 @@ public final class DataFlow {
       return null;
     }
     return methodDataflow(enclosingMethodPath, context, transfer).getAnalysis().getValue(expr);
+  }
+
+  private static final class CFGParams {
+    final ProcessingEnvironment env;
+    final TreePath methodPath;
+
+    public ProcessingEnvironment getEnvironment() {
+      return env;
+    }
+
+    public TreePath getMethodPath() {
+      return methodPath;
+    }
+
+    public CFGParams(TreePath methodPath, ProcessingEnvironment env) {
+      this.env = env;
+      this.methodPath = methodPath;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(methodPath);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null) {
+        return false;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      CFGParams other = (CFGParams) obj;
+      return Objects.equals(methodPath, other.methodPath);
+    }
+  }
+
+  private static final class AnalysisParams {
+    private final ProcessingEnvironment env;
+    private final ControlFlowGraph cfg;
+    private final TransferFunction<?, ?> transfer;
+
+    public AnalysisParams(TransferFunction<?, ?> trans, ControlFlowGraph cfg,
+        ProcessingEnvironment env) {
+      this.env = env;
+      this.cfg = cfg;
+      this.transfer = trans;
+    }
+
+    public ProcessingEnvironment getEnvironment() {
+      return env;
+    }
+
+    public ControlFlowGraph getCFG() {
+      return cfg;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public TransferFunction getTransferFunction() {
+      return transfer;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(cfg, transfer);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null) {
+        return false;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      AnalysisParams other = (AnalysisParams) obj;
+      return Objects.equals(cfg, other.cfg)
+          && Objects.equals(transfer, other.transfer);
+    }
   }
 }
