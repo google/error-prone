c5f6ec0d7abd7a03b4974c280c475990770f4139
awturner <awturner@google.com>
2014-12-04 01:17:07 -0800

    Add errorprone check for Preconditions.checkNotNull on a proto field. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=81318124

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/ProtoFieldPreconditionsCheckNotNull.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.Category.GUAVA;
+import static com.google.errorprone.BugPattern.MaturityLevel.MATURE;
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.argument;
+import static com.google.errorprone.matchers.Matchers.methodSelect;
+import static com.google.errorprone.matchers.Matchers.parentNode;
+import static com.google.errorprone.matchers.Matchers.staticMethod;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.fixes.Fix;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.Tree.Kind;
+import com.sun.tools.javac.tree.JCTree.JCFieldAccess;
+
+/**
+ * @author awturner@google.com (Andy Turner)
+ */
+@BugPattern(name = "ProtoFieldPreconditionsCheckNotNull",
+    summary = "Protobuf fields cannot be null, so this check is redundant",
+    explanation = "This checker looks for comparisons of protocol buffer fields with null "
+        + "via the com.google.common.base.Preconditions.checkNotNull method. "
+        + "If a proto field is not specified, its field accessor will return a non-null default "
+        + "value. Thus, the result of calling one of these accessors can never be null, and "
+        + "comparisons like these often indicate a nearby error.\n\n"
+        + "If you meant to check whether an optional field has been set, you should use the "
+        + "hasField() method instead.",
+    category = GUAVA, severity = WARNING, maturity = MATURE)
+public class ProtoFieldPreconditionsCheckNotNull
+    extends BugChecker implements MethodInvocationTreeMatcher {
+
+  private static final String PROTO_SUPER_CLASS = "com.google.protobuf.GeneratedMessage";
+
+  private static final Matcher<MethodInvocationTree> protoMessageReceiverMatcher =
+      Matchers.methodSelect(Matchers.methodReceiver(Matchers.isSubtypeOf(PROTO_SUPER_CLASS)));
+
+  private static final String LIST_INTERFACE = "java.util.List";
+
+  private static final Matcher<Tree> returnsListMatcher =
+      Matchers.isCastableTo(LIST_INTERFACE);
+
+  private static final Matcher<ExpressionTree> PROTO_MESSAGE_INVOCATION_MATCHER =
+      new Matcher<ExpressionTree>() {
+        @Override
+        public boolean matches(ExpressionTree tree, VisitorState state) {
+          return isProtoMessageGetInvocation(tree, state);
+        }
+      };
+
+  @SuppressWarnings({"unchecked"})
+  private static final Matcher<MethodInvocationTree> CHECK_NOT_NULL_MATCHER = allOf(
+      methodSelect(staticMethod("com.google.common.base.Preconditions", "checkNotNull")),
+      argument(0, Matchers.<ExpressionTree>allOf(
+          Matchers.<ExpressionTree>kindIs(Kind.METHOD_INVOCATION),
+          PROTO_MESSAGE_INVOCATION_MATCHER)));
+
+  private static boolean isProtoMessageGetInvocation(ExpressionTree tree, VisitorState state) {
+    return (isGetMethodInvocation(tree, state) || isGetListMethodInvocation(tree, state))
+        && receiverIsProtoMessage(tree, state);
+  }
+
+  private static boolean isFieldGetMethod(String methodName) {
+    return methodName.startsWith("get");
+  }
+
+  private static boolean isGetListMethodInvocation(ExpressionTree tree, VisitorState state) {
+    if (tree.getKind() == Tree.Kind.METHOD_INVOCATION) {
+      MethodInvocationTree method = (MethodInvocationTree) tree;
+      if (!method.getArguments().isEmpty()) {
+        return false;
+      }
+      if (!returnsListMatcher.matches(method, state)) {
+        return false;
+      }
+      ExpressionTree expressionTree = method.getMethodSelect();
+      if (expressionTree instanceof JCFieldAccess) {
+        JCFieldAccess access = (JCFieldAccess) expressionTree;
+        String methodName = access.sym.getQualifiedName().toString();
+        return isFieldGetMethod(methodName);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean isGetMethodInvocation(ExpressionTree tree, VisitorState state) {
+    if (tree.getKind() == Tree.Kind.METHOD_INVOCATION) {
+      MethodInvocationTree method = (MethodInvocationTree) tree;
+      if (!method.getArguments().isEmpty()) {
+        return false;
+      }
+      if (returnsListMatcher.matches(method, state)) {
+        return false;
+      }
+      ExpressionTree expressionTree = method.getMethodSelect();
+      if (expressionTree instanceof JCFieldAccess) {
+        JCFieldAccess access = (JCFieldAccess) expressionTree;
+        String methodName = access.sym.getQualifiedName().toString();
+        return isFieldGetMethod(methodName);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean receiverIsProtoMessage(ExpressionTree tree, VisitorState state) {
+    return protoMessageReceiverMatcher.matches(((MethodInvocationTree) tree), state);
+  }
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (!CHECK_NOT_NULL_MATCHER.matches(tree, state)
+        || tree.getArguments().isEmpty()) {
+      return Description.NO_MATCH;
+    }
+
+    Fix fix;
+    if (parentNode(Matchers.<MethodInvocationTree>kindIs(Kind.EXPRESSION_STATEMENT))
+        .matches(tree, state)) {
+      fix = SuggestedFix.delete(state.getPath().getParentPath().getLeaf());
+    } else {
+      fix = SuggestedFix.replace(tree, tree.getArguments().get(0).toString());
+    }
+    return describeMatch(tree, fix);
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/bugpatterns/ProtoFieldPreconditionsCheckNotNullTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.CompilationTestHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+import javax.tools.JavaFileObject;
+
+/**
+ * @author awturner@google.com (Andy Turner)
+ */
+@RunWith(JUnit4.class)
+public class ProtoFieldPreconditionsCheckNotNullTest {
+
+  private CompilationTestHelper compilationHelper;
+  private JavaFileObject protoFile;
+
+  @Before
+  public void setUp() throws Exception {
+    compilationHelper =
+        CompilationTestHelper.newInstance(new ProtoFieldPreconditionsCheckNotNull());
+    protoFile = compilationHelper.fileManager().source(getClass(), "proto/ProtoTest.java");
+  }
+
+  private List<JavaFileObject> getSourceFiles(String mainFileName) throws URISyntaxException {
+    JavaFileObject mainFile = compilationHelper.fileManager().source(getClass(), mainFileName);
+    return ImmutableList.of(mainFile, protoFile);
+  }
+
+  @Test
+  public void testPositiveCase() throws Exception {
+    compilationHelper.assertCompileSucceedsWithMessages(
+        getSourceFiles("ProtoFieldPreconditionsCheckNotNullPositiveCases.java"));
+  }
+
+  @Test
+  public void testNegativeCase() throws Exception {
+    compilationHelper.assertCompileSucceeds(
+        getSourceFiles("ProtoFieldPreconditionsCheckNotNullNegativeCases.java"));
+  }
+}
