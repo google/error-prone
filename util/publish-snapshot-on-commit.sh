a9044130fb1b6b8461204001ffa2ec3b36107454
Liam Miller-Cushon <cushon@google.com>
2015-01-06 17:10:55 -0800

    Configure travis to publish snapshots and update documentation.

---
+++ util/publish-snapshot-on-commit.sh
@@ -0,0 +1,13 @@
+# https://github.com/google/dagger/blob/master/util/publish-snapshot-on-commit.sh
+
+if [ "$TRAVIS_REPO_SLUG" == "cushon/error-prone" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo -e "Publishing maven snapshot...\n"
+
+  mvn clean deploy --settings="util/settings.xml" -DskipTests=true -Dinvoker.skip=true -Dmaven.javadoc.skip=true
+
+  echo -e "Published maven snapshot"
+fi
+
