51f96797af4aa0b26f2bcfb6f90a397a107a0629
cushon <cushon@google.com>
2015-01-21 20:49:42 -0800

    Ignore void-returning methods in classes/packages with @CheckReturnValue.
    Also enforce that @CRV is not applied directly to void methods. This is more
    important now that we support the annotation on classes and packages.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=84495547

--- core/src/main/java/com/google/errorprone/bugpatterns/CheckReturnValue.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/CheckReturnValue.java
@@ -25,11 +25,17 @@ import static com.google.errorprone.util.ASTHelpers.hasAnnotation;
 
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
 
 import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
+import com.sun.tools.javac.code.TypeTag;
+
+import javax.lang.model.element.ElementKind;
 
 /**
  * @author eaftan@google.com (Eddie Aftandilian)
@@ -41,19 +47,22 @@ import com.sun.tools.javac.code.Symbol.MethodSymbol;
         + "should be checked.  This error is triggered when one of these methods is called but "
         + "the result is not used.",
     category = JDK, severity = ERROR, maturity = MATURE)
-public class CheckReturnValue extends AbstractReturnValueIgnored {
+public class CheckReturnValue extends AbstractReturnValueIgnored implements MethodTreeMatcher {
   
   private static final Matcher<MethodInvocationTree> MATCHER = 
       new Matcher<MethodInvocationTree>() {
         @Override
         public boolean matches(MethodInvocationTree tree, VisitorState state) {
           MethodSymbol method = ASTHelpers.getSymbol(tree);
+          if (method.getReturnType().getTag() == TypeTag.VOID) {
+            return false;
+          }
           return hasAnnotation(method, javax.annotation.CheckReturnValue.class)
               || hasAnnotation(enclosingClass(method), javax.annotation.CheckReturnValue.class)
               || hasAnnotation(enclosingPackage(method), javax.annotation.CheckReturnValue.class);
         }
       };
-  
+
   /**
    * Return a matcher for method invocations in which the method being called has the
    * @CheckReturnValue annotation.
@@ -61,5 +70,27 @@ public class CheckReturnValue extends AbstractReturnValueIgnored {
   @Override
   public Matcher<MethodInvocationTree> specializedMatcher() {
     return MATCHER;
+  }
+
+  /**
+   * This enforces that {@link @CheckReturnValue} is not applied to void-returning method
+   * declarations. Doing so makes no sense, because there is no return value to check.
+   */
+  @Override
+  public Description matchMethod(MethodTree tree, VisitorState state) {
+    MethodSymbol method = ASTHelpers.getSymbol(tree);
+    if (!hasAnnotation(method, javax.annotation.CheckReturnValue.class)) {
+      return Description.NO_MATCH;
+    }
+    if (method.getKind() != ElementKind.METHOD) {
+      // skip contructors (which javac thinks are void-returning)
+      return Description.NO_MATCH;
+    }
+    if (method.getReturnType().getTag() != TypeTag.VOID) {
+      return Description.NO_MATCH;
+    }
+    return buildDescription(tree)
+        .setMessage("@CheckReturnValue may not be applied to void-returning methods")
+        .build();
   }
 }
