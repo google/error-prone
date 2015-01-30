7702f36fed11ca80e6ece43c649c4a731d218a5d
cushon <cushon@google.com>
2015-01-30 11:25:10 -0800

    Create @CanIgnoreReturnValue
    Fixes #279
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=85175748

--- core/src/main/java/com/google/errorprone/bugpatterns/CheckReturnValue.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/CheckReturnValue.java
@@ -23,15 +23,21 @@ import static com.google.errorprone.util.ASTHelpers.enclosingClass;
 import static com.google.errorprone.util.ASTHelpers.enclosingPackage;
 import static com.google.errorprone.util.ASTHelpers.hasAnnotation;
 
+import com.google.common.base.Optional;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.errorprone.bugpatterns.BugChecker.ClassTreeMatcher;
 import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
 
+import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Symbol.ClassSymbol;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
 import com.sun.tools.javac.code.TypeTag;
 
@@ -47,19 +53,61 @@ import javax.lang.model.element.ElementKind;
         + "should be checked.  This error is triggered when one of these methods is called but "
         + "the result is not used.",
     category = JDK, severity = ERROR, maturity = MATURE)
-public class CheckReturnValue extends AbstractReturnValueIgnored implements MethodTreeMatcher {
-  
+public class CheckReturnValue extends AbstractReturnValueIgnored
+    implements MethodTreeMatcher, ClassTreeMatcher {
+
+  private static Optional<Boolean> shouldCheckReturnValue(Symbol sym) {
+    if (hasAnnotation(sym, CanIgnoreReturnValue.class)) {
+      return Optional.of(false);
+    }
+    if (hasAnnotation(sym, javax.annotation.CheckReturnValue.class)) {
+      return Optional.of(true);
+    }
+    return Optional.absent();
+  }
+
+  private static Optional<Boolean> checkEnclosingClasses(MethodSymbol method) {
+    Symbol enclosingClass = enclosingClass(method);
+    while (enclosingClass instanceof ClassSymbol) {
+      Optional<Boolean> result = shouldCheckReturnValue(enclosingClass);
+      if (result.isPresent()) {
+        return result;
+      }
+      enclosingClass = enclosingClass.owner;
+    }
+    return Optional.absent();
+  }
+
+  private static Optional<Boolean> checkPackage(MethodSymbol method) {
+    return shouldCheckReturnValue(enclosingPackage(method));
+  }
+
   private static final Matcher<MethodInvocationTree> MATCHER = 
       new Matcher<MethodInvocationTree>() {
         @Override
         public boolean matches(MethodInvocationTree tree, VisitorState state) {
+
           MethodSymbol method = ASTHelpers.getSymbol(tree);
           if (method.getReturnType().getTag() == TypeTag.VOID) {
             return false;
           }
-          return hasAnnotation(method, javax.annotation.CheckReturnValue.class)
-              || hasAnnotation(enclosingClass(method), javax.annotation.CheckReturnValue.class)
-              || hasAnnotation(enclosingPackage(method), javax.annotation.CheckReturnValue.class);
+
+          Optional<Boolean> result = shouldCheckReturnValue(method);
+          if (result.isPresent()) {
+            return result.get();
+          }
+
+          result = checkEnclosingClasses(method);
+          if (result.isPresent()) {
+            return result.get();
+          }
+
+          result = checkPackage(method);
+          if (result.isPresent()) {
+            return result.get();
+          }
+
+          return false;
         }
       };
 
@@ -72,14 +120,35 @@ public class CheckReturnValue extends AbstractReturnValueIgnored implements Meth
     return MATCHER;
   }
 
+  private static final String BOTH_ERROR =
+      "@CheckReturnValue and @CanIgnoreReturnValue cannot both be applied to the same %s";
+
   /**
-   * This enforces that {@link @CheckReturnValue} is not applied to void-returning method
-   * declarations. Doing so makes no sense, because there is no return value to check.
+   * Validate {@link javax.annotation.CheckReturnValue} and {@link CanIgnoreReturnValue} usage on
+   * methods.
+   *
+   * <p>The annotations should not both be appled to the same method.
+   *
+   * <p>The annotations should not be applied to void-returning methods. Doing so makes no sense,
+   * because there is no return value to check.
    */
   @Override
   public Description matchMethod(MethodTree tree, VisitorState state) {
     MethodSymbol method = ASTHelpers.getSymbol(tree);
-    if (!hasAnnotation(method, javax.annotation.CheckReturnValue.class)) {
+
+    boolean checkReturn = hasAnnotation(method, javax.annotation.CheckReturnValue.class);
+    boolean canIgnore = hasAnnotation(method, CanIgnoreReturnValue.class);
+
+    if (checkReturn && canIgnore) {
+      return buildDescription(tree).setMessage(String.format(BOTH_ERROR, "method")).build();  
+    }
+
+    String annotationToValidate;
+    if (checkReturn) {
+      annotationToValidate = javax.annotation.CheckReturnValue.class.getSimpleName();
+    } else if (canIgnore) {
+      annotationToValidate = CanIgnoreReturnValue.class.getSimpleName();
+    } else {
       return Description.NO_MATCH;
     }
     if (method.getKind() != ElementKind.METHOD) {
@@ -89,8 +158,21 @@ public class CheckReturnValue extends AbstractReturnValueIgnored implements Meth
     if (method.getReturnType().getTag() != TypeTag.VOID) {
       return Description.NO_MATCH;
     }
-    return buildDescription(tree)
-        .setMessage("@CheckReturnValue may not be applied to void-returning methods")
-        .build();
+    String message =
+        String.format("@%s may not be applied to void-returning methods", annotationToValidate);
+    return buildDescription(tree).setMessage(message).build();
+  }
+
+  /**
+   * Validate that at most one of {@link javax.annotation.CheckReturnValue} and
+   * {@link CanIgnoreReturnValue} are applied to a class (or interface or enum).
+   */
+  @Override
+  public Description matchClass(ClassTree tree, VisitorState state) {
+    if (hasAnnotation(tree, javax.annotation.CheckReturnValue.class)
+        && hasAnnotation(tree, CanIgnoreReturnValue.class)) {
+      return buildDescription(tree).setMessage(String.format(BOTH_ERROR, "class")).build();
+    }
+    return Description.NO_MATCH;
   }
 }
