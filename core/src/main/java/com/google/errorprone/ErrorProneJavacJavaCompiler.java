google/error-prone

commit 40112e847348bca3204bdd6eec8134c6b991336c

Merge the error-prone implementations of javac.main.JavaCompiler, and begin using the javac8 TaskListener interface for ErrorProneAnalyzer.
Actually wiring up the TaskListener (see ErrorProneAnalyzer#register) is
blocked on having the context be preserved across annotation processing rounds.

-------------
Created by MOE: http://code.google.com/p/moe-java
MOE_MIGRATED_REVID=77800781

core/src/main/java/com/google/errorprone/ErrorProneJavacJavaCompiler.java
@@ -16,9 +16,14 @@
 
 package com.google.errorprone;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.common.base.Optional;
 
+import com.sun.source.util.TaskEvent;
+import com.sun.source.util.TaskEvent.Kind;
 import com.sun.tools.javac.comp.AttrContext;
+import com.sun.tools.javac.comp.CompileStates.CompileState;
 import com.sun.tools.javac.comp.Env;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.util.Context;
@@ -36,7 +41,7 @@ import java.util.Queue;
  *
  * @author alexeagle@google.com (Alex Eagle)
  */
-public class ErrorReportingJavaCompiler extends JavaCompiler {
+public class ErrorProneJavacJavaCompiler extends JavaCompiler {
 
   private final ErrorProneAnalyzer errorProneAnalyzer;
 
@@ -47,17 +52,20 @@ public class ErrorReportingJavaCompiler extends JavaCompiler {
     JavacMessages.instance(context).add("com.google.errorprone.errors");
   }
 
-  public ErrorReportingJavaCompiler(Context context) {
+  private ErrorProneJavacJavaCompiler(Context context, Scanner scanner, 
+      SearchResultsPrinter resultsPrinter) {
     super(context);
-    if (context.get(Scanner.class) == null) {
-      throw new IllegalArgumentException("context must have a Scanner instance");
-    }
+    checkNotNull(scanner);
 
     // Setup message bundle.
     setupMessageBundle(context);
 
     // Create ErrorProneAnalyzer.
-    errorProneAnalyzer = new ErrorProneAnalyzer(log, context);
+    errorProneAnalyzer = ErrorProneAnalyzer.create(scanner, resultsPrinter).init(context);
+  }
+
+  public static void preRegister(Context context, Scanner scanner) {
+    preRegister(context, scanner, null);
   }
 
   /**
@@ -66,31 +74,30 @@ public class ErrorReportingJavaCompiler extends JavaCompiler {
    * superclass, JavaCompiler) will actually construct and return our version.
    * It's necessary since many new JavaCompilers may
    * be requested for later stages of the compilation (annotation processing),
-   * within the same Context. And it's the preferred way for extending behavior
-   * within javac, per the documentation in {@link com.sun.tools.javac.util.Context}.
+   * within the same Context.
    */
-  public static void preRegister(final Context context) {
-    final Scanner scanner = context.get(Scanner.class);
+  public static void preRegister(Context context, final Scanner scanner,
+      final SearchResultsPrinter resultsPrinter) {
     context.put(compilerKey, new Factory<JavaCompiler>() {
       @Override
       public JavaCompiler make(Context ctx) {
         // Ensure that future processing rounds continue to use the same Scanner.
-        ctx.put(Scanner.class, scanner);
-        return new ErrorReportingJavaCompiler(ctx);
+        return new ErrorProneJavacJavaCompiler(ctx, scanner, resultsPrinter);
       }
     });
   }
 
   @Override
-  protected void flow(Env<AttrContext> attrContextEnv, Queue<Env<AttrContext>> envs) {
-    super.flow(attrContextEnv, envs);
+  protected void flow(Env<AttrContext> env, Queue<Env<AttrContext>> results) {
+    if (compileStates.isDone(env, CompileState.FLOW)) {
+      return;
+    }
+    super.flow(env, results);
     try {
-      postFlow(attrContextEnv);
+      postFlow(env);
     } catch (Throwable e) {
       ByteArrayOutputStream stackTrace = new ByteArrayOutputStream();
-      PrintWriter writer = new PrintWriter(stackTrace);
-      e.printStackTrace(writer);
-      writer.flush();
+      e.printStackTrace(new PrintWriter(stackTrace, true));
       String version = loadVersionFromPom().or("unknown version");
       log.error("error.prone.crash", stackTrace.toString(), version);
     }
@@ -111,24 +118,10 @@ public class ErrorReportingJavaCompiler extends JavaCompiler {
     return Optional.of(mavenProperties.getProperty("version"));
   }
 
-  public void profilePostFlow(Env<AttrContext> attrContextEnv) {
-    try {
-      // For profiling with YourKit, add to classpath:
-      // <Profiler Installation Directory>/lib/yjp-controller-api-redist.jar
-//      Controller controller = new Controller();
-//      controller.startCPUProfiling(ProfilingModes.CPU_SAMPLING, "");
-      postFlow(attrContextEnv);
-//      controller.stopCPUProfiling();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
   /**
    * Run Error Prone analysis after performing dataflow checks.
    */
   public void postFlow(Env<AttrContext> env) {
-    errorProneAnalyzer.reportReadyForAnalysis(env, errorCount() > 0);
+    errorProneAnalyzer.finished(new TaskEvent(Kind.ANALYZE, env.toplevel, env.enclClass.sym));
   }
-
 }

core/pom.xml
@@ -84,6 +84,11 @@
       <scope>test</scope>
     </dependency>
     <dependency>
+      <groupId>com.google.truth</groupId>
+      <artifactId>truth</artifactId>
+      <version>0.23</version>
+    </dependency>
+    <dependency>
       <groupId>com.google.inject</groupId>
       <artifactId>guice</artifactId>
       <version>3.0</version>

core/src/main/java/com/google/errorprone/ErrorProneAnalyzer.java
@@ -16,11 +16,22 @@
 
 package com.google.errorprone;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.base.Objects;
+
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.util.TaskEvent;
+import com.sun.source.util.TaskEvent.Kind;
+import com.sun.source.util.TaskListener;
 import com.sun.source.util.TreePath;
+import com.sun.tools.javac.api.MultiTaskListener;
 import com.sun.tools.javac.code.Symbol.CompletionFailure;
-import com.sun.tools.javac.comp.AttrContext;
-import com.sun.tools.javac.comp.Env;
+import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCClassDecl;
 import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.Log;
@@ -33,30 +44,50 @@ import java.util.Set;
 /**
  * Used to run an error-prone analysis as a phase in the javac compiler.
  */
-public class ErrorProneAnalyzer {
+public class ErrorProneAnalyzer implements TaskListener {
 
-  private final Log log;
-  private final Context context;
+  public static ErrorProneAnalyzer create(Scanner scanner) {
+    return create(scanner, null);
+  }
+
+  public static ErrorProneAnalyzer create(
+      Scanner scanner, SearchResultsPrinter searchResultsPrinter) {
+    checkNotNull(scanner);
+    return new ErrorProneAnalyzer(scanner, searchResultsPrinter);
+  }
+
+  /**
+   * Initializes the analyzer with the current compilation context. (E.g. for the current
+   * annotation processing round.)
+   */
+  public ErrorProneAnalyzer init(Context context) {
+    this.initialized = true;
+    this.context = context;
+    this.log = Log.instance(context);
+    this.compiler = JavaCompiler.instance(context);
+    return this;
+  }
+
+  public ErrorProneAnalyzer register(Context context) {
+    init(context);
+    MultiTaskListener.instance(context).add(this);
+    return this;
+  }
+
   private final Scanner errorProneScanner;
   // If matchListener != null, then we are in search mode.
   private final SearchResultsPrinter resultsPrinter;
+  // The set of trees that have already been scanned.
+  private final Set<Tree> seen = new HashSet<>();
 
-  /** The set of trees that have already been scanned. */
-  private final Set<JCTree> seen = new HashSet<>();
+  private Context context;
+  private Log log;
+  private JavaCompiler compiler;
+  private boolean initialized = false;
 
-  public ErrorProneAnalyzer(Log log, Context context) {
-    this(log, context, null);
-  }
-
-  public ErrorProneAnalyzer(Log log, Context context, SearchResultsPrinter resultsPrinter) {
-    this.log = log;
-    this.context = context;
+  private ErrorProneAnalyzer(Scanner scanner, SearchResultsPrinter resultsPrinter) {
+    this.errorProneScanner = scanner;
     this.resultsPrinter = resultsPrinter;
-    this.errorProneScanner = context.get(Scanner.class);
-    if (this.errorProneScanner == null) {
-      throw new IllegalStateException(
-          "No error-prone scanner registered in context. Is annotation processing enabled?");
-    }
   }
 
   private static class DeclFreeCompilationUnitWrapper extends JCCompilationUnit {
@@ -72,33 +103,62 @@ public class ErrorProneAnalyzer {
     }
   }
 
+  @Override
+  public void started(TaskEvent taskEvent) {
+    checkState(initialized);
+  }
+
+  @Override
+  public void finished(TaskEvent taskEvent) {
+    if (taskEvent.getKind() == Kind.ANALYZE) {
+      // One TaskEvent is created per class declaration after FLOW is finished, but the TaskEvent
+      // only provides the CompilationUnitTree and the symbol (element) for the class declaration.
+      // We have to search for the class decl manually...
+      // TODO(user): suggest to upstream that TaskEvents provide a TreePath?
+      JCClassDecl currentClassTree = null;
+      for (Tree declTree : taskEvent.getCompilationUnit().getTypeDecls()) {
+        if (declTree instanceof JCClassDecl) {
+          JCClassDecl classTree = (JCClassDecl) declTree;
+          if (Objects.equal(classTree.sym, taskEvent.getTypeElement())) {
+            currentClassTree = classTree;
+          }
+        }
+      }
+
+      TreePath path = currentClassTree != null
+          ? TreePath.getPath(taskEvent.getCompilationUnit(), currentClassTree)
+          : new TreePath(taskEvent.getCompilationUnit());
+      reportReadyForAnalysis(taskEvent, path, compiler.errorCount() > 0);
+    }
+  }
+
   /**
-   * Reports that a class (represented by the env) is ready for error-prone to analyze.
+   * Reports that a class is ready for error-prone to analyze.
+   *
+   * @param path the path from the compilation unit to the class declaration
+   * @param hasErrors true if errors have been reported during the compilation
    */
-  public void reportReadyForAnalysis(Env<AttrContext> env, boolean hasErrors) {
+  public void reportReadyForAnalysis(TaskEvent taskEvent, TreePath path, boolean hasErrors) {
     try {
-      // Javac sometimes performs the flow phase on class decls twice. If javac is desugaring some
-      // class whose superclass hasn't been desugared, the superclass gets desugared early.
-      // When the compilation reaches the point where the superclass would originally have gone
-      // through dataflow and desugaring it goes through them again. This is normally fine since
-      // those phases are (hopefully?) both idempotent. However, error-prone assumes that (1) it
-      // sees each class once, and (2) that they haven't been desugared yet.
-      if (!seen.add(env.tree)) {
+      if (seen.add(path.getCompilationUnit())) {
+        // Visit the compilation unit separately from the enclosed class declarations, and
+        // prevent scanners from accessing the (incomplete) class declarations.
+        TreePath rootPath = new TreePath(new DeclFreeCompilationUnitWrapper(
+            (JCCompilationUnit) path.getCompilationUnit()));
+        errorProneScanner.scan(rootPath, createVisitorState(path.getCompilationUnit()));
+      }
+
+      if (path.getLeaf().getKind() == Tree.Kind.COMPILATION_UNIT) {
+        // If we're visiting, e.g., a package-info.java file with no class decls, then we're done.
         return;
       }
 
-      TreePath path;
-      if (env.toplevel != env.tree) {
-        // The current tree is a class decl, so construct a TreePath rooted at the enclosing
-        // compilation unit.
-        path = TreePath.getPath(env.toplevel, env.tree);
-      } else {
-        // The current tree is a compilation unit. Wrap the compilation unit to hide its class
-        // decls, since (1) we've already visited them, and (2) the bodies of all but the last class
-        // decl have been lowered out of them.
-        path = new TreePath(new DeclFreeCompilationUnitWrapper(env.toplevel));
+      if (!seen.add(path.getLeaf())) {
+        throw new IllegalStateException("Duplicate FLOW event for: " + taskEvent.getTypeElement());
       }
-      errorProneScanner.scan(path, createVisitorState(env));
+
+      errorProneScanner.scan(path, createVisitorState(path.getCompilationUnit()));
+
     } catch (CompletionFailure e) {
       // A CompletionFailure can be triggered when error-prone tries to complete a symbol
       // that isn't on the compilation classpath. This can occur when a check performs an
@@ -119,19 +179,18 @@ public class ErrorProneAnalyzer {
   }
 
   /**
-   * Create a VisitorState object from an environment.
+   * Create a VisitorState object from a compilation unit.
    */
-  private VisitorState createVisitorState(Env<AttrContext> env) {
+  private VisitorState createVisitorState(CompilationUnitTree compilation) {
     if (resultsPrinter != null) {
-      resultsPrinter.setCompilationUnit(env.toplevel.sourcefile);
+      resultsPrinter.setCompilationUnit(compilation.getSourceFile());
       return new VisitorState(context, resultsPrinter);
     } else {
-      DescriptionListener logReporter = new JavacErrorDescriptionListener(log,
-          env.toplevel.endPositions,
-          env.enclClass.sym.sourcefile != null
-          ? env.enclClass.sym.sourcefile
-              : env.toplevel.sourcefile,
-              context);
+      DescriptionListener logReporter = new JavacErrorDescriptionListener(
+          log,
+          ((JCCompilationUnit) compilation).endPositions,
+          compilation.getSourceFile(),
+          compiler);
       return new VisitorState(context, logReporter);
     }
   }

core/src/main/java/com/google/errorprone/ErrorProneCompiler.java
@@ -16,13 +16,14 @@
 
 package com.google.errorprone;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.errorprone.ErrorProneScanner.EnabledPredicate.DEFAULT_CHECKS;
 
 import com.sun.tools.javac.file.JavacFileManager;
-import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.main.Main;
 import com.sun.tools.javac.main.Main.Result;
 import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.JavacMessages;
 import com.sun.tools.javac.util.List;
 
 import java.io.PrintWriter;
@@ -81,32 +82,29 @@ public class ErrorProneCompiler {
   }
 
   private final DiagnosticListener<? super JavaFileObject> diagnosticListener;
-  private final Class<? extends JavaCompiler> compilerClass;
   private final Main main;
   private final PrintWriter printWriter;
-
-  /**
-   * A custom Scanner to use if we want to use a non-default set of error-prone checks, e.g.
-   * for testing.  Null if we want to use the default set of checks.
-   */
+  private final SearchResultsPrinter resultsPrinter;
   private final Scanner errorProneScanner;
 
-  private ErrorProneCompiler(String s, PrintWriter printWriter,
+  private ErrorProneCompiler(
+      String compilerName,
+      PrintWriter printWriter,
       DiagnosticListener<? super JavaFileObject> diagnosticListener,
       Scanner errorProneScanner,
-      Class<? extends JavaCompiler> compilerClass) {
-    this.printWriter = printWriter;
-    this.main = new Main(s, printWriter);
+      boolean useResultsPrinter) {
+    this.printWriter = checkNotNull(printWriter);
+    this.main = new Main(compilerName, printWriter);
     this.diagnosticListener = diagnosticListener;
-    this.errorProneScanner = errorProneScanner;
-    this.compilerClass = compilerClass;
+    this.errorProneScanner = checkNotNull(errorProneScanner);
+    this.resultsPrinter = useResultsPrinter ? new SearchResultsPrinter(printWriter) : null;
   }
 
   public static class Builder {
-    DiagnosticListener<? super JavaFileObject> diagnosticListener = null;
-    PrintWriter out = new PrintWriter(System.err, true);
-    String compilerName = "javac (with error-prone)";
-    Class<? extends JavaCompiler> compilerClass = ErrorReportingJavaCompiler.class;
+    private DiagnosticListener<? super JavaFileObject> diagnosticListener = null;
+    private PrintWriter out = new PrintWriter(System.err, true);
+    private String compilerName = "javac (with error-prone)";
+    private boolean useResultsPrinter = false;
 
     /**
      * A custom Scanner to use if we want to use a non-default set of error-prone checks, e.g.
@@ -115,7 +113,12 @@ public class ErrorProneCompiler {
     Scanner scanner;
 
     public ErrorProneCompiler build() {
-      return new ErrorProneCompiler(compilerName, out, diagnosticListener, scanner, compilerClass);
+      return new ErrorProneCompiler(
+          compilerName,
+          out,
+          diagnosticListener,
+          scanner != null ? scanner : new ErrorProneScanner(DEFAULT_CHECKS),
+          useResultsPrinter);
     }
 
     public Builder named(String compilerName) {
@@ -133,14 +136,15 @@ public class ErrorProneCompiler {
       return this;
     }
 
+    // TODO(user): SearchingResultPrinter interacts awkwardly with everything else and is barely
+    // used; consider deleting it.
     public Builder search(Scanner scanner) {
-      this.compilerClass = SearchingJavaCompiler.class;
       this.scanner = scanner;
+      this.useResultsPrinter = true;
       return this;
     }
 
     public Builder report(Scanner scanner) {
-      this.compilerClass = ErrorReportingJavaCompiler.class;
       this.scanner = scanner;
       return this;
     }
@@ -172,28 +176,31 @@ public class ErrorProneCompiler {
       context.put(DiagnosticListener.class, diagnosticListener);
     }
 
-    Scanner scannerInContext = context.get(Scanner.class);
-    if (scannerInContext == null) {
-      if (errorProneScanner == null) {
-        scannerInContext = new ErrorProneScanner(DEFAULT_CHECKS);
-      } else {
-        scannerInContext = errorProneScanner;
-      }
-      context.put(Scanner.class, scannerInContext);
-    }
     try {
-      scannerInContext.setDisabledChecks(epOptions.getDisabledChecks());
+      errorProneScanner.setDisabledChecks(epOptions.getDisabledChecks());
     } catch (InvalidCommandLineOptionException e) {
       printWriter.println(e.getMessage());
+      printWriter.flush();
       return Result.CMDERR;
     }
 
-    try {
-      compilerClass.getMethod("preRegister", Context.class).invoke(null, context);
-    } catch (ReflectiveOperationException e) {
-      throw new RuntimeException("The JavaCompiler used must have the preRegister static method. "
-          + "We are very sorry.", e);
+    setupMessageBundle(context);
+    ErrorProneJavacJavaCompiler.preRegister(context, errorProneScanner, resultsPrinter);
+
+    Result result =
+        main.compile(epOptions.getRemainingArgs(), context, javaFileObjects, processors);
+
+    if (resultsPrinter != null) {
+      resultsPrinter.printMatches();
     }
-    return main.compile(epOptions.getRemainingArgs(), context, javaFileObjects, processors);
+
+    return result;
+  }
+
+  /**
+   * Registers our message bundle.
+   */
+  public static void setupMessageBundle(Context context) {
+    JavacMessages.instance(context).add("com.google.errorprone.errors");
   }
 }

core/src/main/java/com/google/errorprone/ErrorProneJavaCompiler.java
@@ -16,6 +16,8 @@
 
 package com.google.errorprone;
 
+import static com.google.errorprone.ErrorProneScanner.EnabledPredicate.DEFAULT_CHECKS;
+
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
 import com.google.errorprone.bugpatterns.BugChecker;
@@ -84,7 +86,7 @@ public class ErrorProneJavaCompiler implements JavaCompiler {
     this(javacTool, new Supplier<ErrorProneScanner>() {
       @Override
       public ErrorProneScanner get() {
-        return new ErrorProneScanner(ErrorProneScanner.EnabledPredicate.DEFAULT_CHECKS);
+        return new ErrorProneScanner(DEFAULT_CHECKS);
       }
     });
   }
@@ -113,13 +115,12 @@ public class ErrorProneJavaCompiler implements JavaCompiler {
         out, fileManager, diagnosticListener, remainingOptions, classes, compilationUnits);
     Context context = ((JavacTaskImpl) task).getContext();
     ErrorProneScanner scanner = scannerSupplier.get();
-    context.put(Scanner.class, scanner);
     try {
       scanner.setDisabledChecks(errorProneOptions.getDisabledChecks());
     } catch (InvalidCommandLineOptionException e) {
       throw new RuntimeException(e);
     }
-    ErrorReportingJavaCompiler.preRegister(context);
+    ErrorProneJavacJavaCompiler.preRegister(context, scanner);
     return task;
   }
 

core/src/main/java/com/google/errorprone/JavacErrorDescriptionListener.java
@@ -24,7 +24,6 @@ import com.google.errorprone.matchers.Description;
 
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.tree.EndPosTable;
-import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.JCDiagnostic.DiagnosticPosition;
 import com.sun.tools.javac.util.Log;
 
@@ -55,12 +54,15 @@ public class JavacErrorDescriptionListener implements DescriptionListener {
   // The suffix for properties in src/main/resources/com/google/errorprone/errors.properties
   private static final String MESSAGE_BUNDLE_KEY = "error.prone";
 
-  public JavacErrorDescriptionListener(Log log, EndPosTable endPositions,
-                                       JavaFileObject sourceFile, Context context) {
+  public JavacErrorDescriptionListener(
+      Log log,
+      EndPosTable endPositions,
+      JavaFileObject sourceFile,
+      JavaCompiler compiler) {
     this.log = log;
     this.endPositions = endPositions;
     this.sourceFile = sourceFile;
-    this.compiler = JavaCompiler.instance(context);
+    this.compiler = compiler;
     try {
       this.sourceFileContent = sourceFile.getCharContent(true);
     } catch (IOException e) {

core/src/main/java/com/google/errorprone/SearchResultsPrinter.java
@@ -17,10 +17,12 @@
 package com.google.errorprone;
 
 import com.sun.source.tree.Tree;
+import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.JCDiagnostic.DiagnosticPosition;
 import com.sun.tools.javac.util.Log;
 import com.sun.tools.javac.util.Pair;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,27 +33,26 @@ import javax.tools.JavaFileObject;
 */
 class SearchResultsPrinter implements MatchListener {
 
-  private final List<Pair<Tree, JavaFileObject>> matches =
-      new ArrayList<>();
+  private final List<Pair<Tree, JavaFileObject>> matches = new ArrayList<>();
   private JavaFileObject sourceFile;
+  private final Log log;
+
+  public SearchResultsPrinter(PrintWriter out) {
+    Context context = new Context();
+    context.put(Log.outKey, out);
+    ErrorProneCompiler.setupMessageBundle(context);
+    this.log = Log.instance(context);
+  }
 
   @Override
   public void onMatch(Tree tree) {
     matches.add(new Pair<Tree, JavaFileObject>(tree, sourceFile));
   }
 
-  public void printMatches(Log log) {
+  public void printMatches() {
     for (Pair<Tree, JavaFileObject> match : matches) {
-      JavaFileObject originalSource;
-      // Swap the log's source and the current file's source; then be sure to swap them back later.
-      originalSource = log.useSource(match.snd);
-      try {
-        log.note((DiagnosticPosition)match.fst, "searchresult", "Matched.");
-      } finally {
-        if (originalSource != null) {
-          log.useSource(originalSource);
-        }
-      }
+      log.useSource(match.snd);
+      log.note((DiagnosticPosition)match.fst, "searchresult", "Matched.");
     }
     log.note("searchresult.count", matches.size());
   }

core/src/main/java/com/google/errorprone/SearchingJavaCompiler.java
@@ -1,94 +1,0 @@
-/*
- * Copyright 2011 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.google.errorprone;
-
-import com.sun.tools.javac.comp.AttrContext;
-import com.sun.tools.javac.comp.Env;
-import com.sun.tools.javac.main.JavaCompiler;
-import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.Context.Factory;
-
-import java.util.Queue;
-
-/**
- * Look for matches, but only report the locations of the matches.
- * Useful for preliminary exploration of a potential problem, before
- * you've written any error messaging or suggested fixes.
- *
- * @author alexeagle@google.com (Alex Eagle)
- */
-public class SearchingJavaCompiler extends JavaCompiler {
-
-  private final ErrorProneAnalyzer errorProneAnalyzer;
-  private SearchResultsPrinter resultsPrinter;
-
-  public SearchingJavaCompiler(Context context) {
-    super(context);
-    if (context.get(Scanner.class) == null) {
-      throw new IllegalArgumentException("context must have a Scanner instance");
-    }
-
-    errorProneAnalyzer = new ErrorProneAnalyzer(log, context, resultsPrinter);
-    resultsPrinter = new SearchResultsPrinter();
-  }
-
-  /**
-   * Adds an initialization hook to the Context, such that each subsequent
-   * request for a JavaCompiler (i.e., a lookup for 'compilerKey' of our
-   * superclass, JavaCompiler) will actually construct and return our version.
-   * It's necessary since many new JavaCompilers may
-   * be requested for later stages of the compilation (annotation processing),
-   * within the same Context. And it's the preferred way for extending behavior
-   * within javac, per the documentation in {@link com.sun.tools.javac.util.Context}.
-   */
-  public static void preRegister(final Context context) {
-    final Scanner scanner = context.get(Scanner.class);
-    context.put(compilerKey, new Factory<JavaCompiler>() {
-      @Override
-      public JavaCompiler make(Context ctx) {
-        // Ensure that future processing rounds continue to use the same Scanner.
-        ctx.put(Scanner.class, scanner);
-        return new SearchingJavaCompiler(ctx);
-      }
-    });
-  }
-
-  @Override
-  protected void flow(Env<AttrContext> attrContextEnv, Queue<Env<AttrContext>> envs) {
-    super.flow(attrContextEnv, envs);
-    postFlow(attrContextEnv);
-  }
-
-  /**
-   * Run Error Prone analysis after performing dataflow checks.
-   */
-  public void postFlow(Env<AttrContext> env) {
-    errorProneAnalyzer.reportReadyForAnalysis(env, errorCount() > 0);
-  }
-
-  private boolean printed = false;
-
-  @Override
-  public void close(boolean disposeNames) {
-    if (!printed) {
-      resultsPrinter.printMatches(log);
-      printed = true;
-    }
-    super.close(disposeNames);
-  }
-
-}

commit 1f912d223295f55acca7456e46eea2a035e4629a

Fix for compilation units getting stuck at 'flow'
The regression was introduced in 40112e84, and prevented classes from
being emitted for compilation units that were desugared early (e.g.
because they were referenced from classes in an earlier compilation
unit).

This will be less bug-prone once we start using TaskListener instead of
overriding flow().

core/src/main/java/com/google/errorprone/ErrorProneJavacJavaCompiler.java
@@ -85,6 +85,7 @@ public class ErrorProneJavacJavaCompiler extends JavaCompiler {
   @Override
   protected void flow(Env<AttrContext> env, Queue<Env<AttrContext>> results) {
     if (compileStates.isDone(env, CompileState.FLOW)) {
+      super.flow(env, results);
       return;
     }
     super.flow(env, results);