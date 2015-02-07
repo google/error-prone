ca87db0b02c7996431c498df2195622326d9318e
Pedro Henrique B. T. Perez <pedrohbtp@gmail.com>
2015-02-07 20:47:32 -0200

    Added Functionality to annotate the calling stacks
    Now it counts how many passes and fails each calling stack has.

--- ContextTree/run.m
+++ ContextTree/run.m
@@ -1,8 +1,14 @@
 %%%% Runs a test on the contextTree function
 clear
 Calltree = contextTree('./Data/input.txt');
+
+%Given an empty stack structure, gives all the stacks in the Calltree stack
 stack = {};
 stacks = stackCall(stack, Calltree{1}, Calltree); %%Calltree{1} is the parent 
+
+%Given an empty stacks_stats array, it fills it
+stacks_stats = {};
+[ stacks_stats ] = passfailStats( stacks_stats, stacks, false );
 
 
 
