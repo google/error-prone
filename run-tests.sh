4653fdb13438870d3343dfc85e3b8ca8307a7496
Alan Litteneker <alan.litteneker@gmail.com>
2015-02-08 00:57:10 -0800

    Minor fixes, addition of class names to method prints, more reliable cct output

--- run-tests.sh
+++ run-tests.sh
@@ -2,37 +2,38 @@
 
 passcount=0
 failcount=0
+tmpresult="./core/instrument/cct/test_results.tmp"
+tmpcct="./core/instrument/cct/test_cct.tmp"
 
-while read line ;
+trap "exit" INT
+
+while read line;
 do
 
 echo "Executing: $line"
 
-temppath="./core/instrument/cct/$line.tmp"
+mvn -Dtest=$line -DfailIfNoTests=false surefire:test 2> $tmpcct 1> $tmpresult
 
-mvn -Dtest=$line -DfailIfNoTests=false surefire:test > $temppath
-
-cct=$( awk '/Tests run: 1/{f=0} f; /Running/{f=1}' < $temppath )
-
-result=$( grep "FAILED" $temppath )
+result=$( grep "FAILED" $tmpresult )
 if [ -z $result ]
 then
 
-echo -e "$cct" > ./core/instrument/cct/pass/$passcount.cct
+mv $tmpcct ./core/instrument/cct/pass/$passcount.cct
 echo "Passed ($passcount.cct)"
 passcount=$((passcount+1))
 
 else
 
-echo -e "$cct" > ./core/instrument/cct/fail/$failcount.cct
+mv $tmpcct ./core/instrument/cct/fail/$failcount.cct
 echo "Failed ($failcount.cct)"
 failcount=$((failcount+1))
 
 fi 
 
-rm $temppath
-
 done < ./core/instrument/testnames.txt
+
+rm $tmpresult
+rm $tmpcct
 
 echo "Done. Wrote all calling context trees to ./core/instrument/cct/"
 echo "$passcount tests passed, $failcount tests failed"
