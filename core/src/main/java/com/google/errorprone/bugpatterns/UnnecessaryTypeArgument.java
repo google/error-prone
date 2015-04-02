1e5b4280cea2bfa0ea2e0205fa27c764d983ec70
cushon <cushon@google.com>
2015-04-02 11:41:47 -0700

    Add a check for non-generic methods that are invoked with type arguments
    RELNOTES: Add a check for non-generic methods that are invoked with type arguments
    
    Fixes #307
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=90181044

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/UnnecessaryTypeArgument.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.common.base.Verify.verify;
+import static com.google.errorprone.BugPattern.Category.JDK;
+import static com.google.errorprone.BugPattern.MaturityLevel.MATURE;
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.NewClassTreeMatcher;
+import com.google.errorprone.fixes.Fix;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.util.ASTHelpers;
+
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Symbol.MethodSymbol;
+import com.sun.tools.javac.tree.JCTree;
+
+import java.util.List;
+
+/**
+ * @author cushon@google.com (Liam Miller-Cushon)
+ */
+@BugPattern(name = "UnnecessaryTypeArgument",
+    summary = "Non-generic methods should not be invoked with type arguments",
+    category = JDK, severity = ERROR, maturity = MATURE)
+public class UnnecessaryTypeArgument extends BugChecker
+    implements MethodInvocationTreeMatcher, NewClassTreeMatcher {
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    return check(tree, tree.getTypeArguments(), state);
+  }
+
+  @Override
+  public Description matchNewClass(NewClassTree tree, VisitorState state) {
+    return check(tree, tree.getTypeArguments(), state);
+  }
+
+  private Description check(Tree tree, List<? extends Tree> arguments, VisitorState state) {
+    Symbol sym = ASTHelpers.getSymbol(tree);
+    if (!(sym instanceof MethodSymbol)) {
+      return Description.NO_MATCH;
+    }
+    MethodSymbol methodSymbol = (MethodSymbol) sym;
+
+    for (MethodSymbol superMethod : ASTHelpers.findSuperMethods(methodSymbol, state.getTypes())) {
+      if (!superMethod.getTypeParameters().isEmpty()) {
+        // Exempt methods that override generic methods to preserve the substitutability of the
+        // two types.
+        return Description.NO_MATCH;  
+      }
+    }
+    
+    int expected = methodSymbol.getTypeParameters().size();
+    int actual = arguments.size();
+
+    if (actual <= expected) {
+      return Description.NO_MATCH;
+    }
+
+    return describeMatch(tree, buildFix(tree, arguments, state));
+  }
+
+  /**
+   * Constructor a fix that deletes the set of type arguments.
+   */
+  private Fix buildFix(Tree tree, List<? extends Tree> arguments, VisitorState state) {
+
+    JCTree node = (JCTree) tree;
+    int startAbsolute = node.getStartPosition();
+    int lower = ((JCTree) arguments.get(0)).getStartPosition() - startAbsolute;
+    int upper = state.getEndPosition((JCTree) arguments.get(arguments.size() - 1)) - startAbsolute;
+
+    CharSequence source = state.getSourceForNode(node);
+
+    while (lower >= 0 && source.charAt(lower) != '<') {
+      lower--;
+    }
+    while (upper < source.length() && source.charAt(upper) != '>') {
+      upper++;
+    }
+
+    // There's a small chance that the fix will be incorrect because there's a '<' or '>' in
+    // a comment (e.g. `this.</*<*/T/*>*/>f()`), it should never be the case that we don't find
+    // any angle brackets.
+    verify(source.charAt(lower) == '<' && source.charAt(upper) == '>');
+
+    Fix fix = SuggestedFix.replace(startAbsolute + lower, startAbsolute + upper + 1, "");
+    return fix;
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/bugpatterns/UnnecessaryTypeArgumentTest.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+
+/** {@link UnnecessaryTypeArgument}Test */
+@RunWith(JUnit4.class)
+public class UnnecessaryTypeArgumentTest {
+
+  private CompilationTestHelper compilationHelper;
+
+  @Before
+  public void setUp() {
+    compilationHelper = CompilationTestHelper.newInstance(new UnnecessaryTypeArgument());
+  }
+
+  @Test
+  public void positiveCall() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(
+        compilationHelper.fileManager().forSourceLines("Test.java",
+            "package foo.bar;",
+            "class Test {",
+            "  void f() {}",
+            "  void m() {",
+            "    // BUG: Diagnostic contains: this.f()",
+            "    this.<String>f();",
+            "  }",
+            "}")
+    );
+  }
+
+  @Test
+  public void positiveThis() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(
+        compilationHelper.fileManager().forSourceLines("Test.java",
+            "package foo.bar;",
+            "class Test {",
+            "  static class C {",
+            "    public <T> C() {",
+            "      // BUG: Diagnostic contains: /*START*/this(42)",
+            "      /*START*/<String>this(42);",
+            "    }",
+            "    public C(int i) {",
+            "    }",
+            "  }",
+            "}")
+    );
+  }
+
+  @Test
+  public void positiveSuper() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(
+        compilationHelper.fileManager().forSourceLines("Test.java",
+            "package foo.bar;",
+            "class Test {",
+            "  static class B {",
+            "    public B() {",
+            "    }",
+            "  }",
+            "  static class C extends B {",
+            "    public <T> C() {",
+            "      // BUG: Diagnostic contains: /*START*/super()",
+            "      /*START*/<String>super();",
+            "    }",
+            "  }",
+            "}")
+    );
+  }
+
+  @Test
+  public void positiveInstantiation() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(
+        compilationHelper.fileManager().forSourceLines("Test.java",
+            "package foo.bar;",
+            "class Test {",
+            "  static class C {",
+            "    public C() {}",
+            "  }",
+            "  void m() {",
+            "    // BUG: Diagnostic contains: new C()",
+            "    new <String, Integer>C();",
+            "  }",
+            "}")
+    );
+  }
+
+  @Test
+  public void negative() throws Exception {
+    compilationHelper.assertCompileSucceeds(
+        compilationHelper.fileManager().forSourceLines("Test.java",
+            "package foo.bar;",
+            "class Test {",
+            "  <T> void f() {}",
+            "  void m() {",
+            "    this.<String>f();",
+            "  }",
+            "}")
+    );
+  }
+  
+  @Test
+  public void negativeGenericSuper() throws Exception {
+    compilationHelper.assertCompileSucceeds(
+        Arrays.asList(
+            compilationHelper.fileManager().forSourceLines("Super.java",
+                "public class Super {",
+                "  public <T> T f(T x) { return x; }",
+                "}"),
+            compilationHelper.fileManager().forSourceLines("Sub.java",
+                "@SuppressWarnings(\"unchecked\")",
+                "public class Sub extends Super {",
+                "  public Object f(Object x) { return x; }",
+                "}"),
+            compilationHelper.fileManager().forSourceLines("Test.java",
+                "public class Test {",
+                "  void m(Sub s) {",
+                "    s.<String>f(null);",
+                "  }",
+                "}")));
+  }
+  
+  @Test
+  public void whitespaceFix() throws Exception {
+    compilationHelper.assertCompileFailsWithMessages(
+        compilationHelper.fileManager().forSourceLines("Test.java",
+            "package foo.bar;",
+            "class Test {",
+            "  void f() {}",
+            "  void m() {",
+            "    // BUG: Diagnostic contains: this.f()",
+            "    this.<  /*  */ String  /*  */ >f();",
+            "  }",
+            "}")
+    );
+  }
+}
+
