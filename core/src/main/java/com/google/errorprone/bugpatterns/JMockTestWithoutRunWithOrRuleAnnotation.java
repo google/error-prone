5b393acb32810bc45e960a5f83f51194d7237a37
tom-smalls <tommckee@gmail.com>
2015-04-17 14:12:08 +0100

    Create a checker to ensure that a jMock Mockery is assigned as a JUnit rule or uses the jMock test runner.

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/JMockTestWithoutRunWithOrRuleAnnotation.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.Category.JMOCK;
+import static com.google.errorprone.BugPattern.MaturityLevel.EXPERIMENTAL;
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.AT_LEAST_ONE;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.classLiteral;
+import static com.google.errorprone.matchers.Matchers.enclosingClass;
+import static com.google.errorprone.matchers.Matchers.hasAnnotation;
+import static com.google.errorprone.matchers.Matchers.hasArgumentWithValue;
+import static com.google.errorprone.matchers.Matchers.isField;
+import static com.google.errorprone.matchers.Matchers.isSameType;
+import static com.google.errorprone.matchers.Matchers.isSubtypeOf;
+import static com.google.errorprone.matchers.Matchers.not;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.VariableTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.JUnitMatchers;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.VariableTree;
+
+@BugPattern(name = "JMockTestWithoutRunWithOrRuleAnnotation",
+    summary = "jMock tests must have a @RunWith(JMock.class) annotation, or the Mockery field must "
+        + "have a @Rule JUnit annotation",
+    explanation = "jMock tests must have a @RunWith(JMock.class) annotation, or the Mockery field "
+        + "must have a @Rule JUnit annotation. If this is not done, then all of your jMock tests "
+        + "will run and pass, but none of your assertions will actually be evaluated. Your tests "
+        + "will pass even if they shouldn't.",
+    category = JMOCK, severity = ERROR, maturity = EXPERIMENTAL)
+public class JMockTestWithoutRunWithOrRuleAnnotation extends BugChecker
+    implements VariableTreeMatcher {
+
+  private static final String JMOCK_TEST_RUNNER_CLASS = "org.jmock.integration.junit4.JMock";
+
+  private static final Matcher<VariableTree> fieldIsMockery = allOf(
+      isSubtypeOf("org.jmock.Mockery"), 
+      isField());
+  
+  private static final Matcher<VariableTree> fieldHasRuleAnnotation =
+      hasAnnotation("org.junit.Rule");
+  
+  private static final Matcher<Tree> enclosingClassRunsWithJMockTestRunner =
+      enclosingClass(allOf(
+          hasAnnotation(JUnitMatchers.JUNIT4_RUN_WITH_ANNOTATION),
+          Matchers.<ClassTree>annotations(
+              AT_LEAST_ONE,
+              hasArgumentWithValue("value", classLiteral(isSameType(JMOCK_TEST_RUNNER_CLASS))))));
+  
+  private static final Matcher<VariableTree> BUG_PATTERN_MATCHER = allOf(
+      fieldIsMockery,
+      not(anyOf(fieldHasRuleAnnotation, enclosingClassRunsWithJMockTestRunner)));
+
+  @Override
+  public Description matchVariable(VariableTree tree, VisitorState state) {
+    if (BUG_PATTERN_MATCHER.matches(tree, state)) {
+      return describeMatch(tree);
+    }
+    return Description.NO_MATCH;
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/bugpatterns/JMockTestWithoutRunWithOrRuleAnnotationTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class JMockTestWithoutRunWithOrRuleAnnotationTest {
+
+  private CompilationTestHelper compilationTestHelper;
+
+  @Before
+  public void setup() {
+    compilationTestHelper = CompilationTestHelper.newInstance(
+        new JMockTestWithoutRunWithOrRuleAnnotation(), getClass());
+  }
+  
+  @Test
+  public void testShouldFlagNoRuleAndNoRunWith() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.Mockery;", 
+            "public class Test {", 
+            "  // BUG: Diagnostic contains:", 
+            "  private final Mockery mockery = new Mockery();", 
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldFlagWrongRunWith() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.Mockery;", 
+            "import org.junit.runner.RunWith;", 
+            "import org.junit.runners.Parameterized;", 
+            "@RunWith(Parameterized.class)", 
+            "public class Test {", 
+            "  // BUG: Diagnostic contains:", 
+            "  private final Mockery mockery = new Mockery();", 
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldFlagJUnitRuleMockery() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.integration.junit4.JUnitRuleMockery;", 
+            "public class Test {", 
+            "  // BUG: Diagnostic contains:", 
+            "  public final JUnitRuleMockery mockery = new JUnitRuleMockery();",
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldNotFlagWithRuleAnnotation() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.Mockery;", 
+            "import org.jmock.integration.junit4.JUnitRuleMockery;", 
+            "import org.junit.Rule;", 
+            "public class Test {", 
+            "  @Rule", 
+            "  private final Mockery mockery = new Mockery();", 
+            "  @Rule", 
+            "  public final JUnitRuleMockery mockery2 = new JUnitRuleMockery();", 
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldNotFlagWithRuleAnnotationAndWrongRunWith() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.Mockery;", 
+            "import org.jmock.integration.junit4.JUnitRuleMockery;", 
+            "import org.junit.Rule;", 
+            "import org.junit.runner.RunWith;", 
+            "import org.junit.runners.Parameterized;", 
+            "@RunWith(Parameterized.class)", 
+            "public class Test {", 
+            "  @Rule", 
+            "  private final Mockery mockery = new Mockery();", 
+            "  @Rule", 
+            "  public final JUnitRuleMockery mockery2 = new JUnitRuleMockery();", 
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldNotFlagRunWithJMock() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.Mockery;", 
+            "import org.jmock.integration.junit4.JMock;", 
+            "import org.jmock.integration.junit4.JUnitRuleMockery;", 
+            "import org.junit.runner.RunWith;", 
+            "@RunWith(JMock.class)", 
+            "public class Test {", 
+            "  private final Mockery mockery = new org.jmock.Mockery();", 
+            "  public final JUnitRuleMockery mockery2 = new JUnitRuleMockery();", 
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldNotFlagWithRuleAnnotationAndRunWithJMock() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.jmock.Mockery;", 
+            "import org.jmock.integration.junit4.JMock;", 
+            "import org.jmock.integration.junit4.JUnitRuleMockery;", 
+            "import org.junit.Rule;", 
+            "import org.junit.runner.RunWith;", 
+            "@RunWith(JMock.class)", 
+            "public class Test {", 
+            "  @Rule", 
+            "  private final Mockery mockery = new Mockery();", 
+            "  @Rule", 
+            "  public final JUnitRuleMockery mockery2 = new JUnitRuleMockery();", 
+            "}")
+        .doTest();
+  }
+  
+  @Test
+  public void testShouldNotFlagSingleFieldWithRuleAnnotation() {
+    compilationTestHelper
+        .addSourceLines("Test.java",
+            "import org.junit.Rule;", 
+            "import org.jmock.Mockery;", 
+            "public class Test {", 
+            "  @Rule", 
+            "  final Mockery mockery = new Mockery();", 
+            "}")
+        .doTest();
+  }
+}
