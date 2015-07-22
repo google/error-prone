97efa36c6e283aa8e7373ad6ea7f0b698a05bed5
cushon <cushon@google.com>
2015-07-22 15:04:07 -0700

    Remove questionable handling of JCFieldAccess from ASTHelpers
    RELNOTES: N/A
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=98874704

--- core/src/main/java/com/google/errorprone/util/ASTHelpers.java
+++ core/src/main/java/com/google/errorprone/util/ASTHelpers.java
@@ -470,13 +470,10 @@ public class ASTHelpers {
    * {@link VariableTree#getType()} or {@link MethodTree#getReturnType()}.
    */
   public static Type getType(Tree tree) {
-    if (tree instanceof JCFieldAccess) {
-      return ((JCFieldAccess) tree).sym.type;
-    } else if (tree instanceof JCTree) {
+    if (tree instanceof JCTree) {
       return ((JCTree) tree).type;
-    } else {
-      return null;
     }
+    return null;
   }
 
   public static String getAnnotationName(AnnotationTree tree) {
