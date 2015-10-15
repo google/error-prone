fdb6a0c08009580b662106a7301556bf6cabec9d
mwacker <mwacker@google.com>
2015-10-15 15:18:30 -0700

    Improve documentation for JUnitAmbiguousTestClass
    RELNOTES: none
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=105548039

--- core/src/main/java/com/google/errorprone/bugpatterns/JUnitAmbiguousTestClass.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/JUnitAmbiguousTestClass.java
@@ -37,10 +37,15 @@ import com.sun.source.tree.ClassTree;
  */
 @BugPattern(
     name = "JUnitAmbiguousTestClass",
-    summary = "Test class mixes JUnit 3 and JUnit 4 idioms",
-    explanation = "The test class could execute either as a JUnit 3 class or a JUnit 4 class, "
-        + "and tests could behave differently depending on whether it runs in JUnit 3 or JUnit 4. "
-        + "In particular, as a JUnit 4 class, it may not work as intended.",
+    summary = "Test class inherits from JUnit 3's TestCase but has JUnit 4 @Test annotations.",
+    explanation = "JUnit 3 uses inheritance, whereas JUnit 4 uses composition. "
+        + "Mixing these two design patterns historically has been a source of test bugs and "
+        + "unexpected behavior (e.g.: teardown logic and/or verification does not run). "
+        + "Error Prone also cannot infer whether the test class runs with JUnit 3 or JUnit 4. "
+        + "Thus, even if the test class runs with JUnit 4, Error Prone will not run "
+        + "additional checks which can catch common errors with JUnit 4 test classes. "
+        + "Either use only JUnit 4 and remove the inheritance from TestCase, "
+        + "or use only JUnit 3 and remove the @Test annotations. ",
     category = JUNIT, maturity = MATURE, severity = WARNING)
 public class JUnitAmbiguousTestClass extends BugChecker implements ClassTreeMatcher {
 
