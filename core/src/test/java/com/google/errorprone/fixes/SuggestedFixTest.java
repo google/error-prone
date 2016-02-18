1585dba1fd11121067fe821c08f59aa4dc93b705
cushon <cushon@google.com>
2016-02-17 16:31:25 -0800

    Fix path of SuggestedFixTest
    RELNOTES: N/A
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=114912156

--- core/src/test/java/com/google/errorprone/fixes/SuggestedFixTest.java
+++ core/src/test/java/com/google/errorprone/fixes/SuggestedFixTest.java
@@ -16,6 +16,8 @@
 
 package com.google.errorprone.fixes;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import com.google.common.base.Verify;
 import com.google.common.collect.ImmutableMap;
 import com.google.errorprone.BugPattern;
@@ -25,16 +27,22 @@ import com.google.errorprone.BugPattern.SeverityLevel;
 import com.google.errorprone.CompilationTestHelper;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.ReturnTreeMatcher;
 import com.google.errorprone.bugpatterns.BugChecker.VariableTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.util.ASTHelpers;
 
 import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ReturnTree;
 import com.sun.source.tree.VariableTree;
 
+import com.sun.tools.javac.code.Type;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.lang.annotation.Retention;
 
 import javax.lang.model.element.Modifier;
 
@@ -42,6 +50,7 @@ import javax.lang.model.element.Modifier;
 @RunWith(JUnit4.class)
 public class SuggestedFixTest {
 
+  @Retention(RUNTIME)
   public @interface EditModifiers {
     String value() default "";
 
@@ -147,6 +156,86 @@ public class SuggestedFixTest {
             "  @Nullable public final Object three = null;",
             "  // BUG: Diagnostic contains: public Object four",
             "  public final Object four = null;",
+            "}")
+        .doTest();
+  }
+
+  @Retention(RUNTIME)
+  public @interface TypeToCast {
+    String value();
+  }
+
+  @BugPattern(
+    category = Category.ONE_OFF,
+    maturity = MaturityLevel.EXPERIMENTAL,
+    name = "CastReturn",
+    severity = SeverityLevel.ERROR,
+    summary = "Adds casts to returned expressions"
+  )
+  public static class CastReturn extends BugChecker implements ReturnTreeMatcher {
+
+    @Override
+    public Description matchReturn(ReturnTree tree, VisitorState state) {
+      if (tree.getExpression() == null) {
+        return Description.NO_MATCH;
+      }
+      TypeToCast typeToCast =
+          ASTHelpers.getAnnotation(
+              ASTHelpers.findEnclosingNode(state.getPath(), MethodTree.class), TypeToCast.class);
+      SuggestedFix.Builder fixBuilder = SuggestedFix.builder();
+      Type type = state.getTypeFromString(typeToCast.value());
+      Verify.verifyNotNull(type, "could not find type: %s", typeToCast.value());
+      String qualifiedTargetType = SuggestedFix.qualifyType(state, fixBuilder, type.tsym);
+      fixBuilder.prefixWith(tree.getExpression(), String.format("(%s) ", qualifiedTargetType));
+      return describeMatch(tree, fixBuilder.build());
+    }
+  }
+
+  @Test
+  public void qualifiedName_Object() {
+    CompilationTestHelper.newInstance(CastReturn.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            String.format("import %s;", TypeToCast.class.getCanonicalName()),
+            "class Test {",
+            "  @TypeToCast(\"java.lang.Object\")",
+            "  Object f() {",
+            "    // BUG: Diagnostic contains: return (Object) null;",
+            "    return null;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void qualifiedName_imported() {
+    CompilationTestHelper.newInstance(CastReturn.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Map.Entry;",
+            String.format("import %s;", TypeToCast.class.getCanonicalName()),
+            "class Test {",
+            "  @TypeToCast(\"java.util.Map.Entry\")",
+            "  Object f() {",
+            "    // BUG: Diagnostic contains: return (Entry) null;",
+            "    return null;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void qualifiedName_notImported() {
+    CompilationTestHelper.newInstance(CastReturn.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            String.format("import %s;", TypeToCast.class.getCanonicalName()),
+            "class Test {",
+            "  @TypeToCast(\"java.util.Map.Entry\")",
+            "  Object f() {",
+            "    // BUG: Diagnostic contains: return (Map.Entry) null;",
+            "    return null;",
+            "  }",
             "}")
         .doTest();
   }
