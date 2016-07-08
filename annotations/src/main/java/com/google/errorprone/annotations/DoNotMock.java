a5d598e4c4142d181c7ea111650a8d10d7a72071
arielb <arielb@google.com>
2016-07-08 14:46:39 -0700

    Make @DoNotMock inherited
    MOE_MIGRATED_REVID=126949888

--- annotations/src/main/java/com/google/errorprone/annotations/DoNotMock.java
+++ annotations/src/main/java/com/google/errorprone/annotations/DoNotMock.java
@@ -18,16 +18,15 @@ package com.google.errorprone.annotations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({
-  ElementType.TYPE,
-  ElementType.ANNOTATION_TYPE
-})
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 public @interface DoNotMock {
   /**
    * The reason why the annotated type should not be mocked.
