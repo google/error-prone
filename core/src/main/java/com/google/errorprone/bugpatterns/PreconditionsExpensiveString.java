b41d11442eb441cdcbe6bb54220af204c63a5301
cushon <cushon@google.com>
2016-07-12 15:47:40 -0700

    Add a test for PreconditionsExpensiveString
    and update the check to include a suggested fix.
    
    MOE_MIGRATED_REVID=127252263

--- core/src/main/java/com/google/errorprone/bugpatterns/PreconditionsExpensiveString.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/PreconditionsExpensiveString.java
@@ -24,72 +24,80 @@ import static com.google.errorprone.matchers.Matchers.anyOf;
 import static com.google.errorprone.matchers.Matchers.argument;
 import static com.google.errorprone.matchers.Matchers.staticMethod;
 
+import com.google.common.collect.Iterables;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.Matchers;
-
+import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.LiteralTree;
-import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.Tree.Kind;
-
-import java.util.List;
+import com.sun.tools.javac.tree.JCTree;
 import java.util.regex.Pattern;
 
 /**
- * Error checker for calls to the Preconditions class in Guava which use
- * 'expensive' methods of producing the error string. In most cases, users are
- * better off using the equivalent methods which defer the computation of the
- * string until the test actually fails.
+ * Error checker for calls to the Preconditions class in Guava which use 'expensive' methods of
+ * producing the error string. In most cases, users are better off using the equivalent methods
+ * which defer the computation of the string until the test actually fails.
  *
  * @author sjnickerson@google.com (Simon Nickerson)
  */
-@BugPattern(name = "PreconditionsErrorMessageEagerEvaluation",
-    summary = "Second argument to Preconditions.* is a call to String.format(), which " +
-        "can be unwrapped",
-    explanation =
-        "Preconditions checks take an error message to display if the check fails. " +
-        "The error message is rarely needed, so it should either be cheap to construct " +
-        "or constructed only when needed. This check ensures that these error messages " +
-        "are not constructed using expensive methods that are evaluated eagerly.",
-    category = GUAVA, severity = WARNING, maturity = EXPERIMENTAL)
-public class PreconditionsExpensiveString
-    extends BugChecker implements MethodInvocationTreeMatcher {
+@BugPattern(
+  name = "PreconditionsErrorMessageEagerEvaluation",
+  summary =
+      "Second argument to Preconditions.* is a call to String.format(), which "
+          + "can be unwrapped",
+  explanation =
+      "Preconditions checks take an error message to display if the check fails. "
+          + "The error message is rarely needed, so it should either be cheap to construct "
+          + "or constructed only when needed. This check ensures that these error messages "
+          + "are not constructed using expensive methods that are evaluated eagerly.",
+  category = GUAVA,
+  severity = WARNING,
+  maturity = EXPERIMENTAL
+)
+public class PreconditionsExpensiveString extends BugChecker
+    implements MethodInvocationTreeMatcher {
 
   @SuppressWarnings({"vararg", "unchecked"})
-  private static final Matcher<MethodInvocationTree> matcher = allOf(
-      anyOf(
-          staticMethod().onClass("com.google.common.base.Preconditions").named("checkNotNull"),
-          staticMethod().onClass("com.google.common.base.Preconditions").named("checkState"),
-          staticMethod().onClass("com.google.common.base.Preconditions").named("checkArgument")),
-      argument(1, Matchers.<ExpressionTree>allOf(
-          Matchers.<ExpressionTree>kindIs(Kind.METHOD_INVOCATION),
-          staticMethod().onClass("java.lang.String").named("format"),
-          new StringFormatCallContainsNoSpecialFormattingMatcher(Pattern.compile("%[^%s]"))
-      ))
-  );
+  private static final Matcher<MethodInvocationTree> matcher =
+      allOf(
+          anyOf(
+              staticMethod().onClass("com.google.common.base.Preconditions").named("checkNotNull"),
+              staticMethod().onClass("com.google.common.base.Preconditions").named("checkState"),
+              staticMethod()
+                  .onClass("com.google.common.base.Preconditions")
+                  .named("checkArgument")),
+          argument(
+              1,
+              Matchers.allOf(
+                  Matchers.<ExpressionTree>kindIs(Kind.METHOD_INVOCATION),
+                  staticMethod().onClass("java.lang.String").named("format"),
+                  new StringFormatCallContainsNoSpecialFormattingMatcher(
+                      Pattern.compile("%[^%s]")))));
 
   @Override
-  public Description matchMethodInvocation(MethodInvocationTree methodInvocationTree, VisitorState state) {
-    if (!matcher.matches(methodInvocationTree, state)) {
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (!matcher.matches(tree, state)) {
       return Description.NO_MATCH;
     }
-
-    MemberSelectTree method =
-        (MemberSelectTree) methodInvocationTree.getMethodSelect();
-
-    List<? extends ExpressionTree> arguments =
-        methodInvocationTree.getArguments();
-    MethodInvocationTree stringFormat = (MethodInvocationTree) arguments.get(1);
-
-    // TODO(sjnickerson): Figure out how to get a suggested fix. Basically we
-    // remove the String.format() wrapper, but I don't know how to express
-    // this.
-    return describeMatch(arguments.get(1));
+    MethodInvocationTree stringFormat = (MethodInvocationTree) tree.getArguments().get(1);
+    return describeMatch(
+        stringFormat,
+        SuggestedFix.builder()
+            .replace(
+                ((JCTree) stringFormat).getStartPosition(),
+                ((JCTree) stringFormat.getArguments().get(0)).getStartPosition(),
+                "")
+            .replace(
+                state.getEndPosition((JCTree) Iterables.getLast(stringFormat.getArguments())),
+                state.getEndPosition((JCTree) stringFormat),
+                "")
+            .build());
   }
 
   private static class StringFormatCallContainsNoSpecialFormattingMatcher
@@ -97,25 +105,21 @@ public class PreconditionsExpensiveString
 
     private final Pattern invalidFormatCharacters;
 
-    StringFormatCallContainsNoSpecialFormattingMatcher(
-        Pattern invalidFormatCharacters) {
+    StringFormatCallContainsNoSpecialFormattingMatcher(Pattern invalidFormatCharacters) {
       this.invalidFormatCharacters = invalidFormatCharacters;
     }
 
     @Override
-    public boolean matches(ExpressionTree t, VisitorState state) {
-      if (!(t instanceof MethodInvocationTree)) {
+    public boolean matches(ExpressionTree tree, VisitorState state) {
+      if (!(tree instanceof MethodInvocationTree)) {
         return false;
       }
-      MethodInvocationTree stringFormatInvocation = (MethodInvocationTree) t;
-      if (!(stringFormatInvocation.getArguments().get(0)
-          instanceof LiteralTree)) {
+      String formatString =
+          ASTHelpers.constValue(((MethodInvocationTree) tree).getArguments().get(0), String.class);
+      if (formatString == null) {
         return false;
       }
-      LiteralTree firstArg = (LiteralTree)
-          stringFormatInvocation.getArguments().get(0);
-      String literal = firstArg.getValue().toString();
-      return !invalidFormatCharacters.matcher(literal).find();
+      return !invalidFormatCharacters.matcher(formatString).find();
     }
   }
 }
