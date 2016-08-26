5b3008cbcd2b35416ec74b1bd55b3b8804532691
glorioso <glorioso@google.com>
2016-08-26 15:51:51 -0700

    Adjust InvalidTargetingOnScopingAnnotation to only flag instances where there's a Target without METHOD and TYPE in its element list.
    MOE_MIGRATED_REVID=131451224

--- core/src/main/java/com/google/errorprone/bugpatterns/inject/InvalidTargetingOnScopingAnnotation.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/inject/InvalidTargetingOnScopingAnnotation.java
@@ -14,41 +14,54 @@
 
 package com.google.errorprone.bugpatterns.inject;
 
+import static com.google.common.collect.Sets.immutableEnumSet;
 import static com.google.errorprone.BugPattern.Category.INJECT;
 import static com.google.errorprone.BugPattern.MaturityLevel.EXPERIMENTAL;
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.AT_LEAST_ONE;
 import static com.google.errorprone.matchers.InjectMatchers.GUICE_SCOPE_ANNOTATION;
 import static com.google.errorprone.matchers.InjectMatchers.JAVAX_SCOPE_ANNOTATION;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.annotations;
 import static com.google.errorprone.matchers.Matchers.anyOf;
 import static com.google.errorprone.matchers.Matchers.hasAnnotation;
+import static com.google.errorprone.matchers.Matchers.isType;
+import static com.google.errorprone.matchers.Matchers.kindIs;
+import static com.google.errorprone.util.ASTHelpers.getAnnotation;
+import static com.sun.source.tree.Tree.Kind.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableSet;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.ClassTreeMatcher;
+import com.google.errorprone.fixes.Fix;
 import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.fixes.SuggestedFix.Builder;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
-import com.google.errorprone.util.ASTHelpers;
+import com.google.errorprone.matchers.MultiMatcher;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ClassTree;
-import com.sun.tools.javac.code.Attribute.Compound;
-import com.sun.tools.javac.code.Flags;
-import com.sun.tools.javac.code.Symbol;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
 
-/**
- * @author sgoldfeder@google.com (Steven Goldfeder)
- */
+/** @author sgoldfeder@google.com (Steven Goldfeder) */
 @BugPattern(
   name = "InjectInvalidTargetingOnScopingAnnotation",
-  summary = "The target of a scoping annotation must be set to METHOD and/or TYPE.",
+  summary = "A scoping annotation's Target should include TYPE and METHOD.",
   explanation =
-      "Scoping annotations are only appropriate for provision and therefore are only "
-          + "appropriate on @Provides methods and classes that will be provided just-in-time.",
+      "`@Scope` annotations should be applicable to TYPE (annotating classes that should"
+          + " be scoped) and to METHOD (annotating `@Provides` methods to apply scoping to the"
+          + " returned object.\n\n"
+          + " If an annotation's use is restricted by `@Target` and it doesn't include those two"
+          + " element types, the annotation can't be used where it should be able to be used.",
   category = INJECT,
   severity = ERROR,
   maturity = EXPERIMENTAL
@@ -57,59 +70,48 @@ public class InvalidTargetingOnScopingAnnotation extends BugChecker implements C
 
   private static final String TARGET_ANNOTATION = "java.lang.annotation.Target";
 
-  /** Matches classes that are annotated with @Scope or @ScopeAnnotation. */
-  private static final Matcher<ClassTree> SCOPE_ANNOTATION_MATCHER =
-      anyOf(hasAnnotation(GUICE_SCOPE_ANNOTATION), hasAnnotation(JAVAX_SCOPE_ANNOTATION));
+  private static final MultiMatcher<ClassTree, AnnotationTree> HAS_TARGET_ANNOTATION =
+      annotations(AT_LEAST_ONE, isType(TARGET_ANNOTATION));
+
+  private static final Matcher<ClassTree> ANNOTATION_WITH_SCOPE_AND_TARGET =
+      allOf(
+          kindIs(ANNOTATION_TYPE),
+          anyOf(hasAnnotation(GUICE_SCOPE_ANNOTATION), hasAnnotation(JAVAX_SCOPE_ANNOTATION)),
+          HAS_TARGET_ANNOTATION);
+
+  private static final ImmutableSet<ElementType> REQUIRED_ELEMENT_TYPES =
+      immutableEnumSet(TYPE, METHOD);
 
   @Override
   public final Description matchClass(ClassTree classTree, VisitorState state) {
-    Symbol classSymbol = ASTHelpers.getSymbol(classTree);
-    if ((classSymbol.flags() & Flags.ANNOTATION) != 0
-        && SCOPE_ANNOTATION_MATCHER.matches(classTree, state)) {
-      Target target = ASTHelpers.getAnnotation(classSymbol, Target.class);
-      boolean hasExclusivelyTypeAndOrMethodTargeting = false;
-      if (target != null) {
-        for (ElementType elementType : target.value()) {
-          if (elementType != METHOD && elementType != TYPE) {
-            return describe(classTree, state);
-          } else if (elementType == METHOD || elementType == TYPE) {
-            hasExclusivelyTypeAndOrMethodTargeting = true;
-          }
-        }
-      }
-      if (!hasExclusivelyTypeAndOrMethodTargeting) { // true for no target set and for @Target({})
-        return describe(classTree, state);
+    if (ANNOTATION_WITH_SCOPE_AND_TARGET.matches(classTree, state)) {
+      AnnotationTree targetTree = HAS_TARGET_ANNOTATION.getMatchingNodes().get(0);
+      Target target = getAnnotation(classTree, Target.class);
+      if (target != null
+          && // Unlikely to occur, but just in case Target isn't on the classpath.
+          !Arrays.asList(target.value()).containsAll(REQUIRED_ELEMENT_TYPES)) {
+        return describeMatch(targetTree, replaceTargetAnnotation(target, targetTree));
       }
     }
     return Description.NO_MATCH;
   }
 
-  public Description describe(ClassTree classTree, VisitorState state) {
-    Compound target =
-        ASTHelpers.getSymbol(classTree).attribute(state.getSymbolFromString(TARGET_ANNOTATION));
-    if (target == null) {
-      return describeMatch(
-          classTree,
-          SuggestedFix.builder()
-              .addImport("java.lang.annotation.Target")
-              .addStaticImport("java.lang.annotation.ElementType.TYPE")
-              .addStaticImport("java.lang.annotation.ElementType.METHOD")
-              .prefixWith(classTree, "@Target({TYPE, METHOD})\n")
-              .build());
-    }
-    AnnotationTree targetNode = null;
-    for (AnnotationTree annotation : classTree.getModifiers().getAnnotations()) {
-      if (ASTHelpers.getSymbol(annotation).equals(state.getSymbolFromString(TARGET_ANNOTATION))) {
-        targetNode = annotation;
-      }
-    }
-    return describeMatch(
-        targetNode,
+  /**
+   * Rewrite the annotation with static imports, adding TYPE and METHOD to the @Target annotation
+   * value (and reordering them to their declaration order in ElementType).
+   */
+  private Fix replaceTargetAnnotation(Target annotation, AnnotationTree targetAnnotationTree) {
+    Set<ElementType> types = EnumSet.copyOf(REQUIRED_ELEMENT_TYPES);
+    types.addAll(Arrays.asList(annotation.value()));
+
+    Builder builder =
         SuggestedFix.builder()
-            .addImport("java.lang.annotation.Target")
-            .addStaticImport("java.lang.annotation.ElementType.TYPE")
-            .addStaticImport("java.lang.annotation.ElementType.METHOD")
-            .replace(targetNode, "@Target({TYPE, METHOD})")
-            .build());
+            .replace(targetAnnotationTree, "@Target({" + Joiner.on(", ").join(types) + "})");
+
+    for (ElementType type : types) {
+      builder.addStaticImport("java.lang.annotation.ElementType." + type);
+    }
+
+    return builder.build();
   }
 }
