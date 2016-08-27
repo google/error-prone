d9f2ddc3d982646ff9e746ab9c97a40792ee64f9
avenet <avenet@google.com>
2016-08-26 17:24:29 -0700

    Improved the documentation for the InsecureCipherMode check. In particular, the process for requesting an exception through a security review is fully detailed.
    MOE_MIGRATED_REVID=131460147

--- core/src/main/java/com/google/errorprone/bugpatterns/InsecureCipherMode.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/InsecureCipherMode.java
@@ -35,16 +35,6 @@ import com.sun.tools.javac.tree.JCTree;
 @BugPattern(
   name = "InsecureCipherMode",
   summary = "Cipher.getInstance() is invoked using either the default settings or ECB mode",
-  explanation =
-      "This error is triggered when a Cipher instance is created using either"
-          + " the default settings or the notoriously insecure ECB mode. In particular, Java's"
-          + " default `Cipher.getInstance(\"AES\")` returns a cipher object that operates in ECB"
-          + " mode. Dynamically constructed transformation strings are also flagged, as they may"
-          + " conceal an instance of ECB mode. The problem with ECB mode is that encrypting the"
-          + " same block of plaintext always yields the same block of ciphertext. Hence,"
-          + " repetitions in the plaintext translate into repetitions in the ciphertext, which can"
-          + " be readily used to conduct cryptanalysis.\n\n"
-  ,
   category = JDK,
   severity = ERROR,
   documentSuppression = false,
