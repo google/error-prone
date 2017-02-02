79c7c91ce597ae48c29816edc60099bec14fa513
avenet <avenet@google.com>
2017-02-01 16:36:13 -0800

    Add the alias "InsecureCipherMode"
    (the former name of the check) to InsecureCryptoUsage.
    
    MOE_MIGRATED_REVID=146306442

--- core/src/main/java/com/google/errorprone/bugpatterns/InsecureCipherMode.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/InsecureCipherMode.java
@@ -34,6 +34,7 @@ import com.sun.tools.javac.tree.JCTree;
 /** @author avenet@google.com (Arnaud J. Venet) */
 @BugPattern(
   name = "InsecureCryptoUsage",
+  altNames = {"InsecureCipherMode"},
   summary = "A standard cryptographic operation is used in a mode that is prone to vulnerabilities",
   category = JDK,
   documentSuppression = false,
