9e2a977ea5078ce2e11fccaf76b980a647a536b8
cushon <cushon@google.com>
2017-02-06 18:15:32 -0800

    Add a utility for determining whether a statement definitely completes
    This can be used e.g. to detect switch statements that fall through.
    
    MOE_MIGRATED_REVID=146736965

---
+++ check_api/src/main/java/com/google/errorprone/util/Reachability.java
@@ -0,0 +1,367 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.util;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.getLast;
+import static java.util.Objects.requireNonNull;
+
+import com.sun.source.tree.AssertTree;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.BreakTree;
+import com.sun.source.tree.CaseTree;
+import com.sun.source.tree.CatchTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.ContinueTree;
+import com.sun.source.tree.DoWhileLoopTree;
+import com.sun.source.tree.EmptyStatementTree;
+import com.sun.source.tree.EnhancedForLoopTree;
+import com.sun.source.tree.ExpressionStatementTree;
+import com.sun.source.tree.ForLoopTree;
+import com.sun.source.tree.IfTree;
+import com.sun.source.tree.LabeledStatementTree;
+import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.SwitchTree;
+import com.sun.source.tree.SynchronizedTree;
+import com.sun.source.tree.ThrowTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TryTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.tree.WhileLoopTree;
+import com.sun.source.util.SimpleTreeVisitor;
+import com.sun.tools.javac.tree.JCTree;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+/** An implementation of JLS 14.21 reachability. */
+public class Reachability {
+
+  /** Returns true if the given statement can complete normally, as defined by JLS 14.21. */
+  public static boolean canCompleteNormally(StatementTree statement) {
+    return statement.accept(new CanCompleteNormallyVisitor(), null);
+  }
+
+  private static class CanCompleteNormallyVisitor extends SimpleTreeVisitor<Boolean, Void> {
+
+    /** Trees that are the target of a reachable break statement. */
+    private final Set<Tree> breaks = new HashSet<>();
+
+    /** Trees that are the target of a reachable continue statement. */
+    private final Set<Tree> continues = new HashSet<>();
+
+    boolean scan(List<? extends StatementTree> trees) {
+      boolean completes = true;
+      for (StatementTree tree : trees) {
+        // javac has already rejected any unreachable statements, so all statements
+        // except the final one should complete
+        checkState(completes);
+        completes = scan(tree);
+      }
+      return completes;
+    }
+
+    private boolean scan(Tree tree) {
+      return tree.accept(this, null);
+    }
+
+    /* A break statement cannot complete normally. */
+    @Override
+    public Boolean visitBreak(BreakTree tree, Void unused) {
+      breaks.add(requireNonNull(((JCTree.JCBreak) tree).target));
+      return false;
+    }
+
+    /* A continue statement cannot complete normally. */
+    @Override
+    public Boolean visitContinue(ContinueTree tree, Void unused) {
+      continues.add(requireNonNull(((JCTree.JCContinue) tree).target));
+      return false;
+    }
+
+    @Override
+    public Boolean visitBlock(BlockTree tree, Void unused) {
+      return scan(tree.getStatements());
+    }
+
+    /* A local class declaration statement can complete normally iff it is reachable. */
+    @Override
+    public Boolean visitClass(ClassTree tree, Void unused) {
+      return true;
+    }
+
+    /* A local variable declaration statement can complete normally iff it is reachable. */
+    @Override
+    public Boolean visitVariable(VariableTree tree, Void unused) {
+      return true;
+    }
+
+    /* An empty statement can complete normally iff it is reachable. */
+    @Override
+    public Boolean visitEmptyStatement(EmptyStatementTree tree, Void unused) {
+      return true;
+    }
+
+    @Override
+    public Boolean visitLabeledStatement(LabeledStatementTree tree, Void unused) {
+      // break/continue targets have already been resolved by javac, so
+      // there's nothing to do here
+      return scan(tree.getStatement());
+    }
+
+    /* An expression statement can complete normally iff it is reachable. */
+    @Override
+    public Boolean visitExpressionStatement(ExpressionStatementTree tree, Void unused) {
+      return true;
+    }
+
+    /*
+     * An if-then statement can complete normally iff it is reachable.
+     *
+     * The then-statement is reachable iff the if-then statement is reachable.
+     * An if-then-else statement can complete normally iff the then-statement
+     * can complete normally or the else-statement can complete normally.
+     *
+     * The then-statement is reachable iff the if-then-else statement is
+     * reachable.
+     *
+     * The else-statement is reachable iff the if-then-else statement is
+     * reachable.
+     */
+    @Override
+    public Boolean visitIf(IfTree tree, Void unused) {
+      boolean thenCompletes = scan(tree.getThenStatement());
+      boolean elseCompletes = tree.getElseStatement() == null || scan(tree.getElseStatement());
+      return thenCompletes || elseCompletes;
+    }
+
+    /* An assert statement can complete normally iff it is reachable. */
+    @Override
+    public Boolean visitAssert(AssertTree tree, Void unused) {
+      return true;
+    }
+
+    /*
+     * A switch statement can complete normally iff at least one of the
+     * following is true:
+     *
+     *  1) The switch block is empty or contains only switch labels.
+     *  2) The last statement in the switch block can complete normally.
+     *  3) There is at least one switch label after the last switch block
+     *     statement group.
+     *  4) The switch block does not contain a default label.
+     *  5) There is a reachable break statement that exits the switch statement.
+     *
+     * A switch block is reachable iff its switch statement is reachable.
+     *
+     * A statement in a switch block is reachable iff its switch statement is
+     * reachable and at least one of the following is true:
+     *
+     *  - It bears a case or default label.
+     *
+     *  - There is a statement preceding it in the switch block and that
+     *    preceding statement can complete normally.
+     */
+    @Override
+    public Boolean visitSwitch(SwitchTree tree, Void unused) {
+      // (1)
+      if (tree.getCases().stream().allMatch(c -> c.getStatements().isEmpty())) {
+        return true;
+      }
+      // (2)
+      boolean lastCompletes = true;
+      for (CaseTree c : tree.getCases()) {
+        lastCompletes = scan(c.getStatements());
+      }
+      if (lastCompletes) {
+        return true;
+      }
+      // (3)
+      if (getLast(tree.getCases()).getStatements().isEmpty()) {
+        return true;
+      }
+      // (4)
+      if (tree.getCases().stream().noneMatch(c -> c.getExpression() == null)) {
+        return true;
+      }
+      // (5)
+      if (breaks.contains(tree)) {
+        return true;
+      }
+      return false;
+    }
+
+    /*
+     * A while statement can complete normally iff at least one of the
+     * following is true:
+     *
+     *  1) The while statement is reachable and the condition expression is not
+     *     a constant expression (§15.28) with value true.
+     *  2) There is a reachable break statement that exits the while statement.
+     *
+     *  The contained statement is reachable iff the while statement is
+     *  reachable and the condition expression is not a constant expression
+     *  whose value is false.
+     */
+    @Override
+    public Boolean visitWhileLoop(WhileLoopTree tree, Void unused) {
+      Boolean condValue = ASTHelpers.constValue(tree.getCondition(), Boolean.class);
+      if (!Objects.equals(condValue, false)) {
+        scan(tree.getStatement());
+      }
+      // (1)
+      if (!Objects.equals(condValue, true)) {
+        return true;
+      }
+      // (2)
+      if (breaks.contains(tree)) {
+        return true;
+      }
+      return false;
+    }
+
+    /*
+     * A do statement can complete normally iff at least one of the following
+     * is true:
+     *
+     *  1) The contained statement can complete normally and the condition
+     *     expression is not a constant expression (§15.28) with value true.
+     *
+     *  2) The do statement contains a reachable continue statement with no
+     *     label, and the do statement is the innermost while, do, or for
+     *     statement that contains that continue statement, and the continue
+     *     statement continues that do statement, and the condition expression
+     *     is not a constant expression with value true.
+     *
+     *  3) The do statement contains a reachable continue statement with a
+     *     label L, and the do statement has label L, and the continue
+     *     statement continues that do statement, and the condition expression
+     *     is not a constant expression with value true.
+     *
+     *  4) There is a reachable break statement that exits the do statement.
+     *
+     * The contained statement is reachable iff the do statement is reachable.
+     */
+    @Override
+    public Boolean visitDoWhileLoop(DoWhileLoopTree that, Void unused) {
+      boolean completes = scan(that.getStatement());
+      boolean conditionIsAlwaysTrue =
+          firstNonNull(ASTHelpers.constValue(that.getCondition(), Boolean.class), false);
+      // (1)
+      if (completes && !conditionIsAlwaysTrue) {
+        return true;
+      }
+      // (2) or (3)
+      if (continues.contains(that) && !conditionIsAlwaysTrue) {
+        return true;
+      }
+      // (4)
+      if (breaks.contains(that)) {
+        return true;
+      }
+      return false;
+    }
+
+    /*
+     * A basic for statement can complete normally iff at least one of the
+     * following is true:
+     *
+     * 1) The for statement is reachable, there is a condition expression, and
+     *    the condition expression is not a constant expression (§15.28) with
+     *    value true.
+     *
+     * 2) There is a reachable break statement that exits the for statement.
+     *
+     * The contained statement is reachable iff the for statement is reachable
+     * and the condition expression is not a constant expression whose value is
+     * false.
+     */
+    @Override
+    public Boolean visitForLoop(ForLoopTree that, Void unused) {
+      Boolean condValue = ASTHelpers.constValue(that.getCondition(), Boolean.class);
+      if (!Objects.equals(condValue, false)) {
+        scan(that.getStatement());
+      }
+      // (1)
+      if (!Objects.equals(condValue, true)) {
+        return true;
+      }
+      // (2)
+      if (breaks.contains(that)) {
+        return true;
+      }
+      return false;
+    }
+
+    /* An enhanced for statement can complete normally iff it is reachable. */
+    @Override
+    public Boolean visitEnhancedForLoop(EnhancedForLoopTree that, Void unused) {
+      scan(that.getStatement());
+      return true;
+    }
+
+    /* A return statement cannot complete normally. */
+    @Override
+    public Boolean visitReturn(ReturnTree tree, Void unused) {
+      return false;
+    }
+
+    /* A throw statement cannot complete normally. */
+    @Override
+    public Boolean visitThrow(ThrowTree tree, Void unused) {
+      return false;
+    }
+
+    /*
+     * A synchronized statement can complete normally iff the contained
+     * statement can complete normally.
+     *
+     * The contained statement is reachable iff the synchronized statement
+     * is reachable.
+     */
+    @Override
+    public Boolean visitSynchronized(SynchronizedTree tree, Void unused) {
+      return scan(tree.getBlock());
+    }
+
+    /*
+     * A try statement can complete normally iff both of the following are true:
+     *
+     *  1) The try block can complete normally or any catch block can complete
+     *     normally.
+     *
+     *  2) If the try statement has a finally block, then the finally block can
+     *     complete normally.
+     */
+    @Override
+    public Boolean visitTry(TryTree that, Void unused) {
+      boolean completes = scan(that.getBlock());
+      // assume all catch blocks are reachable; javac has already rejected unreachable
+      // checked exception handlers
+      for (CatchTree catchTree : that.getCatches()) {
+        completes |= scan(catchTree.getBlock());
+      }
+      if (that.getFinallyBlock() != null && !scan(that.getFinallyBlock())) {
+        completes = false;
+      }
+      return completes;
+    }
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/util/ReachabilityTest.java
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.util;
+
+import static com.google.errorprone.BugPattern.Category.JDK;
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static java.util.stream.Collectors.toList;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.CompilationTestHelper;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.SwitchTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.tree.SwitchTree;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/** {@link Reachability}Test. */
+@RunWith(Parameterized.class)
+public class ReachabilityTest {
+
+  /** Reports an error if the first case in a switch falls through to the second. */
+  @BugPattern(name = "FirstCaseFallsThrough", category = JDK, summary = "", severity = ERROR)
+  public static class FirstCaseFallsThrough extends BugChecker implements SwitchTreeMatcher {
+
+    @Override
+    public Description matchSwitch(SwitchTree tree, VisitorState state) {
+      if (tree.getCases().size() != 2 || tree.getCases().get(0).getStatements().isEmpty()) {
+        return NO_MATCH;
+      }
+      return Reachability.canCompleteNormally(
+              Iterables.getLast(tree.getCases().get(0).getStatements()))
+          ? describeMatch(tree.getCases().get(1))
+          : NO_MATCH;
+    }
+  }
+
+  private final String[] lines;
+
+  public ReachabilityTest(String[] lines) {
+    this.lines = lines;
+  }
+
+  @Test
+  public void test() {
+    CompilationTestHelper.newInstance(FirstCaseFallsThrough.class, getClass())
+        .addSourceLines(
+            "in/Test.java",
+            "import java.io.*;",
+            "import java.nio.file.*;",
+            "class Test {",
+            "  void f(int x) {",
+            "    switch (x) {",
+            "      case 1:",
+            Joiner.on('\n').join(lines),
+            "      default:",
+            "        break;",
+            "    }",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Parameters
+  public static List<Object[]> parameters() {
+    String[][] parameters = {
+      {
+        "int a = 1;", //
+        "int b = 2;",
+        "break;",
+      },
+      {
+        "System.err.println();", //
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "int a = 1;",
+        "int b = 2;",
+        "class L {}",
+        ";;",
+        "assert false;",
+        "label: System.err.println();",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "if (true) {", //
+        "} else {",
+        "  break;",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "if (true) {", //
+        "  break;",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "if (true) {", //
+        "  break;",
+        "} else {",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "if (true) {", //
+        "  break;",
+        "} else {",
+        "  break;",
+        "}",
+      },
+      {
+        "switch (42) {", //
+        "  case 0:",
+        "  case 1:",
+        "  case 2:",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "switch (42) {", //
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "switch (42) {",
+        "  case 0:",
+        "    break;",
+        "  case 1:",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "switch (42) {", //
+        "  default:",
+        "    break;",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "switch (42) {", //
+        "  default:",
+        "    return;",
+        "}",
+      },
+      {
+        "while (true) {", //
+        "}",
+      },
+      {
+        "while (true) {", //
+        "  break;",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "while (true) {", //
+        "  return;",
+        "}",
+      },
+      {
+        "while (x == 0) {}", //
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "loop: do {", //
+        "  continue loop;",
+        "} while (x == 0);",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "loop: do {", //
+        "  continue loop;",
+        "} while (true);",
+      },
+      {
+        "int i = 1;",
+        "loop: do {",
+        "  i++;",
+        "  continue loop;",
+        "} while (i == 0);",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "try {",
+        "  Files.readAllBytes(Paths.get(\"file\"));",
+        "  return;",
+        "} catch (IOException e) {",
+        "  return;",
+        "}",
+      },
+      {
+        "try {",
+        "  try {",
+        "    Files.readAllBytes(Paths.get(\"file\"));",
+        "    return;",
+        "  } catch (NullPointerException e) {",
+        "    return;",
+        "  }",
+        "} catch (IOException e) {",
+        "  return;",
+        "}",
+      },
+      {
+        "try {",
+        "  Files.readAllBytes(Paths.get(\"file\"));",
+        "  return;",
+        "} catch (IOException e) {",
+        "  return;",
+        "} finally {",
+        "}",
+      },
+      {
+        "try {", //
+        "  //",
+        "} catch (Throwable t) {",
+        "  return;",
+        "} finally {",
+        "  return;",
+        "}",
+      },
+      {
+        "try {", //
+        "  return;",
+        "} catch (Throwable t) {",
+        "  //",
+        "} finally {",
+        "  return;",
+        "}",
+      },
+      {
+        "int y = 0;",
+        "while (true) {",
+        "  if (y++ > 10) {",
+        "    return;",
+        "  }",
+        "  if (y-- < 10) {",
+        "    return;",
+        "  }",
+        "}",
+      },
+      {
+        "int y = 0;",
+        "while (true) {",
+        "  do {",
+        "    switch (y) {",
+        "      case 0:",
+        "        continue;", // continue target is do/while, not switch or outer while
+        "    }",
+        "  } while (y > 0);",
+        "  break;",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "try {",
+        "  if (Files.readAllBytes(Paths.get(\"file\")) != null) {}",
+        "  return;",
+        "} catch (IOException e) {",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "try {",
+        "  Files.readAllBytes(Paths.get(\"file\"));",
+        "} catch (IOException e) {",
+        "  throw new IOError(e);",
+        "}",
+        "// BUG: Diagnostic contains:",
+      },
+      {
+        "try {",
+        "  Files.readAllBytes(Paths.get(\"file\"));",
+        "  return;",
+        "} catch (IOException e) {",
+        "  throw new IOError(e);",
+        "}",
+      },
+      {
+        "throw new AssertionError();",
+      },
+    };
+    return Arrays.stream(parameters).map(x -> new Object[] {x}).collect(toList());
+  }
+}
