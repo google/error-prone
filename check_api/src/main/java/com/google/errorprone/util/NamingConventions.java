57fb205687df7a6087358c72374e29879a916839
andrewrice <andrewrice@google.com>
2017-03-23 11:44:03 -0700

    Add NamingConventions class to hold a splitToLowercaseTerms method
    The upcoming check for detecting accidentally permuted arguments needs to split
    identifiers into terms. Its needed both for heuristically finding a name from
    an expression tree (e.g. remove the first term from the method name if its
    'get') and also for computing edit distances between names.
    
    RELNOTES: Add splitToLowercaseTerms to split Java identifiers
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=151032960

---
+++ check_api/src/main/java/com/google/errorprone/util/NamingConventions.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.util;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/** Utility functions for dealing with Java naming conventions */
+public class NamingConventions {
+
+  private static final Pattern ONLY_UNDERSCORES = Pattern.compile("^_+$");
+
+  private static final String UNDERSCORE = "_";
+  private static final String CASE_TRANSITION = "(?<=[a-z0-9])(?=[A-Z])";
+  private static final String TRAILING_DIGITS = "(?<![0-9_])(?=[0-9]+$)";
+
+  private static final Pattern TERM_SPLITTER =
+      Pattern.compile(String.format("%s|%s|%s", UNDERSCORE, CASE_TRANSITION, TRAILING_DIGITS));
+
+  /**
+   * Split a Java name into terms based on either Camel Case or Underscores. We also split digits at
+   * the end of the name into a separate term so as to treat PERSON1 and PERSON_1 as the same thing.
+   *
+   * @param identifierName to split
+   * @return a list of the terms in the name, in order and converted to lowercase
+   */
+  public static ImmutableList<String> splitToLowercaseTerms(String identifierName) {
+    if (ONLY_UNDERSCORES.matcher(identifierName).matches()) {
+      // Degenerate case of names which contain only underscore
+      return ImmutableList.of(identifierName);
+    }
+    return Arrays.stream(TERM_SPLITTER.split(identifierName))
+        .map(String::toLowerCase)
+        .collect(toImmutableList());
+  }
+}
---
+++ check_api/src/test/java/com/google/errorprone/util/NamingConventionsTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.util;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for code in NamingConventions */
+@RunWith(JUnit4.class)
+public class NamingConventionsTest {
+
+  @Test
+  public void splitToLowercaseTerms_separatesTerms_withLowerCamelCase() {
+    String identifierName = "camelCaseTerm";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("camel", "case", "term");
+  }
+
+  @Test
+  public void splitToLowercaseTerms_separatesTerms_withUpperCamelCase() {
+    String identifierName = "CamelCaseTerm";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("camel", "case", "term");
+  }
+
+  @Test
+  public void splitToLowercaseTerms_separatesTrailingDigits_withoutDelimiter() {
+    String identifierName = "term123";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("term", "123");
+  }
+
+  @Test
+  public void splitToLowercaseTerms_doesntSplit_withIntermediateDigits() {
+    String identifierName = "i8n";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("i8n");
+  }
+
+  @Test
+  public void splitToLowercaseTerms_separatesTerms_withUnderscoreSeparator() {
+    String identifierName = "UNDERSCORE_SEPARATED";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("underscore", "separated");
+  }
+
+  @Test
+  public void splitToLowercaseTerms_findsSingleTerm_withOnlyUnderscore() {
+    String identifierName = "_____";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("_____");
+  }
+
+  @Test
+  public void splitToLowercaseTerms_noEmptyTerm_withTrailingUnderscoreDigit() {
+    String identifierName = "test_1";
+
+    ImmutableList<String> terms = NamingConventions.splitToLowercaseTerms(identifierName);
+
+    assertThat(terms).containsExactly("test", "1");
+  }
+}
