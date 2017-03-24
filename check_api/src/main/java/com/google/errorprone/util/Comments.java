11e3248aed7fa4a1b432a9bb0bfaf0b298cb8033
andrewrice <andrewrice@google.com>
2017-03-23 18:03:01 -0700

    Add utility for attaching comments to arguments in method invocations
    RELNOTES: Add utility for attaching comments to arguments in method invocations
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=151079726

---
+++ check_api/src/main/java/com/google/errorprone/util/Comments.java
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.errorprone.util;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.CharMatcher;
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.VisitorState;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.util.TreePath;
+import com.sun.tools.javac.parser.Tokens.Comment;
+import com.sun.tools.javac.parser.Tokens.TokenKind;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.util.Position.LineMap;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Utilities for attaching comments to relevant AST nodes
+ *
+ * @author andrewrice@google.com (Andrew Rice)
+ */
+public class Comments {
+
+  /**
+   * Attach comments to nodes on arguments of constructor calls. Calls such as {@code new Test(
+   * param1 /* c1 *&#47;, /* c2 *&#47; param2)} will attach the comment c1 to {@code param1} and the
+   * comment c2 to {@code param2}.
+   *
+   * <p>Warning: this is expensive to compute as it involves re-tokenizing the source for this node
+   */
+  public static ImmutableList<Commented<ExpressionTree>> findCommentsForArguments(
+      NewClassTree newClassTree, VisitorState state) {
+    return findCommentsForArguments(newClassTree, newClassTree.getArguments(), state);
+  }
+
+  /**
+   * Attach comments to nodes on arguments of method calls. Calls such as {@code test(param1 /* c1
+   * *&#47;, /* c2 *&#47; param2)} will attach the comment c1 to {@code param1} and the comment c2
+   * to {@code param2}.
+   *
+   * <p>Warning: this is expensive to compute as it involves re-tokenizing the source for this node
+   */
+  public static ImmutableList<Commented<ExpressionTree>> findCommentsForArguments(
+      MethodInvocationTree methodInvocationTree, VisitorState state) {
+    return findCommentsForArguments(
+        methodInvocationTree, methodInvocationTree.getArguments(), state);
+  }
+
+  private static ImmutableList<Commented<ExpressionTree>> findCommentsForArguments(
+      Tree tree, List<? extends ExpressionTree> arguments, VisitorState state) {
+
+    if (arguments.isEmpty()) {
+      return ImmutableList.of();
+    }
+
+    CharSequence sourceCode = state.getSourceCode();
+    int startPosition = ((JCTree) tree).getStartPosition();
+    int endPosition = computeEndPosition(tree, sourceCode, state);
+    CharSequence source = sourceCode.subSequence(startPosition, endPosition);
+
+    ErrorProneTokens errorProneTokens = new ErrorProneTokens(source.toString(), state.context);
+    ImmutableList<ErrorProneToken> tokens = errorProneTokens.getTokens();
+    LineMap lineMap = errorProneTokens.getLineMap();
+
+    ArgumentTracker argumentTracker = new ArgumentTracker(arguments, startPosition, state, lineMap);
+    TokenTracker tokenTracker = new TokenTracker(lineMap);
+
+    argumentTracker.advance();
+    for (ErrorProneToken token : tokens) {
+      tokenTracker.advance(token);
+      if (tokenTracker.atStartOfLine() && !tokenTracker.wasPreviousLineEmpty()) {
+        for (Comment c : token.comments()) {
+          if (tokenTracker.isCommentOnPreviousLine(c)
+              && token.pos() <= argumentTracker.currentArgumentStartPosition
+              && argumentTracker.isPreviousArgumentOnPreviousLine()) {
+            // token was on the previous line so therefore we should add it to the previous comment
+            // unless the previous argument was not on the the previous line with it
+            argumentTracker.addCommentToPreviousArgument(c);
+          } else {
+            argumentTracker.addCommentToCurrentArgument(c);
+          }
+        }
+      } else {
+        argumentTracker.addAllCommentsToCurrentArgument(token.comments());
+      }
+      if (token.pos() >= argumentTracker.currentArgumentEndPosition) {
+        // We are between arguments so wait for a (lexed) comma to delimit them
+        if (token.kind() == TokenKind.COMMA) {
+          argumentTracker.advance();
+        }
+      }
+    }
+
+    return argumentTracker.build();
+  }
+
+  /**
+   * Finds the end position of this MethodInvocationTree. This is complicated by the fact that
+   * sometimes a comment will fall outside of the bounds of the tree.
+   *
+   * <p>For example:
+   *
+   * <pre>
+   *   test(arg1,  // comment1
+   *        arg2); // comment2
+   *   int i;
+   * </pre>
+   *
+   * In this case {@code comment2} lies beyond the end of the invocation tree. In order to get the
+   * comment we need the tokenizer to lex the token which follows the invocation ({@code int} in the
+   * example).
+   *
+   * <p>We over-approximate this end position by looking for the next node in the AST and using the
+   * end position of this node.
+   *
+   * <p>As a heuristic we first scan for any {@code /} characters on the same line as the end of the
+   * method invocation. If we don't find any then we use the end of the method invocation as the end
+   * position.
+   */
+  @VisibleForTesting
+  static int computeEndPosition(
+      Tree methodInvocationTree, CharSequence sourceCode, VisitorState state) {
+    int invocationEnd = state.getEndPosition(methodInvocationTree);
+
+    // Finding a good end position is expensive so first check whether we have any comment at
+    // the end of our line. If we don't then we can just use the end of the methodInvocationTree
+    int nextNewLine = CharMatcher.is('\n').indexIn(sourceCode, invocationEnd);
+    if (nextNewLine == -1) {
+      return invocationEnd;
+    }
+
+    if (CharMatcher.is('/').matchesNoneOf(sourceCode.subSequence(invocationEnd, nextNewLine))) {
+      return invocationEnd;
+    }
+
+    return state.getEndPosition(getNextNodeOrParent(methodInvocationTree, state));
+  }
+
+  /**
+   * Find the node which (approximately) follows this one in the tree. This works by walking upwards
+   * to find enclosing block (or class) and then looking for the node after the subtree we walked.
+   * If our subtree is the last of the block then we return the node for the block instead, if we
+   * can't find a suitable block we return the parent node.
+   */
+  private static Tree getNextNodeOrParent(Tree current, VisitorState state) {
+    Tree predecessorNode = current;
+    TreePath enclosingPath = state.getPath();
+    while (enclosingPath != null
+        && !(enclosingPath.getLeaf() instanceof BlockTree)
+        && !(enclosingPath.getLeaf() instanceof ClassTree)) {
+      predecessorNode = enclosingPath.getLeaf();
+      enclosingPath = enclosingPath.getParentPath();
+    }
+
+    if (enclosingPath == null) {
+      return state.getPath().getParentPath().getLeaf();
+    }
+
+    Tree parent = enclosingPath.getLeaf();
+
+    if (parent instanceof BlockTree) {
+      return after(predecessorNode, ((BlockTree) parent).getStatements(), parent);
+    } else if (parent instanceof ClassTree) {
+      return after(predecessorNode, ((ClassTree) parent).getMembers(), parent);
+    }
+    return parent;
+  }
+
+  /**
+   * Find the element in the iterable following the target
+   *
+   * @param target is the element to search for
+   * @param iterable is the iterable to search
+   * @param defaultValue will be returned if there is no item following the searched for item
+   * @return the item following {@code target} or {@code defaultvalue} if not found
+   */
+  private static <T> T after(T target, Iterable<? extends T> iterable, T defaultValue) {
+    Iterator<? extends T> iterator = iterable.iterator();
+    while (iterator.hasNext()) {
+      if (iterator.next().equals(target)) {
+        break;
+      }
+    }
+
+    if (iterator.hasNext()) {
+      return iterator.next();
+    }
+
+    return defaultValue;
+  }
+
+  /** This class is used to keep track of state between lines of code when consuming tokens */
+  private static class TokenTracker {
+
+    private final LineMap lineMap;
+
+    private int tokensOnCurrentLine = 0;
+    private int currentLineNumber = -1;
+    private boolean previousLineEmpty = true;
+
+    TokenTracker(LineMap lineMap) {
+      this.lineMap = lineMap;
+    }
+
+    void advance(ErrorProneToken token) {
+      int line = lineMap.getLineNumber(token.pos());
+      if (line != currentLineNumber) {
+        currentLineNumber = line;
+        previousLineEmpty = tokensOnCurrentLine == 0;
+        tokensOnCurrentLine = 0;
+      } else {
+        tokensOnCurrentLine++;
+      }
+    }
+
+    boolean isCommentOnPreviousLine(Comment c) {
+      int tokenLine = lineMap.getLineNumber(c.getSourcePos(0));
+      return tokenLine == currentLineNumber - 1;
+    }
+
+    boolean atStartOfLine() {
+      return tokensOnCurrentLine == 0;
+    }
+
+    boolean wasPreviousLineEmpty() {
+      return previousLineEmpty;
+    }
+  }
+
+  /**
+   * This class is used to keep track of the arguments we are processing. It keeps a window of the
+   * current and previous argument as builders so that more comments can be added to them as we find
+   * them. When we advance everything is shuffled down: the builder for the previous argument is
+   * built and put in the final results list, the builder for the current argument is moved to
+   * previous and a new builder is made for the next argument. We also track the positions of the
+   * current and previous argument so that we know whether a comment occurred before or after it
+   */
+  private static class ArgumentTracker {
+
+    private final VisitorState state;
+    private final Iterator<? extends ExpressionTree> argumentsIterator;
+    private final int offset;
+    private final LineMap lineMap;
+
+    private Commented.Builder<ExpressionTree> currentCommentedResultBuilder = null;
+    private Commented.Builder<ExpressionTree> previousCommentedResultBuilder = null;
+    private final ImmutableList.Builder<Commented<ExpressionTree>> resultBuilder =
+        ImmutableList.builder();
+
+    private int currentArgumentStartPosition = -1;
+    private int currentArgumentEndPosition = -1;
+    private int previousArgumentEndPosition = -1;
+
+    ArgumentTracker(
+        Iterable<? extends ExpressionTree> arguments,
+        int offset,
+        VisitorState state,
+        LineMap lineMap) {
+      this.state = state;
+      this.offset = offset;
+      this.argumentsIterator = arguments.iterator();
+      this.lineMap = lineMap;
+    }
+
+    void advance() {
+      ExpressionTree nextArgument = argumentsIterator.next();
+
+      currentArgumentEndPosition = state.getEndPosition(nextArgument) - offset;
+      previousArgumentEndPosition = currentArgumentStartPosition;
+      currentArgumentStartPosition = ((JCTree) nextArgument).getStartPosition() - offset;
+
+      if (previousCommentedResultBuilder != null) {
+        resultBuilder.add(previousCommentedResultBuilder.build());
+      }
+      previousCommentedResultBuilder = currentCommentedResultBuilder;
+      currentCommentedResultBuilder = Commented.builder().setTree(nextArgument);
+    }
+
+    /** Returns the final result. The object should not be used after calling this method */
+    ImmutableList<Commented<ExpressionTree>> build() {
+      if (previousCommentedResultBuilder != null) {
+        resultBuilder.add(previousCommentedResultBuilder.build());
+      }
+
+      if (currentCommentedResultBuilder != null) {
+        resultBuilder.add(currentCommentedResultBuilder.build());
+      }
+
+      return resultBuilder.build();
+    }
+
+    boolean isPreviousArgumentOnPreviousLine() {
+      return lineMap.getLineNumber(previousArgumentEndPosition)
+          == lineMap.getLineNumber(currentArgumentStartPosition) - 1;
+    }
+
+    void addCommentToPreviousArgument(Comment c) {
+      previousCommentedResultBuilder.addComment(c, previousArgumentEndPosition);
+    }
+
+    void addCommentToCurrentArgument(Comment c) {
+      currentCommentedResultBuilder.addComment(c, currentArgumentStartPosition);
+    }
+
+    void addAllCommentsToCurrentArgument(Iterable<Comment> comments) {
+      currentCommentedResultBuilder.addAllComment(comments, currentArgumentStartPosition);
+    }
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/util/CommentsTest.java
@@ -0,0 +1,355 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.util;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.Category;
+import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.CompilationTestHelper;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.NewClassTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.NewClassTree;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Test code for {@code Comments} */
+@RunWith(JUnit4.class)
+public class CommentsTest {
+
+  /**
+   * A {@link BugChecker} that calls computeEndPosition for each invocation and prints the final
+   * line of source ending at that position
+   */
+  @BugPattern(
+    name = "ComputeEndPosition",
+    category = Category.ONE_OFF,
+    severity = SeverityLevel.ERROR,
+    summary = "Calls computeEndPosition and prints results"
+  )
+  public static class ComputeEndPosition extends BugChecker implements MethodInvocationTreeMatcher {
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+      CharSequence sourceCode = state.getSourceCode();
+      int endPosition = Comments.computeEndPosition(tree, sourceCode, state);
+      int startPosition = endPosition;
+      do {
+        startPosition--;
+      } while (sourceCode.charAt(startPosition) != '\n');
+
+      return buildDescription(tree)
+          .setMessage(sourceCode.subSequence(startPosition + 1, endPosition).toString())
+          .build();
+    }
+  }
+
+  @Test
+  public void computeEndPosition_returnsEndOfInvocation_whenNoTrailingComment() {
+    CompilationTestHelper.newInstance(ComputeEndPosition.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param);",
+            "  private void test(Object param) {",
+            "    // BUG: Diagnostic contains: target(param)",
+            "    target(param);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void computeEndPosition_returnsEndOfNextStatement_whenTrailingComment() {
+    CompilationTestHelper.newInstance(ComputeEndPosition.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param);",
+            "  private void test(Object param) {",
+            "    // BUG: Diagnostic contains: int i;",
+            "    target(param); // 1",
+            "    int i;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void computeEndPosition_returnsEndOfBlock_whenLastWithTrailingComment() {
+    CompilationTestHelper.newInstance(ComputeEndPosition.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param);",
+            "  private void test(Object param) {",
+            "    // BUG: Diagnostic contains: }",
+            "    target(param); // 1",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  /** A {@link BugChecker} that prints the comments around arguments */
+  @BugPattern(
+    name = "PrintCommentsForArguments",
+    category = Category.ONE_OFF,
+    severity = SeverityLevel.ERROR,
+    summary = "Prints comments occurring around arguments"
+  )
+  public static class PrintCommentsForArguments extends BugChecker
+      implements MethodInvocationTreeMatcher, NewClassTreeMatcher {
+    @Override
+    public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+      if (ASTHelpers.getSymbol(tree).getSimpleName().contentEquals("<init>")) {
+        return Description.NO_MATCH;
+      }
+      return buildDescription(tree)
+          .setMessage(commentsToString(Comments.findCommentsForArguments(tree, state)))
+          .build();
+    }
+
+    @Override
+    public Description matchNewClass(NewClassTree tree, VisitorState state) {
+      return buildDescription(tree)
+          .setMessage(commentsToString(Comments.findCommentsForArguments(tree, state)))
+          .build();
+    }
+
+    private static String commentsToString(ImmutableList<Commented<ExpressionTree>> comments) {
+      return comments
+          .stream()
+          .map(
+              c ->
+                  c.beforeComments().stream().map(i -> i.getText()).collect(toImmutableList())
+                      + " "
+                      + c.tree()
+                      + " "
+                      + c.afterComments().stream().map(i -> i.getText()).collect(toImmutableList()))
+          .collect(toImmutableList())
+          .toString();
+    }
+  }
+
+  @Test
+  public void findCommentsForArguments_findsBothComments_beforeAndAfter() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param);",
+            "  private void test(Object param) {",
+            "    // BUG: Diagnostic contains: [[/* 1 */] param [/* 2 */]]",
+            "    target(/* 1 */ param /* 2 */);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_findsBothComments_before() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param);",
+            "  private void test(Object param) {",
+            "    // BUG: Diagnostic contains: [[/* 1 */, /* 2 */] param []]",
+            "    target(/* 1 */ /* 2 */ param);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_findsBothComments_after() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param);",
+            "  private void test(Object param) {",
+            "    // BUG: Diagnostic contains: [[] param [/* 1 */, /* 2 */]]",
+            "    target(param /* 1 */ /* 2 */);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_assignsToCorrectParameter_adjacentComments() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param1, Object param2);",
+            "  private void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [/* 1 */], [/* 2 */] param2 []]",
+            "    target(param1 /* 1 */, /* 2 */ param2);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_findsNoComments_whenNoComments() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param1, Object param2);",
+            "  private void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [], [] param2 []]",
+            "    target(param1, param2);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_findsNothing_whenNoArguments() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target();",
+            "  private void test() {",
+            "    // BUG: Diagnostic contains: []",
+            "    target();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_assignToFirstParameter_withLineCommentAfterComma() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param1, Object param2);",
+            "  private void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [// 1], [] param2 []]",
+            "    target(param1, // 1",
+            "           param2);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_assignToFirstParameter_withBlockAfterComma() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param1, Object param2);",
+            "  private void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [/* 1 */], [] param2 []]",
+            "    target(param1, /* 1 */",
+            "           param2);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_assignToSecondParameter_withLineCommentAfterMethod() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param1, Object param2);",
+            "  private void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [], [] param2 [// 2]]",
+            "    target(param1,",
+            "           param2); // 2",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void
+      findCommentsForArguments_assignToSecondParameter_withLineCommentAfterMethodMidBlock() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract void target(Object param1, Object param2);",
+            "  private void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [], [] param2 [// 2]]",
+            "    target(param1,",
+            "           param2); // 2",
+            "    int i = 1;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_assignToSecondParameter_withLineCommentAfterField() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "abstract class Test {",
+            "  abstract Object target(Object param);",
+            "  // BUG: Diagnostic contains: [[] null [// 1]]",
+            "  private Object test = target(null); // 1",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_findsComments_onConstructor() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "class Test {",
+            "  Test(Object param1, Object param2) {}",
+            "  void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[/* 1 */] param1 [], [] param2 [/* 2 */]]",
+            "    new Test(/* 1 */ param1, param2 /* 2 */);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void findCommentsForArguments_attachesCommentToSecondArgument_whenCommentOnOwnLine() {
+    CompilationTestHelper.newInstance(PrintCommentsForArguments.class, getClass())
+        .addSourceLines(
+            "Test.java",
+            "class Test {",
+            "  Test(Object param1, Object param2) {}",
+            "  void test(Object param1, Object param2) {",
+            "    // BUG: Diagnostic contains: [[] param1 [], [// 1] param2 []]",
+            "    new Test(param1, ",
+            "             // 1",
+            "             param2);",
+            "  }",
+            "}")
+        .doTest();
+  }
+}
