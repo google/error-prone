f14b9aaafeff66d956265d82478773a86d0f7658
andrewrice <andrewrice@google.com>
2017-04-22 08:51:18 -0700

    Add an explanation for ArgumentSelectionDefectChecker
    RELNOTES: Add an explanation for ArgumentSelectionDefectChecker
    
    MOE_MIGRATED_REVID=153929015

--- core/src/main/java/com/google/errorprone/bugpatterns/argumentselectiondefects/ArgumentSelectionDefectChecker.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/argumentselectiondefects/ArgumentSelectionDefectChecker.java
@@ -60,6 +60,14 @@ import java.util.function.Function;
 @BugPattern(
   name = "ArgumentSelectionDefectChecker",
   summary = "Arguments to this method call may be in the wrong order",
+  explanation =
+      "If permuting the arguments of a method call means that the argument names are a better "
+          + "match for the parameter names than the original ordering then this might indicate "
+          + "that they have been accidentally swapped.  There are also legitimate reasons for the "
+          + "names not to match such as when rotating an image (swap width and height).  In this "
+          + "case we would recommend annotating the names with a comment to make the deliberate "
+          + "swap clear to future readers of the code. Argument names annotated with a comment "
+          + "containing the parameter name will not generate a warning.",
   category = JDK,
   severity = WARNING
 )
