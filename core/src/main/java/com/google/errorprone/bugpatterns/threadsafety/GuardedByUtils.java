e80f5eeabac63f572cb844a80c5f9aef539f3b59
cushon <cushon@google.com>
2017-04-24 14:51:33 -0700

    Expose a helper for binding GuardedBy strings to symbols
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154100818

--- core/src/main/java/com/google/errorprone/bugpatterns/threadsafety/GuardedByUtils.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/threadsafety/GuardedByUtils.java
@@ -109,4 +109,13 @@ public class GuardedByUtils {
 
     return GuardedByValidationResult.ok();
   }
+
+  public static Symbol bindGuardedByString(Tree tree, String guard, VisitorState visitorState) {
+    Optional<GuardedByExpression> bound =
+        GuardedByBinder.bindString(guard, GuardedBySymbolResolver.from(tree, visitorState));
+    if (!bound.isPresent()) {
+      return null;
+    }
+    return bound.get().sym();
+  }
 }
