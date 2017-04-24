b1a1ef61a535c456885e4636821ba17e27d1cd7b
cushon <cushon@google.com>
2017-04-24 14:51:33 -0700

    Expose a helper for binding GuardedBy strings to symbols
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154100818

--- core/src/main/java/com/google/errorprone/bugpatterns/threadsafety/GuardedBySymbolResolver.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/threadsafety/GuardedBySymbolResolver.java
@@ -53,7 +53,7 @@ public class GuardedBySymbolResolver implements GuardedByBinder.Resolver {
 
   public static GuardedBySymbolResolver from(Tree tree, VisitorState visitorState) {
     return GuardedBySymbolResolver.from(
-        (ClassSymbol) ASTHelpers.getSymbol(tree).owner,
+        ASTHelpers.getSymbol(tree).owner.enclClass(),
         visitorState.getPath().getCompilationUnit(),
         visitorState.context,
         tree);
