799f811003fd24f7212adde86ab2baa4684dbca9
cushon <cushon@google.com>
2017-04-24 12:02:50 -0700

    Update to javac9-r4023
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154078281

--- core/src/main/java/com/google/errorprone/refaster/Inliner.java
+++ core/src/main/java/com/google/errorprone/refaster/Inliner.java
@@ -82,7 +82,9 @@ public final class Inliner {
   public ClassSymbol resolveClass(CharSequence qualifiedClass) 
       throws CouldNotResolveImportException {
     try {
-      Symbol symbol = JavaCompiler.instance(context).resolveIdent(qualifiedClass.toString());
+      Symbol symbol =
+          JavaCompiler.instance(context)
+              .resolveIdent(symtab().java_base, qualifiedClass.toString());
       if (symbol.equals(symtab().errSymbol) || !(symbol instanceof ClassSymbol)) {
         throw new CouldNotResolveImportException(qualifiedClass);
       } else {
