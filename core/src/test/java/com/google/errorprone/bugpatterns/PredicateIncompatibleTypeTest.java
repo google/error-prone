cb142602fc1950be6a4e18caa9ebd3675f81676f
cushon <cushon@google.com>
2017-05-04 12:00:50 -0700

    Fix crash in PredicateIncompatibleType
    if the guava predicate wasn't available. The handling of guava's predicate
    was unnecessary, since recent versions extend the JDK predicate.
    
    Also fix a mistake in the diagnostic message.
    
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=155107932

--- core/src/test/java/com/google/errorprone/bugpatterns/PredicateIncompatibleTypeTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/PredicateIncompatibleTypeTest.java
@@ -37,7 +37,7 @@ public class PredicateIncompatibleTypeTest {
             "import java.util.stream.Stream;",
             "class Test {",
             "  Stream<Integer> f(List<Integer> lx) {",
-            "    // BUG: Diagnostic contains: Using Integer::equals as Predicate<String>;",
+            "    // BUG: Diagnostic contains: Using String::equals as Predicate<Integer>;",
             "    return lx.stream().filter(\"\"::equals);",
             "  }",
             "}")
@@ -71,6 +71,36 @@ public class PredicateIncompatibleTypeTest {
             "class Test {",
             "  Stream<ArrayList<String>> f(List<ArrayList<String>> a, LinkedList<String> b) {",
             "    return a.stream().filter(a::equals);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void positive_guavaPredicate() {
+    testHelper
+        .addSourceLines(
+            "Test.java",
+            "import com.google.common.base.Predicate;",
+            "class Test {",
+            "  void f(Integer x) {",
+            "    // BUG: Diagnostic contains: Using Integer::equals as Predicate<String>;",
+            "    Predicate<String> p = x::equals;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void notAPredicate() {
+    testHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.List;",
+            "import java.util.function.BiFunction;",
+            "class Test {",
+            "  void f() {",
+            "    BiFunction<Object, Object, Boolean> f = Object::equals;",
             "  }",
             "}")
         .doTest();
