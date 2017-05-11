4ad95d45e1b9610b951229f33621d947e820ce6b
cushon <cushon@google.com>
2017-05-11 12:17:31 -0700

    Discourage the reuse of names defined in java.lang
    RELNOTES: New check to discourage the reuse of names defined in java.lang
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=155780170

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/JavaLangClash.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.common.collect.Iterables.getFirst;
+import static com.google.errorprone.BugPattern.Category.JDK;
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static javax.lang.model.element.Modifier.PUBLIC;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.ClassTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.TypeParameterTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TypeParameterTree;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Symbol.PackageSymbol;
+import com.sun.tools.javac.code.Symtab;
+import com.sun.tools.javac.tree.JCTree.JCClassDecl;
+import com.sun.tools.javac.tree.JCTree.JCTypeParameter;
+import com.sun.tools.javac.util.Name;
+import com.sun.tools.javac.util.Names;
+
+/** @author cushon@google.com (Liam Miller-Cushon) */
+@BugPattern(
+  name = "JavaLangClash",
+  category = JDK,
+  summary = "Never reuse class names from java.lang",
+  severity = WARNING
+)
+public class JavaLangClash extends BugChecker
+    implements ClassTreeMatcher, TypeParameterTreeMatcher {
+
+  @Override
+  public Description matchClass(ClassTree tree, VisitorState state) {
+    Name simpleName = ((JCClassDecl) tree).getSimpleName();
+    return check(tree, simpleName, state);
+  }
+
+  @Override
+  public Description matchTypeParameter(TypeParameterTree tree, VisitorState state) {
+    return check(tree, ((JCTypeParameter) tree).getName(), state);
+  }
+
+  private Description check(Tree tree, Name simpleName, VisitorState state) {
+    Symtab symtab = state.getSymtab();
+    PackageSymbol javaLang =
+        symtab.enterPackage(symtab.java_base, Names.instance(state.context).java_lang);
+    Symbol other =
+        getFirst(
+            javaLang.members().getSymbolsByName(simpleName, s -> s.getModifiers().contains(PUBLIC)),
+            null);
+    Symbol symbol = ASTHelpers.getSymbol(tree);
+    if (other == null || other.equals(symbol)) {
+      return NO_MATCH;
+    }
+    return buildDescription(tree)
+        .setMessage(String.format("%s clashes with %s\n", symbol, other))
+        .build();
+  }
+}
---
+++ docs/bugpattern/JavaLangClash.md
@@ -0,0 +1,9 @@
+Class names from `java.lang` should never be reused. From [Java
+Puzzlers](http://www.javapuzzlers.com/java-puzzlers-sampler.pdf):
+
+> Avoid reusing the names of platform classes, and never reuse class names from
+> `java.lang`, because these names are automatically imported everywhere.
+> Programmers are used to seeing these names in their unqualified form and
+> naturally assume that these names refer to the familiar classes from
+> `java.lang`. If you reuse one of these names, the unqualified name will refer
+> to the new definition any time it is used inside its own package.
