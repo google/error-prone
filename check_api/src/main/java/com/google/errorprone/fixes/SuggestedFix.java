a934cf41daa6eca8a70fb394c1d7f93425b49f33
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2017-05-30 00:17:05 +0100

    Apply review comments

--- check_api/src/main/java/com/google/errorprone/fixes/SuggestedFix.java
+++ check_api/src/main/java/com/google/errorprone/fixes/SuggestedFix.java
@@ -67,14 +67,10 @@ public class SuggestedFix implements Fix {
   public String toString(JCCompilationUnit compilationUnit) {
     StringBuilder result = new StringBuilder("replace ");
     for (Replacement replacement : getReplacements(compilationUnit.endPositions)) {
-      result
-          .append("position ")
-          .append(replacement.startPosition())
-          .append(":")
-          .append(replacement.endPosition())
-          .append(" with \"")
-          .append(replacement.replaceWith())
-          .append("\" ");
+      result.append(
+          String.format(
+              "position %d:%d with \"%s\"",
+              replacement.startPosition(), replacement.endPosition(), replacement.replaceWith()));
     }
     return result.toString();
   }
