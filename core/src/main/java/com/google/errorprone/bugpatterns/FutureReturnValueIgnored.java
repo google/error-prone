6ff422dab2c5dc9e1e4f6452e4a610e15ded7267
clm <clm@google.com>
2017-06-28 11:26:21 -0700

    Post-migration cleanup of FutureReturnValueIgnored.
    This removes the suggested fix of adding an unused local variable (since that's not actually a fix) and removes the build flag that makes this check an error - it's already specified in []
    
    RELNOTES: none
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=160430866

--- core/src/main/java/com/google/errorprone/bugpatterns/FutureReturnValueIgnored.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/FutureReturnValueIgnored.java
@@ -26,24 +26,13 @@ import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
-import com.google.errorprone.fixes.SuggestedFix;
-import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
-import com.google.errorprone.util.FindIdentifiers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.Tree;
-import com.sun.source.tree.Tree.Kind;
-import com.sun.source.util.TreePath;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
 import com.sun.tools.javac.code.Type;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.Stack;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ForkJoinTask;
 import java.util.regex.Pattern;
@@ -116,101 +105,5 @@ public final class FutureReturnValueIgnored extends AbstractReturnValueIgnored {
   @Override
   public Matcher<? super MethodInvocationTree> specializedMatcher() {
     return MATCHER;
-  }
-
-  @Override
-  public Description describe(MethodInvocationTree methodInvocationTree, VisitorState state) {
-    return describeUnused(methodInvocationTree, state);
-  }
-
-  // TODO(b/18771748) the following code is hacky and scary, but will only live for as long as the
-  // migration to enable this check takes. Delete once this check is turned into an error.
-  private final Stack<Set<String>> stackNames = new Stack<>();
-  private List<Tree> previousTreePath = new ArrayList<>();
-
-  List<Tree> pathToList(TreePath input) {
-    ArrayList<Tree> list = new ArrayList<>();
-    do {
-      list.add(0, input.getLeaf());
-      input = input.getParentPath();
-    } while (input != null);
-    return list;
-  }
-
-  private String findVariableName(String name, VisitorState state) {
-    if (previousTreePath.size() != stackNames.size()) {
-      throw new IllegalStateException();
-    }
-    TreePath currentPath = state.getPath();
-    List<Tree> currentPathList = pathToList(currentPath);
-    for (int i = 0; i < currentPathList.size(); i++) {
-      if (previousTreePath.size() > i) {
-        if (!previousTreePath.get(i).equals(currentPathList.get(i))) {
-          while (stackNames.size() > i) {
-            stackNames.pop();
-          }
-          stackNames.push(new HashSet<String>());
-        }
-      } else {
-        stackNames.push(new HashSet<String>());
-      }
-    }
-    previousTreePath = currentPathList;
-    if (previousTreePath.size() != stackNames.size()) {
-      throw new IllegalStateException();
-    }
-
-    final String chosenName;
-    search:
-    for (int i = 0; ; i++) {
-      final String identifierName;
-      if (i == 0) {
-        identifierName = name;
-      } else {
-        identifierName = name + i;
-      }
-
-      for (Set<String> set : stackNames) {
-        if (set.contains(identifierName)) {
-          continue search;
-        }
-      }
-      if (FindIdentifiers.findIdent(identifierName, state) == null) {
-        chosenName = identifierName;
-        break;
-      }
-    }
-    for (int i = stackNames.size() - 1; i >= 0; i--) {
-      if (declaresVariableScope(currentPathList.get(i).getKind())) {
-        stackNames.get(i).add(chosenName);
-        return chosenName;
-      }
-    }
-    throw new IllegalStateException("Didn't find enclosing block");
-  }
-
-  boolean declaresVariableScope(Kind kind) {
-    switch (kind) {
-      case BLOCK:
-      case METHOD:
-        return true;
-      default:
-        return false;
-    }
-  }
-
-  /** Fixes the error by assigning the result of the call to a new local variable. */
-  public Description describeUnused(MethodInvocationTree methodInvocationTree, VisitorState state) {
-    // Fix by assigning the assigning the result of the call to an unused variable.
-    SuggestedFix fix =
-        SuggestedFix.builder()
-            .addImport("java.util.concurrent.Future")
-            .prefixWith(
-                methodInvocationTree,
-                String.format(
-                    "@SuppressWarnings(\"unused\") // go/futurereturn-lsc\n" + "Future<?> %s = ",
-                    findVariableName("possiblyIgnoredError", state)))
-            .build();
-    return describeMatch(methodInvocationTree, fix);
   }
 }
