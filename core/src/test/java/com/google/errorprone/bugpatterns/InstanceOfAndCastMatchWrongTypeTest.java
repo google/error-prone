b605305b546a895c1b63352dbd370cd7171bfba3
glorioso <glorioso@google.com>
2017-06-29 12:15:32 -0700

    Add regression test for issue 651
    RELNOTES: n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=160556571

--- core/src/test/java/com/google/errorprone/bugpatterns/InstanceOfAndCastMatchWrongTypeTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/InstanceOfAndCastMatchWrongTypeTest.java
@@ -17,6 +17,7 @@ package com.google.errorprone.bugpatterns;
 
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -43,5 +44,24 @@ public class InstanceOfAndCastMatchWrongTypeTest {
   @Test
   public void testNegativeCase() throws Exception {
     compilationHelper.addSourceFile("InstanceOfAndCastMatchWrongTypeNegativeCases.java").doTest();
+  }
+
+  @Ignore("regression for issue 651, not yet fixed")
+  @Test
+  public void regressionTestIssue651() throws Exception {
+    compilationHelper
+        .addSourceLines(
+            "Foo.java",
+            "class Foo {",
+            "  void foo() {",
+            "    Object[] values = null;",
+            "    if (values[0] instanceof Integer) {",
+            "      int x = (Integer) values[0];",
+            "    } else if (values[0] instanceof Long) {",
+            "      long y = (Long) values[0];",
+            "    }",
+            "  }",
+            "}")
+        .doTest();
   }
 }
