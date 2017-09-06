2ba786ed87f5df09a06d601dee3b633ff8e59737
glorioso <glorioso@google.com>
2017-09-06 15:29:00 -0700

    Add a checker that checks to see if Arrays.fill(Object[], Object) is called with incompatible types.
    RELNOTES: New error: ArrayFillIncompatibleType. Triggered when
    Arrays.fill(Object[], Object) is called when the second argument can't safely
    be inserted into the array parameter (e.g. storing Integer into a String[]).
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=167782668

---
+++ core/src/test/java/com/google/errorprone/bugpatterns/ArrayFillIncompatibleTypeTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link ArrayFillIncompatibleType} */
+@RunWith(JUnit4.class)
+public class ArrayFillIncompatibleTypeTest {
+  private final CompilationTestHelper compilationHelper =
+      CompilationTestHelper.newInstance(ArrayFillIncompatibleType.class, getClass());
+
+  @Test
+  public void testPrimitiveBoxingIntoObject() throws Exception {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Arrays;",
+            "class Test {",
+            "  void something(boolean b, Object[] o) {",
+            "     Arrays.fill(o, b);",
+            "  }",
+            "}")
+        .setArgs(Arrays.asList("-source", "1.6", "-target", "1.6"))
+        .doTest();
+  }
+
+  @Test
+  public void testPositive() throws Exception {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Arrays;",
+            "class Test {",
+            "  void something(String b, Integer[] o, Number n) {",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(o, b);",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(o, 2.0d);",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(o, n);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void testTernary() throws Exception {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Arrays;",
+            "class Test {",
+            "  enum Foo {BAR, BAZ};",
+            "  void something(Foo[] o, String[] s) {",
+            "     Arrays.fill(o, true ? Foo.BAR : Foo.BAZ);",
+            "     Arrays.fill(s, true ? \"a\" : \"b\");",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(s, true ? \"a\" : 123);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void testBoxing() throws Exception {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Arrays;",
+            "class Test {",
+            "  void something(int i, long l, Integer[] o) {",
+            "     Arrays.fill(o, 1);",
+            "     Arrays.fill(o, i);",
+            "     Arrays.fill(o, 0, 4, i);",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(o, l);",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(o, 4L);",
+            "     // BUG: Diagnostic contains: ",
+            "     Arrays.fill(o, 0, 4, l);",
+            "  }",
+            "}")
+        .doTest();
+  }
+}
---
+++ docs/bugpattern/ArrayFillIncompatibleType.md
@@ -0,0 +1,31 @@
+`Arrays.fill(Object[], Object)` is used to copy a reference into every slot of
+an array.
+
+For example:
+
+```java
+String[] foo = new String[42];
+Arrays.fill(foo, "life");
+// 42 references to the same String instance of "life" in the foo array
+```
+
+However, because of Array covariance (e.g.: `String[]` is assignable to
+`Object[]`), and the signature of Arrays.fill is `Arrays.fill(Object[],
+Object)`, this also allows you to do the following:
+
+```java
+String[] foo = new String[42];
+Arrays.fill(foo, 42); // ArrayStoreException! Integer can't be put into a String[]
+```
+
+This check detects the above circumstances, and won't let you attempt to put
+`Integer`s into a `String[]`.
+
+## What about Lists?
+
+`List<T>` doesn't have the same issue, since generic types are _not_ covariant.
+
+```java
+List<String> foo = new ArrayList<>();
+foo.add(42); // Compile time error: Integer is not assignable to String
+```
