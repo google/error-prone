2f29d1c0c62e7b9db51c5a49095ef40b1094c773
bhagwani <bhagwani@google.com>
2018-01-02 12:37:34 -0800

    Fix FlagSpecWithRedundantAltName check for flags with explicit empty name
    This is the common case with generated flags.
    
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=180579301

--- check_api/src/main/java/com/google/errorprone/fixes/SuggestedFixes.java
+++ check_api/src/main/java/com/google/errorprone/fixes/SuggestedFixes.java
@@ -571,6 +571,35 @@ public class SuggestedFixes {
     }
   }
 
+  /**
+   * Returns a fix that updates {@code newValues} to the {@code parameterName} argument for {@code
+   * annotation}, regardless of whether there is already an argument.
+   *
+   * <p>N.B.: {@code newValues} are source-code strings, not string literal values.
+   */
+  public static Builder updateAnnotationArgumentValues(
+      AnnotationTree annotation, String parameterName, Collection<String> newValues) {
+    if (annotation.getArguments().isEmpty()) {
+      String parameterPrefix = parameterName.equals("value") ? "" : (parameterName + " = ");
+      return SuggestedFix.builder()
+          .replace(
+              annotation,
+              annotation
+                  .toString()
+                  .replaceFirst("\\(\\)", "(" + parameterPrefix + newArgument(newValues) + ")"));
+    }
+    Optional<ExpressionTree> maybeExistingArgument = findArgument(annotation, parameterName);
+    if (!maybeExistingArgument.isPresent()) {
+      return SuggestedFix.builder()
+          .prefixWith(
+              annotation.getArguments().get(0),
+              parameterName + " = " + newArgument(newValues) + ", ");
+    }
+
+    ExpressionTree existingArgument = maybeExistingArgument.get();
+    return SuggestedFix.builder().replace(existingArgument, newArgument(newValues));
+  }
+
   private static String newArgument(String existingParameters, Collection<String> initializers) {
     return newArgument(
         ImmutableList.<String>builder().add(existingParameters).addAll(initializers).build());
