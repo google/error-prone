311bd862e2cca52314b30dc83239ccaf19453974
cushon <cushon@google.com>
2018-01-05 08:30:44 -0800

    Create a helper method for inferring target types
    MOE_MIGRATED_REVID=180930978

--- core/src/main/java/com/google/errorprone/bugpatterns/NullTernary.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/NullTernary.java
@@ -20,22 +20,13 @@ import static com.google.errorprone.BugPattern.Category.JDK;
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
 import static com.google.errorprone.matchers.Description.NO_MATCH;
 
-import com.google.common.base.Preconditions;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker.ConditionalExpressionTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.util.ASTHelpers;
-import com.sun.source.tree.BinaryTree;
 import com.sun.source.tree.ConditionalExpressionTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.Tree;
 import com.sun.source.tree.Tree.Kind;
-import com.sun.source.tree.VariableTree;
-import com.sun.source.util.SimpleTreeVisitor;
-import com.sun.source.util.TreePath;
-import com.sun.tools.javac.code.Symbol.MethodSymbol;
-import com.sun.tools.javac.code.Type;
 
 /** @author cushon@google.com (Liam Miller-Cushon) */
 @BugPattern(
@@ -55,50 +46,8 @@ public class NullTernary extends BugChecker implements ConditionalExpressionTree
         && conditionalExpression.getTrueExpression().getKind() != Kind.NULL_LITERAL) {
       return NO_MATCH;
     }
-    TreePath path = state.getPath();
-    do {
-      path = path.getParentPath();
-    } while (path != null && path.getLeaf().getKind() == Kind.PARENTHESIZED);
-    if (path == null) {
-      return NO_MATCH;
-    }
-    Tree parent = path.getLeaf();
-    Type type =
-        parent.accept(
-            new SimpleTreeVisitor<Type, Void>() {
-              @Override
-              public Type visitMethodInvocation(MethodInvocationTree tree, Void unused) {
-                int idx = tree.getArguments().indexOf(conditionalExpression);
-                if (idx == -1) {
-                  return null;
-                }
-                MethodSymbol sym = ASTHelpers.getSymbol(tree);
-                if (sym.getParameters().size() <= idx) {
-                  Preconditions.checkState(sym.isVarArgs());
-                  idx = sym.getParameters().size() - 1;
-                }
-                Type type = sym.getParameters().get(idx).asType();
-                if (sym.isVarArgs() && idx == sym.getParameters().size() - 1) {
-                  type = state.getTypes().elemtype(type);
-                }
-                return type;
-              }
-
-              @Override
-              public Type visitVariable(VariableTree tree, Void unused) {
-                return ASTHelpers.getType(tree);
-              }
-
-              @Override
-              public Type visitBinary(BinaryTree tree, Void unused) {
-                return ASTHelpers.getType(
-                    ASTHelpers.stripParentheses(tree.getLeftOperand()).equals(conditionalExpression)
-                        ? tree.getRightOperand()
-                        : tree.getLeftOperand());
-              }
-            },
-            null);
-    if (type == null || !type.isPrimitive()) {
+    ASTHelpers.TargetType targetType = ASTHelpers.targetType(state);
+    if (targetType == null || !targetType.type().isPrimitive()) {
       return NO_MATCH;
     }
     return describeMatch(conditionalExpression);
