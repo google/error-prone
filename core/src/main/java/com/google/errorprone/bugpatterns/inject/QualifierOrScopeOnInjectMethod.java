4af22f30d10f0fef7d50aab9df116cc151dc7445
glorioso <glorioso@google.com>
2018-01-12 15:31:28 -0800

    Enable QualifierOrScopeOnInjectedMethod as a WARNING (previously, it was a disabled ERROR).
    RELNOTES: Enable QualifierOrScopeOnInjectedMethod as a WARNING
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=181801803

--- core/src/main/java/com/google/errorprone/bugpatterns/inject/QualifierOrScopeOnInjectMethod.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/inject/QualifierOrScopeOnInjectMethod.java
@@ -28,6 +28,7 @@ import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.Category;
 import com.google.errorprone.BugPattern.ProvidesFix;
 import com.google.errorprone.BugPattern.SeverityLevel;
+import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
@@ -51,7 +52,8 @@ import java.util.List;
   summary =
       "Qualifiers/Scope annotations on @Inject methods don't have any effect."
           + " Move the qualifier annotation to the binding location.",
-  severity = SeverityLevel.ERROR,
+  severity = SeverityLevel.WARNING,
+  tags = StandardTags.LIKELY_ERROR,
   providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION
 )
 public class QualifierOrScopeOnInjectMethod extends BugChecker implements MethodTreeMatcher {
