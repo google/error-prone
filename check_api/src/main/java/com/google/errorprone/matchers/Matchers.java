b731ac393a5ba07498d5f4470645c1e8465e53cd
bhagwani <bhagwani@google.com>
2018-01-16 14:51:15 -0800

    Matcher which matches a node that is directly an implementation, but not extension, of the given class
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=182115566

--- check_api/src/main/java/com/google/errorprone/matchers/Matchers.java
+++ check_api/src/main/java/com/google/errorprone/matchers/Matchers.java
@@ -1430,4 +1430,31 @@ public class Matchers {
     };
   }
 
+  /**
+   * Matches any node that is directly an implementation, but not extension, of the given Class.
+   *
+   * <p>E.x. {@code class C implements I} will match, but {@code class C extends A} will not.
+   *
+   * <p>Additionally, this will only match <i>direct</i> implementations of interfaces. E.g. the
+   * following will not match:
+   *
+   * <p>{@code interface I1 {} interface I2 extends I1 {} class C implements I2 {} ...
+   * isDirectImplementationOf(I1).match(\/*class tree for C*\/); // will not match }
+   */
+  public static Matcher<ClassTree> isDirectImplementationOf(String clazz) {
+    Matcher<Tree> isProvidedType = isSameType(clazz);
+    return new IsDirectImplementationOf(isProvidedType);
+  }
+
+  private static class IsDirectImplementationOf extends ChildMultiMatcher<ClassTree, Tree> {
+    public IsDirectImplementationOf(Matcher<Tree> classMatcher) {
+      super(MatchType.AT_LEAST_ONE, classMatcher);
+    }
+
+    @Override
+    protected Iterable<? extends Tree> getChildNodes(ClassTree classTree, VisitorState state) {
+      return classTree.getImplementsClause();
+    }
+  }
+
 }
