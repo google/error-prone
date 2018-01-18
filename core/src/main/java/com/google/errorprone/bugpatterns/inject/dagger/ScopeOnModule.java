190bc1c160811916374ed694f320b90bf5d73759
ronshapiro <ronshapiro@google.com>
2018-01-18 11:47:46 -0800

    Add a suggested check for removing javax.inject.Scopes from Dagger modules
    RELNOTES: Add a migration for Dagger users that are incorrectly applying scope annotations on Module declarations
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=182413796

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/inject/dagger/ScopeOnModule.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns.inject.dagger;
+
+import static com.google.errorprone.BugPattern.Category.DAGGER;
+import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
+import static com.google.errorprone.util.ASTHelpers.getSymbol;
+import static com.google.errorprone.util.ASTHelpers.hasAnnotation;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.ProvidesFix;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.ClassTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Symbol.ClassSymbol;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Migrate users who use JSR 330 scopes on Dagger modules. */
+@BugPattern(
+  name = "ScopeOnModule",
+  summary = "Scopes on modules have no function and will soon be an error.",
+  explanation =
+      "Scopes on modules have no function and are often incorrectly assumed to apply the scope to "
+          + "every binding method in the module. This check removes all scope annotations from any "
+          + "class annotated with `@Module` or `@ProducerModule`.",
+  category = DAGGER,
+  severity = SUGGESTION,
+  providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION
+)
+public final class ScopeOnModule extends BugChecker implements ClassTreeMatcher {
+  @Override
+  public Description matchClass(ClassTree classTree, VisitorState state) {
+    ClassSymbol classSymbol = getSymbol(classTree);
+    if (!hasAnnotation(classSymbol, "dagger.Module", state)
+        && !hasAnnotation(classSymbol, "dagger.producers.ProducerModule", state)) {
+      return Description.NO_MATCH;
+    }
+
+    List<SuggestedFix> fixes = new ArrayList<>();
+    for (AnnotationTree annotation : classTree.getModifiers().getAnnotations()) {
+      Symbol annotationType = getSymbol(annotation.getAnnotationType());
+      if (hasAnnotation(annotationType, "javax.inject.Scope", state)) {
+        fixes.add(SuggestedFix.delete(annotation));
+      }
+    }
+    if (fixes.isEmpty()) {
+      return Description.NO_MATCH;
+    }
+
+    return buildDescription(classTree).addAllFixes(fixes).build();
+  }
+}
