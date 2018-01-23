8b72d88693f937ef66e3b9c01edf31c7168d4776
clm <clm@google.com>
2018-01-22 16:06:12 -0800

    Detect returned Futures that have their types ignored due to wildcard or other generics.
    Example error message:
    /com/google/errorprone/bugpatterns/FutureReturnValueIgnoredPositiveCases.java:144: warning: [FutureReturnValueIgnored] Invocation produces a nested Future - Type variable O, as part of return type com.google.common.util.concurrent.ListenableFuture<O> resolved to com.google.common.util.concurrent.ListenableFuture<java.lang.Integer>, because param `function` was passed as <anonymous com.google.common.base.Function<java.lang.String,com.google.common.util.concurrent.ListenableFuture<java.lang.Integer>>>.
                new Function<String, ListenableFuture<Integer>>() {
                        ^
    RELNOTES: Detect more cases of nested and ignored futures.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=182852358

--- core/src/main/java/com/google/errorprone/bugpatterns/FutureReturnValueIgnored.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/FutureReturnValueIgnored.java
@@ -16,26 +16,56 @@
 
 package com.google.errorprone.bugpatterns;
 
+import static com.google.common.collect.Multimaps.toMultimap;
 import static com.google.errorprone.BugPattern.Category.JDK;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.allOf;
 import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.not;
+import static com.google.errorprone.matchers.Matchers.parentNode;
 import static com.google.errorprone.matchers.method.MethodMatchers.instanceMethod;
 import static com.google.errorprone.util.ASTHelpers.hasAnnotation;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.Streams;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.errorprone.bugpatterns.BugChecker.ReturnTreeMatcher;
+import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.LambdaExpressionTree;
+import com.sun.source.tree.MemberReferenceTree;
+import com.sun.source.tree.MemberReferenceTree.ReferenceMode;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
+import com.sun.tools.javac.code.Symbol.TypeVariableSymbol;
 import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.tree.JCTree.JCLambda;
+import com.sun.tools.javac.tree.JCTree.JCMemberReference;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ForkJoinTask;
 import java.util.regex.Pattern;
+import javax.lang.model.type.TypeKind;
 
 /** See BugPattern annotation. */
 @BugPattern(
@@ -47,12 +77,16 @@ import java.util.regex.Pattern;
       "Methods that return `java.util.concurrent.Future` and its subclasses "
           + "generally indicate errors by returning a future that eventually fails.\n\n"
           + "If you don’t check the return value of these methods, you will never find out if they "
-          + "threw an exception.",
+          + "threw an exception. \n\n Nested futures can also result in missed cancellation "
+          + "signals or suppressed exceptions - see [Avoiding Nested Futures]"
+          + "(https://github.com/google/guava/wiki/ListenableFutureExplained#avoid-nested-futures) "
+          + "for details.",
   category = JDK,
   severity = WARNING,
   tags = StandardTags.FRAGILE_CODE
 )
-public final class FutureReturnValueIgnored extends AbstractReturnValueIgnored {
+public final class FutureReturnValueIgnored extends AbstractReturnValueIgnored
+    implements ReturnTreeMatcher {
 
   private static final Matcher<ExpressionTree> BLACKLIST =
       anyOf(
@@ -71,7 +105,7 @@ public final class FutureReturnValueIgnored extends AbstractReturnValueIgnored {
           instanceMethod()
               .onDescendantOf("com.intellij.openapi.application.Application")
               .named("executeOnPooledThread"),
-          // ChannelFuture#addListern(s) returns itself for chaining. Any exception during the
+          // ChannelFuture#addListener(s) returns itself for chaining. Any exception during the
           // future execution should be dealt by the listener(s).
           instanceMethod()
               .onDescendantOf("io.netty.channel.ChannelFuture")
@@ -88,7 +122,10 @@ public final class FutureReturnValueIgnored extends AbstractReturnValueIgnored {
               Objects.requireNonNull(state.getTypeFromString("java.util.concurrent.Future"));
           Symbol untypedSymbol = ASTHelpers.getSymbol(tree);
           if (!(untypedSymbol instanceof MethodSymbol)) {
-            return false;
+            Type resultType = ASTHelpers.getResultType(tree);
+            return resultType != null
+                && ASTHelpers.isSubtype(
+                    ASTHelpers.getUpperBound(resultType, state.getTypes()), futureType, state);
           }
           MethodSymbol sym = (MethodSymbol) untypedSymbol;
           if (hasAnnotation(sym, CanIgnoreReturnValue.class, state)) {
@@ -116,7 +153,268 @@ public final class FutureReturnValueIgnored extends AbstractReturnValueIgnored {
       };
 
   @Override
-  public Matcher<? super ExpressionTree> specializedMatcher() {
+  public Matcher<ExpressionTree> specializedMatcher() {
     return MATCHER;
+  }
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    Description description = super.matchMethodInvocation(tree, state);
+    if (Description.NO_MATCH == description) {
+      return checkLostType(tree, state);
+    } else {
+      return description;
+    }
+  }
+
+  private Description checkLostType(MethodInvocationTree tree, VisitorState state) {
+    Type futureType =
+        Objects.requireNonNull(state.getTypeFromString("java.util.concurrent.Future"));
+
+    MethodSymbol sym = ASTHelpers.getSymbol(tree);
+    Type returnType = ASTHelpers.getResultType(tree);
+    Type returnedFutureType = state.getTypes().asSuper(returnType, futureType.tsym);
+    if (returnedFutureType != null && !returnedFutureType.isRaw()) {
+      if (ASTHelpers.isSubtype(
+          ASTHelpers.getUpperBound(returnedFutureType.getTypeArguments().get(0), state.getTypes()),
+          futureType,
+          state)) {
+        return buildDescription(tree)
+            .setMessage(String.format("Method returns a nested type, %s", returnType))
+            .build();
+      }
+
+      // The type variable that determines the generic on the returned future was not a Future.
+      // However, many methods (like guava's Futures.transform) have signatures like this:
+      // Future<O> do(SomeObject<? extends O>). If O resolves to java.lang.Object or ?, then a
+      // SomeObject<Future> is a valid parameter to pass, but results in a nested future.
+      Type methodReturnType = sym.getReturnType();
+      List<TypeVariableSymbol> typeParameters = sym.getTypeParameters();
+      Set<TypeVariableSymbol> returnTypeChoosing = new HashSet<>();
+      // For each type variable on the method, see if we can reach the type declared as the param
+      // of the returned Future, by traversing its type bounds. If we can reach it, we know that if
+      // an argument is passed to an invocation of this method where the type variable is a subtype
+      // of Future, that means that a nested Future is being returned.
+      for (TypeVariableSymbol tvs : typeParameters) {
+        Queue<TypeVariableSymbol> queue = new ArrayDeque<>();
+        queue.add(tvs);
+        while (!queue.isEmpty()) {
+          TypeVariableSymbol currentTypeParam = queue.poll();
+          for (Type typeParam : methodReturnType.getTypeArguments()) {
+            if (typeParam.tsym == currentTypeParam) {
+              returnTypeChoosing.add(tvs);
+            }
+          }
+          for (Type toAdd : currentTypeParam.getBounds()) {
+            if (toAdd.tsym instanceof TypeVariableSymbol) {
+              queue.add((TypeVariableSymbol) toAdd.tsym);
+            }
+          }
+        }
+      }
+      // If at least one of the method's type parameters is involved in determining the returned
+      // Future's type, check each passed parameter to ensure that it is never passed as a subtype
+      // of Future.
+      if (!returnTypeChoosing.isEmpty()) {
+        Multimap<TypeVariableSymbol, TypeInfo> resolved = getResolvedGenerics(tree);
+        for (TypeVariableSymbol returnTypeChoosingSymbol : returnTypeChoosing) {
+          Collection<TypeInfo> types = resolved.get(returnTypeChoosingSymbol);
+          for (TypeInfo type : types) {
+            if (ASTHelpers.isSubtype(type.resolvedVariableType, futureType, state)) {
+              return buildDescription(type.tree)
+                  .setMessage(
+                      String.format(
+                          "Invocation produces a nested type - Type variable %s, as part of return "
+                              + "type %s resolved to %s.",
+                          returnTypeChoosingSymbol, methodReturnType, type.resolvedVariableType))
+                  .build();
+            }
+          }
+        }
+      }
+    }
+    if (allOf(
+            allOf(
+                parentNode(FutureReturnValueIgnored::isObjectReturningLambdaExpression),
+                not(AbstractReturnValueIgnored::expectedExceptionTest)),
+            specializedMatcher(),
+            not((t, s) -> ASTHelpers.isVoidType(ASTHelpers.getType(t), s)))
+        .matches(tree, state)) {
+      return describe(tree, state);
+    }
+
+    return Description.NO_MATCH;
+  }
+
+  private static final class TypeInfo {
+
+    private final TypeVariableSymbol sym;
+    private final Type resolvedVariableType;
+    private final Tree tree;
+
+    private TypeInfo(TypeVariableSymbol sym, Type resolvedVariableType, Tree tree) {
+      this.sym = sym;
+      this.resolvedVariableType = resolvedVariableType;
+      this.tree = tree;
+    }
+  }
+
+  private static Multimap<TypeVariableSymbol, TypeInfo> getResolvedGenerics(
+      MethodInvocationTree tree) {
+    Type type = ASTHelpers.getType(tree.getMethodSelect());
+    List<Type> from = new ArrayList<>();
+    List<Type> to = new ArrayList<>();
+    getSubst(type, from, to);
+    Multimap<TypeVariableSymbol, TypeInfo> result =
+        Streams.zip(
+                from.stream(),
+                to.stream(),
+                (f, t) -> new TypeInfo((TypeVariableSymbol) f.asElement(), t, tree))
+            .collect(
+                toMultimap(
+                    k -> k.sym, k -> k, MultimapBuilder.linkedHashKeys().arrayListValues()::build));
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void getSubst(Type m, List<Type> from, List<Type> to) {
+    try {
+      // Reflectively extract the mapping from an enclosing instance of Types.Subst
+      Field substField = m.getClass().getDeclaredField("this$0");
+      substField.setAccessible(true);
+      Object subst = substField.get(m);
+      Field fromField = subst.getClass().getDeclaredField("from");
+      Field toField = subst.getClass().getDeclaredField("to");
+      fromField.setAccessible(true);
+      toField.setAccessible(true);
+      from.addAll((Collection<Type>) fromField.get(subst));
+      to.addAll((Collection<Type>) toField.get(subst));
+    } catch (ReflectiveOperationException e) {
+      return;
+    }
+  }
+
+  private static boolean isObjectReturningMethodReferenceExpression(
+      MemberReferenceTree tree, VisitorState state) {
+    return functionalInterfaceReturnsObject(((JCMemberReference) tree).type, state);
+  }
+
+  private static boolean isObjectReturningLambdaExpression(Tree tree, VisitorState state) {
+    if (!(tree instanceof LambdaExpressionTree)) {
+      return false;
+    }
+
+    Type type = ((JCLambda) tree).type;
+    return functionalInterfaceReturnsObject(type, state)
+        && !isWhitelistedInterfaceType(type, state);
+  }
+
+  /**
+   * Checks that the return value of a functional interface is void. Note, we do not use
+   * ASTHelpers.isVoidType here, return values of Void are actually type-checked. Only
+   * void-returning functions silently ignore return values of any type.
+   */
+  private static boolean functionalInterfaceReturnsObject(Type interfaceType, VisitorState state) {
+    Type objectType = Objects.requireNonNull(state.getTypeFromString("java.lang.Object"));
+    return ASTHelpers.isSubtype(
+        objectType,
+        ASTHelpers.getUpperBound(
+            state.getTypes().findDescriptorType(interfaceType).getReturnType(), state.getTypes()),
+        state);
+  }
+
+  /**
+   * Detect member references that implement an interface that return Object, but resolve to a
+   * method that returns Future.
+   */
+  @Override
+  public Description matchMemberReference(MemberReferenceTree tree, VisitorState state) {
+    Description description = super.matchMemberReference(tree, state);
+    if (Description.NO_MATCH == description) {
+      if (allOf(
+              (t, s) -> t.getMode() == ReferenceMode.INVOKE,
+              FutureReturnValueIgnored::isObjectReturningMethodReferenceExpression,
+              not((t, s) -> isWhitelistedInterfaceType(((JCMemberReference) t).type, s)),
+              not((t, s) -> isThrowingFunctionalInterface(s, ((JCMemberReference) t).type)),
+              specializedMatcher())
+          .matches(tree, state)) {
+        return describeMatch(tree);
+      }
+    }
+    return description;
+  }
+
+  private static final ImmutableSet<String> WHITELISTED_TYPES =
+      ImmutableSet.of(
+          "org.mockito.stubbing.Answer",
+          "graphql.schema.DataFetcher",
+          "org.jmock.lib.action.CustomAction",
+          "net.sf.cglib.proxy.MethodInterceptor",
+          "org.aopalliance.intercept.MethodInterceptor",
+          InvocationHandler.class.getName());
+
+  private static boolean isWhitelistedInterfaceType(Type type, VisitorState state) {
+    return WHITELISTED_TYPES
+        .stream()
+        .map(state::getTypeFromString)
+        .anyMatch(whitelistedType -> ASTHelpers.isSubtype(type, whitelistedType, state));
+  }
+
+  private static boolean isWhitelistedInterfaceMethod(MethodSymbol symbol, VisitorState state) {
+    return isWhitelistedInterfaceType(ASTHelpers.enclosingClass(symbol).type, state);
+  }
+
+  /**
+   * Returning a type of Future from a lambda or method that returns Object loses the Future type,
+   * which can result in suppressed errors or race conditions.
+   */
+  @Override
+  public Description matchReturn(ReturnTree tree, VisitorState state) {
+    Type objectType = Objects.requireNonNull(state.getTypeFromString("java.lang.Object"));
+    Type futureType =
+        Objects.requireNonNull(state.getTypeFromString("java.util.concurrent.Future"));
+    Type resultType = ASTHelpers.getResultType(tree.getExpression());
+    if (resultType == null) {
+      return Description.NO_MATCH;
+    }
+    if (resultType.getKind() == TypeKind.NULL || resultType.getKind() == TypeKind.NONE) {
+      return Description.NO_MATCH;
+    }
+    if (ASTHelpers.isSubtype(resultType, futureType, state)) {
+      // Traverse enclosing nodes of this return tree until either a lambda or a Method is reached.
+      for (Tree enclosing : state.getPath()) {
+        if (enclosing instanceof MethodTree) {
+          MethodTree methodTree = (MethodTree) enclosing;
+          MethodSymbol symbol = ASTHelpers.getSymbol(methodTree);
+          if (ASTHelpers.isSubtype(objectType, symbol.getReturnType(), state)
+              && !isWhitelistedInterfaceMethod(symbol, state)) {
+            return buildDescription(tree)
+                .setMessage(
+                    String.format(
+                        "Returning %s from method that returns %s. Errors from the returned future"
+                            + " may be ignored.",
+                        resultType, symbol.getReturnType()))
+                .build();
+          } else {
+            break;
+          }
+        }
+        if (enclosing instanceof LambdaExpressionTree) {
+          LambdaExpressionTree lambdaTree = (LambdaExpressionTree) enclosing;
+          if (isObjectReturningLambdaExpression(lambdaTree, state)) {
+            return buildDescription(tree)
+                .setMessage(
+                    String.format(
+                        "Returning %s from method that returns Object. Errors from the returned"
+                            + " future will be ignored.",
+                        resultType))
+                .build();
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    return Description.NO_MATCH;
   }
 }
--- core/src/test/java/com/google/errorprone/bugpatterns/testdata/FutureReturnValueIgnoredNegativeCases.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/testdata/FutureReturnValueIgnoredNegativeCases.java
@@ -17,18 +17,42 @@
 package com.google.errorprone.bugpatterns.testdata;
 
 import static com.google.common.util.concurrent.Futures.immediateFuture;
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.doAnswer;
 
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Supplier;
+import com.google.common.base.Ticker;
+import com.google.common.reflect.AbstractInvocationHandler;
+import com.google.common.util.concurrent.AbstractFuture;
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import io.netty.channel.ChannelFuture;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.ScheduledExecutorService;
+import org.junit.function.ThrowingRunnable;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 /** */
 public class FutureReturnValueIgnoredNegativeCases {
@@ -96,5 +120,219 @@ public class FutureReturnValueIgnoredNegativeCases {
 
   void ignoreChannelFutureAddListeners(ChannelFuture cf) {
     cf.addListeners((ChannelFuture f) -> {}, (ChannelFuture f) -> {});
+  }
+
+  <V> ListenableFuture<V> ignoreVarArgs(
+      Callable<V> combiner, Executor executor, ListenableFuture<?>... futures) {
+    return combine(combiner, executor, Arrays.asList(futures));
+  }
+
+  public static <V> ListenableFuture<V> combine(
+      final Callable<V> combiner,
+      Executor executor,
+      Iterable<? extends ListenableFuture<?>> futures) {
+    return null;
+  }
+
+  private static final class TypedClass<T> {
+    ListenableFuture<Void> ignoreReturnTypeSetByInputFuture(T input) {
+      return returnsInputType(logAsyncInternal(input), 0);
+    }
+
+    protected ListenableFuture<Void> logAsyncInternal(T record) {
+      return null;
+    }
+
+    <V> ListenableFuture<V> returnsInputType(ListenableFuture<V> future, final int n) {
+      return null;
+    }
+  }
+
+  public static <T, E extends Exception> CheckedFuture<List<T>, E> allAsList(
+      CheckedFuture<? extends T, E>... futures) {
+    return asListHelper(Futures.allAsList(null, null), futures);
+  }
+
+  private static <T, E extends Exception> CheckedFuture<List<T>, E> asListHelper(
+      ListenableFuture<List<T>> future, CheckedFuture<? extends T, E>... originalFutures) {
+    return asListHelper(future, Arrays.asList(originalFutures));
+  }
+
+  /**
+   * Helper method for Iterable allAsList and successfulAsList calls which special-cases empty
+   * iterables.
+   *
+   * @param future the future to make checked
+   * @param originalFutures the original futures
+   * @return a checked version of the future argument
+   */
+  private static <T, E extends Exception> CheckedFuture<List<T>, E> asListHelper(
+      ListenableFuture<List<T>> future,
+      Iterable<? extends CheckedFuture<? extends T, E>> originalFutures) {
+    Iterator<? extends CheckedFuture<? extends T, E>> iterator = originalFutures.iterator();
+    if (!iterator.hasNext()) {
+      return null;
+    }
+    return null;
+  }
+
+  public static class RetryingFuture<T> extends AbstractFuture<T> {
+
+    /**
+     * Enables the user to receive notifications about state changes of a retrying future, and use
+     * them e.g. for logging.
+     */
+    public interface Interceptor<T> {}
+
+    /** Creates a builder for {@link RetryingFuture} instances. */
+    public static Builder<Object> builder() {
+      return new Builder<>();
+    }
+
+    /** A builder for {@link RetryingFuture} instances. */
+    public static final class Builder<T> {
+
+      private Builder() {}
+
+      /** Sets the {@link Executor} in which all tries and retries are performed. */
+      @CanIgnoreReturnValue
+      public Builder<T> setExecutor(Executor executor) {
+        return this;
+      }
+
+      /**
+       * Sets the {@link ScheduledExecutorService} used for scheduling retries after delay. It will
+       * also be used for tries and retries if {@link #setExecutor(Executor)} is not called.
+       */
+      @CanIgnoreReturnValue
+      public Builder<T> setScheduledExecutorService(
+          ScheduledExecutorService scheduledExecutorService) {
+        return this;
+      }
+
+      public <U extends T> Builder<U> setInterceptor(Interceptor<U> interceptor) {
+        // Safely limiting the kinds of RetryingFutures this builder can produce,
+        // based on the type of the interceptor.
+        @SuppressWarnings("unchecked")
+        Builder<U> me = (Builder<U>) this;
+        return me;
+      }
+
+      public Builder<T> setTicker(Ticker ticker) {
+        return this;
+      }
+
+      public <U extends T> RetryingFuture<U> build(
+          Supplier<? extends ListenableFuture<U>> futureSupplier,
+          Predicate<? super Exception> shouldContinue) {
+        return new RetryingFuture<U>(
+            futureSupplier,
+            null,
+            shouldContinue,
+            null,
+            // We need to maintain Java 7 compatibility
+            null,
+            null,
+            null);
+      }
+
+      public <U extends T> RetryingFuture<U> build(
+          Supplier<? extends ListenableFuture<U>> futureSupplier,
+          Object strategy,
+          Predicate<? super Exception> shouldContinue) {
+        return new RetryingFuture<U>(
+            futureSupplier,
+            strategy,
+            shouldContinue,
+            null,
+            // We need to maintain Java 7 compatibility
+            null,
+            null,
+            null);
+      }
+    }
+
+    RetryingFuture(
+        Supplier<? extends ListenableFuture<T>> futureSupplier,
+        Object strategy,
+        Predicate<? super Exception> shouldContinue,
+        Executor executor,
+        ScheduledExecutorService scheduledExecutorService,
+        Ticker ticker,
+        final Interceptor<? super T> interceptor) {}
+
+    public static <T> RetryingFuture<T> retryingFuture(
+        Supplier<? extends ListenableFuture<T>> futureSupplier,
+        Object strategy,
+        Predicate<? super Exception> shouldContinue,
+        Executor executor,
+        Interceptor<? super T> interceptor) {
+      return builder()
+          .setInterceptor(interceptor)
+          .setExecutor(executor)
+          .build(futureSupplier, strategy, shouldContinue);
+    }
+  }
+
+  private static class TypedObject<T> {
+    public <O extends Object> ListenableFuture<O> transformAndClose(
+        Function<? super T, O> function, Executor executor) {
+      return null;
+    }
+
+    public ListenableFuture<T> close() {
+      return transformAndClose(Functions.identity(), directExecutor());
+    }
+  }
+
+  private static void mocking() {
+    doAnswer(invocation -> immediateFuture(null)).when(null);
+    doAnswer(
+            invocation -> {
+              return immediateFuture(null);
+            })
+        .when(null);
+    doAnswer(
+            new Answer() {
+              @Override
+              public Object answer(InvocationOnMock mock) {
+                return immediateFuture(null);
+              }
+            })
+        .when(null);
+  }
+
+  private static void throwing() {
+    assertThrows(RuntimeException.class, () -> immediateFuture(null));
+    assertThrows(
+        RuntimeException.class,
+        () -> {
+          immediateFuture(null);
+        });
+    assertThrows(
+        RuntimeException.class,
+        new ThrowingRunnable() {
+          @Override
+          public void run() throws Throwable {
+            immediateFuture(null);
+          }
+        });
+  }
+
+  private static AsyncFunction<String, String> provideAsyncFunction() {
+    return Futures::immediateFuture;
+  }
+
+  private static Runnable provideNonFutureInterface() {
+    return new FutureTask(null);
+  }
+
+  private static void invocation() {
+    new AbstractInvocationHandler() {
+      @Override
+      protected Object handleInvocation(Object o, Method method, Object[] params) {
+        return immediateFuture(null);
+      }
+    };
   }
 }
