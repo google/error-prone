5033e842457f39515111ad8943a2db10aa3d5bd5
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2018-02-04 16:15:28 +0100

    Recognize more `URLEqualsHashCode` violations
    - Flag maps created using common factory methods.
    - Flag `BiMap`s with `URL` values.

--- core/src/main/java/com/google/errorprone/bugpatterns/URLEqualsHashCode.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/URLEqualsHashCode.java
@@ -18,15 +18,21 @@ package com.google.errorprone.bugpatterns;
 
 import static com.google.errorprone.BugPattern.Category.JDK;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.allOf;
 import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.method.MethodMatchers.instanceMethod;
+import static com.google.errorprone.matchers.method.MethodMatchers.staticMethod;
 
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import com.google.errorprone.bugpatterns.BugChecker.NewClassTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
@@ -44,24 +50,41 @@ import java.util.List;
 @BugPattern(
   name = "URLEqualsHashCode",
   summary =
-      "Creation of a Set/HashSet/HashMap of java.net.URL."
+      "Creation of a Set/Map of java.net.URL."
           + " equals() and hashCode() of java.net.URL class make blocking internet connections.",
   category = JDK,
   severity = WARNING,
   tags = StandardTags.FRAGILE_CODE
 )
-public class URLEqualsHashCode extends BugChecker implements NewClassTreeMatcher {
+public class URLEqualsHashCode extends BugChecker
+    implements MethodInvocationTreeMatcher, NewClassTreeMatcher {
 
   private static final String URL_CLASS = "java.net.URL";
 
-  private static final Matcher<Tree> TYPE_MATCHER =
+  private static final Matcher<ExpressionTree> NEW_CLASS_MATCHER =
       anyOf(
           new URLTypeArgumentMatcher("java.util.Set", 0),
-          new URLTypeArgumentMatcher("java.util.Map", 0));
+          new URLTypeArgumentMatcher("java.util.Map", 0),
+          new URLTypeArgumentMatcher("com.google.common.collect.BiMap", 1));
+  private static final Matcher<ExpressionTree> METHOD_INVOCATION_MATCHER =
+      allOf(
+          anyOf(
+              staticMethod().anyClass().named("create"),
+              staticMethod().anyClass().named("of"),
+              instanceMethod().anyClass().named("build")),
+          NEW_CLASS_MATCHER);
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (METHOD_INVOCATION_MATCHER.matches(tree, state)) {
+      return describeMatch(tree);
+    }
+    return Description.NO_MATCH;
+  }
 
   @Override
   public Description matchNewClass(NewClassTree tree, VisitorState state) {
-    if (TYPE_MATCHER.matches(tree, state)) {
+    if (NEW_CLASS_MATCHER.matches(tree, state)) {
       return describeMatch(tree);
     }
     return Description.NO_MATCH;
