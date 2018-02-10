062e0bc63e72e10b0eeba1f4846f19b837ce9b1b
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2018-02-09 19:35:28 -0800

    Recognize more `URLEqualsHashCode` violations
    - Flag maps created using common factory methods.
    
    Fixes #921
    
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=185225022

--- core/src/main/java/com/google/errorprone/bugpatterns/URLEqualsHashCode.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/URLEqualsHashCode.java
@@ -18,15 +18,26 @@ package com.google.errorprone.bugpatterns;
 
 import static com.google.errorprone.BugPattern.Category.JDK;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.allOf;
 import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.method.MethodMatchers.instanceMethod;
+import static com.google.errorprone.matchers.method.MethodMatchers.staticMethod;
 
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.StandardTags;
 import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import com.google.errorprone.bugpatterns.BugChecker.NewClassTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.predicates.TypePredicates;
 import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.NewClassTree;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Symbol;
@@ -44,24 +55,52 @@ import java.util.List;
 @BugPattern(
   name = "URLEqualsHashCode",
   summary =
-      "Creation of a Set/HashSet/HashMap of java.net.URL."
-          + " equals() and hashCode() of java.net.URL class make blocking internet connections.",
+      "Avoid hash-based containers of java.net.URL--the containers rely on equals() and hashCode(),"
+          + " which cause java.net.URL to make blocking internet connections.",
   category = JDK,
   severity = WARNING,
   tags = StandardTags.FRAGILE_CODE
 )
-public class URLEqualsHashCode extends BugChecker implements NewClassTreeMatcher {
+public class URLEqualsHashCode extends BugChecker
+    implements MethodInvocationTreeMatcher, NewClassTreeMatcher {
 
   private static final String URL_CLASS = "java.net.URL";
 
-  private static final Matcher<Tree> TYPE_MATCHER =
+  private static final Matcher<ExpressionTree> CONTAINER_MATCHER =
       anyOf(
           new URLTypeArgumentMatcher("java.util.Set", 0),
-          new URLTypeArgumentMatcher("java.util.Map", 0));
+          new URLTypeArgumentMatcher("java.util.Map", 0),
+          // BiMap is a Map, so its first type argument is already being checked
+          new URLTypeArgumentMatcher("com.google.common.collect.BiMap", 1));
+
+  private static final Matcher<ExpressionTree> METHOD_INVOCATION_MATCHER =
+      allOf(
+          anyOf(
+              staticMethod()
+                  .onClassAny(
+                      ImmutableSet.class.getName(),
+                      ImmutableMap.class.getName(),
+                      HashBiMap.class.getName()),
+              instanceMethod()
+                  .onClass(
+                      TypePredicates.isExactTypeAny(
+                          ImmutableList.of(
+                              ImmutableSet.Builder.class.getName(),
+                              ImmutableMap.Builder.class.getName())))
+                  .named("build")),
+          CONTAINER_MATCHER);
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (METHOD_INVOCATION_MATCHER.matches(tree, state)) {
+      return describeMatch(tree);
+    }
+    return Description.NO_MATCH;
+  }
 
   @Override
   public Description matchNewClass(NewClassTree tree, VisitorState state) {
-    if (TYPE_MATCHER.matches(tree, state)) {
+    if (CONTAINER_MATCHER.matches(tree, state)) {
       return describeMatch(tree);
     }
     return Description.NO_MATCH;
@@ -100,3 +139,4 @@ public class URLEqualsHashCode extends BugChecker implements NewClassTreeMatcher
     }
   }
 }
+
