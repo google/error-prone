6b70931584c3f2daa2de2024d68a3a1241cbd4a7
ameya <ask1604@gmail.com>
2018-03-14 16:02:22 -0700

    more tests

--- core/src/test/java/com/google/errorprone/bugpatterns/refactoring_experiment/MigrateTypeTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/refactoring_experiment/MigrateTypeTest.java
@@ -6,15 +6,13 @@ import com.google.errorprone.BugCheckerRefactoringTestHelper;
 import com.google.errorprone.bugpatterns.CanonicalDuration;
 import com.google.errorprone.bugpatterns.refactoringexperiment.refactor.MigrateType;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 /**
  * Created by ameya on 3/14/18.
  */
-@RunWith(JUnit4.class)
+//@RunWith(JUnit4.class)
 public class MigrateTypeTest {
-    public void migrateTypeTestGoal1_positive() throws Exception {
+
+    public void testFuncIntInt_IntUnary_positive() throws Exception {
         BugCheckerRefactoringTestHelper.newInstance(new MigrateType(), getClass())
                 .addInputLines(
                         "TestGoal1.java",
@@ -48,7 +46,7 @@ public class MigrateTypeTest {
                 .doTest(TEXT_MATCH);
     }
 
-    public void migrateTypeTestGoal1_negative() throws Exception {
+    public void testFuncIntInt_IntUnary_negative() throws Exception {
         BugCheckerRefactoringTestHelper.newInstance(new CanonicalDuration(), getClass())
                 .addInputLines(
                         "TestGoal1.java",
@@ -73,6 +71,98 @@ public class MigrateTypeTest {
                         "Function<Integer,Integer> f;",
                         "public int test(Function<Integer,Integer> f){",
                         "this.f = f;",
+                        "return f.apply(5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "}")
+                .doTest(TEXT_MATCH);
+    }
+
+    public void testFuncIntInt_IntUnary_Hierarchy_positive() throws Exception {
+        BugCheckerRefactoringTestHelper.newInstance(new MigrateType(), getClass())
+                .addInputLines(
+                        "TestInterface.java",
+                        "import java.util.function.Function;",
+                        "interface TestGoal1 {",
+                        "public int test(Function<Integer,Integer> f);",
+                        "}",
+                        "TestGoal1.java",
+                        "import java.util.function.Function;",
+                        "class TestGoal1 implements {",
+                        "@Override",
+                        "public int test(Function<Integer,Integer> f){",
+                        "return f.apply(5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "}")
+                .addOutputLines(
+                        "TestInterface.java",
+                        "import java.util.function.Function;",
+                        "import java.util.function.IntUnaryOperator;",
+                        "interface TestGoal1 {",
+                        "public int test(IntUnaryOperator f);",
+                        "}",
+                        "TestGoal1.java",
+                        "import java.util.function.Function;",
+                        "import java.util.function.IntUnaryOperator;",
+                        "class TestGoal1 implements {",
+                        "@Override",
+                        "public int test(IntUnaryOperator f){",
+                        "return f.applyAsInt(5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "}")
+                .doTest(TEXT_MATCH);
+    }
+
+    public void testFuncIntInt_IntUnary_Hierarchy_negative() throws Exception {
+        BugCheckerRefactoringTestHelper.newInstance(new MigrateType(), getClass())
+                .addInputLines(
+                        "TestInterface.java",
+                        "import java.util.function.Function;",
+                        "interface TestGoal1 {",
+                        "public int test(Function<T,U> f);",
+                        "}",
+                        "TestGoal1.java",
+                        "import java.util.function.Function;",
+                        "class TestGoal1 implements {",
+                        "@Override",
+                        "public int test(Function<Integer,Integer> f){",
+                        "return f.apply(5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "public int boo5(){",
+                        "return test(x->x + 5);",
+                        "}",
+                        "}")
+                .addOutputLines(
+                        "TestInterface.java",
+                        "import java.util.function.Function;",
+                        "interface TestGoal1 {",
+                        "public int test(Function<T,U> f);",
+                        "}",
+                        "TestGoal1.java",
+                        "import java.util.function.Function;",
+                        "class TestGoal1 implements {",
+                        "@Override",
+                        "public int test(Function<Integer,Integer> f){",
                         "return f.apply(5);",
                         "}",
                         "public int boo5(){",
