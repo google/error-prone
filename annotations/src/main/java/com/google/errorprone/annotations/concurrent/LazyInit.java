b8f04e333dd4d72b775ce36f0164fb263caad279
zorzella <zorzella@google.com>
2018-04-25 17:36:02 -0700

    Improve LazyInit's documentation
    There are several issues with the current documentation that I am attempting to address with this CL:
    
    1. before this CL, hashCode is used to exemplify the situations where it's ok for an initialization to be racy; but hashCode's use of a local variable is evidently needed because it is not atomically constructed in this method, which masks the fact that, even if the hashCode was calculated atomically, a local variable would also be required to avoid the problem of out-of-order reading of the (non-volatile) field; this is aggravated by the fact that the javadoc does not state the problem
    
    2. also, using hashCode as an example precludes demonstrating how to apply the @LazyInit annotation (which is not present in String.hash).
    
    To address these problems:
    
    a. the example code has been change to be minimal and self-contained, and provide a template for people to copy-and-paste when using the annotation; this example code now also includes the field that is annotated with @LazyInit
    
    b. a caution has been added to say that the (weird) pattern is necessary, and a link has been added for the extensive documentation
    
    c. the note on "hashCode" has been left, but it simply links to the code in String.java, rather than copy and paste it
    
    RELNOTES: None
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=194325578

--- annotations/src/main/java/com/google/errorprone/annotations/concurrent/LazyInit.java
+++ annotations/src/main/java/com/google/errorprone/annotations/concurrent/LazyInit.java
@@ -22,22 +22,39 @@ import java.lang.annotation.Target;
 
 /**
  * Use this annotation on any static or field that will be initialized lazily, where races yield no
- * semantic difference in the code. The canonical example of this is String.hashCode():
+ * semantic difference in the code (as, for example, is the case with {@link String#hashCode}). Note
+ * that lazily initializing a non-volatile field is hard to do correctly, and one should rarely use
+ * this. It should also only be done by developers who clearly understand the potential issues, and
+ * then, always using the pattern as presented in the {@code getData} method of this sample code
+ * below:
  *
  * <pre>{@code
- * public int hashCode() {
- *   int h = hash;
- *   if (h == 0 && value.length > 0) {
- *     char val[] = value;
+ * private final String source;
+ * {@literal @}LazyInit private String data;
  *
- *     for (int i = 0; i < value.length; i++) {
- *       h = 31 * h + val[i];
- *     }
- *     hash = h;
+ * public String getData() {
+ *   String local = data;
+ *   if (local == null) {
+ *     local = data = expensiveCalculation(source);
  *   }
- *   return h;
+ *   return local;
+ *  }
+ *
+ * private static String expensiveCalculation(String string) {
+ *   return string.replaceAll(" ", "_");
  * }
  * }</pre>
+ *
+ * <p>The need for using the {@code local} variable is detailed in
+ * http://jeremymanson.blogspot.com/2008/12/benign-data-races-in-java.html (see, particularly, the
+ * part after "Now, let's break the code").
+ *
+ * <p>Also note that {@code LazyInit} must not be used on 64-bit primitives (such as {@code long}s
+ * and {@code double}s), because the Java Language Specification does not guarantee that writing to
+ * these is made atomically. Furthermore, when using for non-primitives, the non-primitive must be
+ * either truly immutable or at least thread safe (in the Java memory model sense). Again, unless
+ * you really understand this <b>and</b> you really need the performance benefits of avoiding the
+ * data race, do not use this construct.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
