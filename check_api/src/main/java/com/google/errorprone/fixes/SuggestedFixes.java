d9d883cd9f1c606517f5127bd1cc6db430c68cc9
cushon <cushon@google.com>
2018-05-04 10:27:36 -0700

    Improve error handling in compilesWithFix
    "Fixes" #849
    
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=195434516

--- check_api/src/main/java/com/google/errorprone/fixes/SuggestedFixes.java
+++ check_api/src/main/java/com/google/errorprone/fixes/SuggestedFixes.java
@@ -734,7 +734,7 @@ public class SuggestedFixes {
     try {
       newTask.analyze();
     } catch (Throwable e) {
-      // ignored
+      return false; // ¯\_(ツ)_/¯
     }
     return countErrors(diagnosticListener) == 0;
   }
