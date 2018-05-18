814674434744d319acc878326585d472c8cdae6f
cushon <cushon@google.com>
2018-05-17 19:11:46 -0700

    Tolerate missing symbols more gracefully in ObjectToString
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=197087693

--- core/src/test/java/com/google/errorprone/bugpatterns/ObjectToStringTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/ObjectToStringTest.java
@@ -16,9 +16,27 @@
 
 package com.google.errorprone.bugpatterns;
 
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+
+import com.google.common.io.ByteStreams;
+import com.google.errorprone.BugPattern;
 import com.google.errorprone.CompilationTestHelper;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.ClassTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.scanner.ScannerSupplier;
+import com.sun.source.tree.ClassTree;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -41,5 +59,69 @@ public class ObjectToStringTest {
   @Test
   public void testNegativeCase() throws Exception {
     compilationHelper.addSourceFile("ObjectToStringNegativeCases.java").doTest();
+  }
+
+  @Rule public final TemporaryFolder tempFolder = new TemporaryFolder();
+
+  /** A class that will be missing at compile-time for {@link #testIncompleteClasspath}. */
+  public static class One {}
+
+  /** A test class for {@link #testIncompleteClasspath}. */
+  public abstract static class TestLib {
+
+    /** Another test class for {@link #testIncompleteClasspath}. */
+    public static final class Two extends One {
+      @Override
+      public String toString() {
+        return "";
+      }
+    }
+
+    public abstract Two f();
+  }
+
+  static void addClassToJar(JarOutputStream jos, Class<?> clazz) throws IOException {
+    String entryPath = clazz.getName().replace('.', '/') + ".class";
+    try (InputStream is = clazz.getClassLoader().getResourceAsStream(entryPath)) {
+      jos.putNextEntry(new JarEntry(entryPath));
+      ByteStreams.copy(is, jos);
+    }
+  }
+
+  // A bugchecker that eagerly completes the missing symbol for testIncompleteClasspath below,
+  // to avoid the CompletionFailure being reported later.
+  /** A checker for {@link #testIncompleteClasspath}. */
+  @BugPattern(name = "CompletionChecker", summary = "", severity = ERROR)
+  public static class CompletionChecker extends BugChecker implements ClassTreeMatcher {
+    @Override
+    public Description matchClass(ClassTree tree, VisitorState state) {
+      state.getSymbolFromString(One.class.getName());
+      return Description.NO_MATCH;
+    }
+  }
+
+  // don't complain if we can't load the type hierarchy of a class that is toString()'d
+  @Test
+  public void testIncompleteClasspath() throws Exception {
+    File libJar = tempFolder.newFile("lib.jar");
+    try (FileOutputStream fis = new FileOutputStream(libJar);
+        JarOutputStream jos = new JarOutputStream(fis)) {
+      addClassToJar(jos, ObjectToStringTest.class);
+      addClassToJar(jos, TestLib.class);
+      addClassToJar(jos, TestLib.Two.class);
+    }
+    CompilationTestHelper.newInstance(
+            ScannerSupplier.fromBugCheckerClasses(ObjectToString.class, CompletionChecker.class),
+            getClass())
+        .addSourceLines(
+            "Test.java",
+            "import " + TestLib.class.getCanonicalName() + ";",
+            "class Test {",
+            "  String f(TestLib lib) {",
+            "    return \"\" + lib.f();",
+            "  }",
+            "}")
+        .setArgs(Arrays.asList("-cp", libJar.toString()))
+        .doTest();
   }
 }
