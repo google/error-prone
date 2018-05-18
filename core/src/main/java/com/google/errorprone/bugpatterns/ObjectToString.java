f85599c63247ec4b42900c462e9d9a58779c8505
cushon <cushon@google.com>
2018-05-17 19:11:46 -0700

    Tolerate missing symbols more gracefully in ObjectToString
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=197087693

--- core/src/main/java/com/google/errorprone/bugpatterns/ObjectToString.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/ObjectToString.java
@@ -20,20 +20,19 @@ import static com.google.errorprone.BugPattern.Category.JDK;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Iterables;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.ProvidesFix;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.fixes.Fix;
 import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.predicates.TypePredicate;
-import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.Tree;
-import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.code.Types;
-import java.util.Set;
+import com.sun.tools.javac.util.Names;
 
 /**
  * Warns against calling toString() on Objects which don't have toString() method overridden and
@@ -42,14 +41,13 @@ import java.util.Set;
  * @author bhagwani@google.com (Sumit Bhagwani)
  */
 @BugPattern(
-  name = "ObjectToString",
-  summary =
-      "Calling toString on Objects that don't override toString() doesn't"
-          + " provide useful information",
-  category = JDK,
-  severity = WARNING,
-  providesFix = ProvidesFix.NO_FIX
-)
+    name = "ObjectToString",
+    summary =
+        "Calling toString on Objects that don't override toString() doesn't"
+            + " provide useful information",
+    category = JDK,
+    severity = WARNING,
+    providesFix = ProvidesFix.NO_FIX)
 public class ObjectToString extends AbstractToString {
 
   private static boolean finalNoOverrides(Type type, VisitorState state) {
@@ -61,30 +59,24 @@ public class ObjectToString extends AbstractToString {
     if (!type.isFinal()) {
       return false;
     }
+    Types types = state.getTypes();
+    Names names = Names.instance(state.context);
+    // find Object.toString
+    MethodSymbol toString =
+        (MethodSymbol) state.getSymtab().objectType.tsym.members().findFirst(names.toString);
     // We explore the superclasses of the receiver type as well as the interfaces it
     // implements and we collect all overrides of java.lang.Object.toString(). If one of those
     // overrides is present, then we don't flag it.
-    Types types = state.getTypes();
-    Set<MethodSymbol> overridesOfToString =
-        ASTHelpers.findMatchingMethods(
-            state.getName("toString"),
-            methodSymbol -> isToString(methodSymbol, state),
-            type,
-            types);
-
-    // only has Object.toString()
-    if (overridesOfToString.size() == 1) {
-      return true;
-    }
-    return false;
+    return Iterables.isEmpty(
+        types
+            .membersClosure(type, /* skipInterface= */ false)
+            .getSymbolsByName(
+                names.toString,
+                m ->
+                    m != toString
+                        && m.overrides(toString, type.tsym, types, /* checkResult= */ false)));
   }
 
-  private static boolean isToString(MethodSymbol methodSymbol, VisitorState state) {
-    return !methodSymbol.isStatic()
-        && ((methodSymbol.flags() & Flags.SYNTHETIC) == 0)
-        && state.getTypes().isSameType(methodSymbol.getReturnType(), state.getSymtab().stringType)
-        && methodSymbol.getParameters().isEmpty();
-  }
 
   @Override
   protected TypePredicate typePredicate() {
