0e55117b2e36500aad82d33df18c2e83471cb92c
seibelsabrina <seibelsabrina@google.com>
2018-06-14 10:23:58 -0700

    Extra semicolon in test RELNOTES: n/a
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=200577056

--- core/src/test/java/com/google/errorprone/bugpatterns/SubstringOfZeroTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/SubstringOfZeroTest.java
@@ -25,7 +25,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SubstringOfZeroTest {
   private final BugCheckerRefactoringTestHelper helper =
-      BugCheckerRefactoringTestHelper.newInstance(new SubstringOfZero(), getClass());;
+      BugCheckerRefactoringTestHelper.newInstance(new SubstringOfZero(), getClass());
 
   @Test
   public void positiveJustVars() throws Exception {
