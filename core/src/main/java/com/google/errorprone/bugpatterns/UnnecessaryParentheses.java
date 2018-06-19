32988fca14ec891e11cd56fc9247a49e4cbc7dbd
cushon <cushon@google.com>
2018-06-19 16:54:28 -0700

    Add a check for unnecessary grouping parenthesis
    RELNOTES: [UnnecessaryParentheses] New check for unnecessary grouping parenthesis
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=201266012

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/UnnecessaryParentheses.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.ProvidesFix.REQUIRES_HUMAN_ATTENTION;
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.BugPattern.StandardTags.STYLE;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.ParenthesizedTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.ParenthesizedTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.UnaryTree;
+import com.sun.tools.javac.tree.JCTree;
+
+/** @author cushon@google.com (Liam Miller-Cushon) */
+@BugPattern(
+    name = "UnnecessaryParentheses",
+    summary = "Unnecessary use of grouping parentheses",
+    severity = WARNING,
+    providesFix = REQUIRES_HUMAN_ATTENTION,
+    tags = STYLE)
+public class UnnecessaryParentheses extends BugChecker implements ParenthesizedTreeMatcher {
+
+  @Override
+  public Description matchParenthesized(ParenthesizedTree tree, VisitorState state) {
+    ExpressionTree expression = tree.getExpression();
+    if (state.getPath().getParentPath().getLeaf() instanceof StatementTree) {
+      return NO_MATCH;
+    }
+    if (necessary(expression)) {
+      return NO_MATCH;
+    }
+    return describeMatch(
+        tree,
+        SuggestedFix.builder()
+            .replace(
+                ((JCTree) tree).getStartPosition(), ((JCTree) expression).getStartPosition(), "")
+            .replace(state.getEndPosition(expression), state.getEndPosition(tree), "")
+            .build());
+  }
+
+  private static boolean necessary(ExpressionTree expression) {
+    switch (expression.getKind()) {
+      case IDENTIFIER:
+      case MEMBER_SELECT:
+      case METHOD_INVOCATION:
+      case ARRAY_ACCESS:
+      case PARENTHESIZED:
+        return false;
+      default: // continue below
+    }
+    if (expression instanceof LiteralTree || expression instanceof UnaryTree) {
+      return false;
+    }
+    return true;
+  }
+}
