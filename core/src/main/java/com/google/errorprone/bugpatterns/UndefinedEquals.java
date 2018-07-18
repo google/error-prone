bfdd94d744e211bcc2a96655f7f9553372b08916
eleanorh <eleanorh@google.com>
2018-07-18 12:17:26 -0700

    Checks for equality-based methods where at least one argument is static type Multimap, Collection, Queue, or Collection.
    RELNOTES: n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=205116681

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/UndefinedEquals.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.assertEqualsInvocation;
+import static com.google.errorprone.matchers.Matchers.instanceEqualsInvocation;
+import static com.google.errorprone.matchers.Matchers.staticEqualsInvocation;
+import static com.google.errorprone.util.ASTHelpers.getReceiverType;
+import static com.google.errorprone.util.ASTHelpers.getType;
+import static com.google.errorprone.util.ASTHelpers.isSameType;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.tools.javac.code.Type;
+import java.util.Optional;
+
+/**
+ * Collection, Iterable, Multimap, and Queue do not have well-defined equals behavior.
+ *
+ * @author eleanorh@google.com (Eleanor Harris)
+ */
+@BugPattern(
+    name = "UndefinedEquals",
+    summary = "Collection, Iterable, Multimap, and Queue do not have well-defined equals behavior",
+    severity = WARNING)
+public final class UndefinedEquals extends BugChecker implements MethodInvocationTreeMatcher {
+
+  private static final ImmutableList<String> BAD_CLASS_LIST =
+      ImmutableList.of(
+          "java.util.Queue",
+          "java.util.Collection",
+          "com.google.common.collect.Multimap",
+          "java.lang.Iterable");
+
+  private static final Matcher<MethodInvocationTree> EQUALS_METHODS =
+      anyOf(staticEqualsInvocation(), instanceEqualsInvocation(), assertEqualsInvocation());
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (!EQUALS_METHODS.matches(tree, state)) {
+      return Description.NO_MATCH;
+    }
+
+    Type receiverType;
+    Type argumentType;
+
+    if (staticEqualsInvocation().matches(tree, state)) {
+      receiverType = getType(tree.getArguments().get(0));
+      argumentType = getType(tree.getArguments().get(1));
+    } else {
+      receiverType = getReceiverType(tree);
+      argumentType = getType(tree.getArguments().get(0));
+    }
+
+    Optional<String> badType =
+        BAD_CLASS_LIST
+            .stream()
+            .filter(
+                t ->
+                    isSameType(receiverType, state.getTypeFromString(t), state)
+                        || isSameType(argumentType, state.getTypeFromString(t), state))
+            .findFirst();
+
+    return badType.isPresent()
+        ? buildDescription(tree)
+            .setMessage(badType.get() + " does not have well-defined equals behavior")
+            .build()
+        : Description.NO_MATCH;
+  }
+}
