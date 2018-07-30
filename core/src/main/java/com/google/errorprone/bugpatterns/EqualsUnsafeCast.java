f858c2b34fba55b89de4bc1f7e64cc3c98f2d5b9
ghm <ghm@google.com>
2018-07-30 03:55:21 -0700

    Add a check for #equals methods that definitely make an unsafe cast.
    There's a surprisingly common pattern out there of,
    try {
      Type that = (Type) o;
      return <do comparisons>;
    } catch (ClassCastException e) {
      return false;
    }
    
    Which we're also matching on here--but I don't think that's a terrible thing to try to discourage.
    
    RELNOTES: [EqualsUnsafeCast] add a bugpattern to match unsafe casts in #equals implementations.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=206556966

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/EqualsUnsafeCast.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static com.google.errorprone.BugPattern.ProvidesFix.REQUIRES_HUMAN_ATTENTION;
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.isSameType;
+import static com.google.errorprone.matchers.Matchers.isStatic;
+import static com.google.errorprone.matchers.Matchers.methodHasParameters;
+import static com.google.errorprone.matchers.Matchers.methodIsNamed;
+import static com.google.errorprone.matchers.Matchers.methodReturns;
+import static com.google.errorprone.matchers.Matchers.not;
+import static com.google.errorprone.matchers.Matchers.variableType;
+import static com.google.errorprone.suppliers.Suppliers.BOOLEAN_TYPE;
+import static com.google.errorprone.suppliers.Suppliers.OBJECT_TYPE;
+import static com.google.errorprone.util.ASTHelpers.findEnclosingNode;
+import static com.google.errorprone.util.ASTHelpers.getSymbol;
+import static com.google.errorprone.util.ASTHelpers.getType;
+import static com.google.errorprone.util.ASTHelpers.isSubtype;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.InstanceOfTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.Tree.Kind;
+import com.sun.source.tree.TypeCastTree;
+import com.sun.source.util.TreePathScanner;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Checks for {@code equals} implementations making unsafe casts.
+ *
+ * @author ghm@google.com (Graeme Morgan)
+ */
+@BugPattern(
+    name = "EqualsUnsafeCast",
+    summary =
+        "The contract of #equals states that it should return false for incompatible types, "
+            + "while this implementation may throw ClassCastException.",
+    providesFix = REQUIRES_HUMAN_ATTENTION,
+    severity = WARNING)
+public final class EqualsUnsafeCast extends BugChecker implements MethodTreeMatcher {
+
+  private static final String INSTANCEOF_CHECK = "if (!(%s instanceof %s)) { return false; }";
+
+  private static final Matcher<MethodTree> EQUALS_IMPLEMENTATION =
+      allOf(
+          methodIsNamed("equals"),
+          methodReturns(BOOLEAN_TYPE),
+          methodHasParameters(variableType(isSameType(OBJECT_TYPE))),
+          not(isStatic()));
+
+  @Override
+  public Description matchMethod(MethodTree tree, VisitorState state) {
+    if (!EQUALS_IMPLEMENTATION.matches(tree, state)) {
+      return NO_MATCH;
+    }
+
+    Symbol parameter = getSymbol(getOnlyElement(tree.getParameters()));
+
+    new TreePathScanner<Void, Void>() {
+      private boolean methodInvoked = false;
+      private final List<Type> checkedTypes = new ArrayList<>();
+
+      @Override
+      public Void visitInstanceOf(InstanceOfTree node, Void unused) {
+        checkedTypes.add(getType(node.getType()));
+        return super.visitInstanceOf(node, null);
+      }
+
+      @Override
+      public Void visitMethodInvocation(MethodInvocationTree node, Void unused) {
+        // Some equals implementations rely on super#equals for their class check. To avoid false
+        // positives there, consider any method invocation to imply that this might be safe.
+        // This also matches for any class comparisons with getClass.
+        methodInvoked = true;
+        return null;
+      }
+
+      @Override
+      public Void visitTypeCast(TypeCastTree node, Void unused) {
+        ExpressionTree expression = node.getExpression();
+        if (!methodInvoked
+            && expression.getKind() == Kind.IDENTIFIER
+            && parameter.equals(getSymbol(expression))
+            && checkedTypes.stream().noneMatch(t -> isSubtype(t, getType(node.getType()), state))) {
+          StatementTree enclosingStatement =
+              findEnclosingNode(getCurrentPath(), StatementTree.class);
+          state.reportMatch(
+              describeMatch(
+                  node,
+                  SuggestedFix.prefixWith(
+                      enclosingStatement,
+                      String.format(
+                          INSTANCEOF_CHECK,
+                          state.getSourceForNode(expression),
+                          state.getSourceForNode(node.getType())))));
+        }
+        return super.visitTypeCast(node, null);
+      }
+    }.scan(state.getPath(), null);
+
+    return NO_MATCH;
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/bugpatterns/EqualsUnsafeCastTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for {@link EqualsUnsafeCast} bugpattern.
+ *
+ * @author ghm@google.com (Graeme Morgan)
+ */
+@RunWith(JUnit4.class)
+public final class EqualsUnsafeCastTest {
+  private final CompilationTestHelper helper =
+      CompilationTestHelper.newInstance(EqualsUnsafeCast.class, getClass());
+
+  @Test
+  public void fixes() throws Exception {
+    BugCheckerRefactoringTestHelper.newInstance(new EqualsUnsafeCast(), getClass())
+        .addInputLines(
+            "Test.java",
+            "class Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    Test that = (Test) o;",
+            "    return that.a == a;",
+            "  }",
+            "}")
+        .addOutputLines(
+            "Test.java",
+            "class Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    if (!(o instanceof Test)) { return false; }",
+            "    Test that = (Test) o;",
+            "    return that.a == a;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void fixesInlineCheck() throws Exception {
+    BugCheckerRefactoringTestHelper.newInstance(new EqualsUnsafeCast(), getClass())
+        .addInputLines(
+            "Test.java",
+            "class Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    return o != null && a == ((Test) o).a;",
+            "  }",
+            "}")
+        .addOutputLines(
+            "Test.java",
+            "class Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    if (!(o instanceof Test)) { return false; }",
+            "    return o != null && a == ((Test) o).a;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void positiveWrongType() {
+    helper
+        .addSourceLines(
+            "Test.java",
+            "class SubTest extends Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    if (!(o instanceof Test)) { return false; }",
+            "    // BUG: Diagnostic contains: instanceof SubTest",
+            "    return o != null && a == ((SubTest) o).a;",
+            "  }",
+            "}",
+            "class Test {}")
+        .doTest();
+  }
+
+  @Test
+  public void negative() {
+    helper
+        .addSourceLines(
+            "Test.java",
+            "class Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    if (getClass() == o.getClass()) { return false; }",
+            "    return o != null && a == ((Test) o).a;",
+            "  }",
+            "}")
+        .doTest();
+
+    helper
+        .addSourceLines(
+            "Test.java",
+            "class Test {",
+            "  private int a;",
+            "  @Override public boolean equals(Object o) {",
+            "    if (!(o instanceof Test)) { return false; }",
+            "    return o != null && a == ((Test) o).a;",
+            "  }",
+            "}")
+        .doTest();
+  }
+}
