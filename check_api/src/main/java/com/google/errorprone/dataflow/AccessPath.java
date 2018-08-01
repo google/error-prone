a156bfb809ae7e908d83d6732e71be740cb9b219
bennostein <bennostein@google.com>
2018-08-01 09:59:07 -0700

    Track AutoValue accessors in Nullness analysis access paths
    RELNOTES: Track AutoValue accessors in Nullness analysis access paths
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=206945111

--- check_api/src/main/java/com/google/errorprone/dataflow/AccessPath.java
+++ check_api/src/main/java/com/google/errorprone/dataflow/AccessPath.java
@@ -17,12 +17,21 @@ package com.google.errorprone.dataflow;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
+import com.google.errorprone.util.MoreAnnotations;
 import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.Tree.Kind;
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.tree.JCTree.JCExpression;
+import com.sun.tools.javac.tree.JCTree.JCFieldAccess;
+import com.sun.tools.javac.tree.JCTree.JCIdent;
+import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
 import javax.annotation.Nullable;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import org.checkerframework.dataflow.cfg.node.AssignmentNode;
 import org.checkerframework.dataflow.cfg.node.FieldAccessNode;
 import org.checkerframework.dataflow.cfg.node.LocalVariableNode;
@@ -31,16 +40,20 @@ import org.checkerframework.dataflow.cfg.node.VariableDeclarationNode;
 import org.checkerframework.javacutil.TreeUtils;
 
 /**
- * A sequence of field names along with either a variable or a reference (explicit or implicit) to
- * {@code this}
+ * A sequence of field names or autovalue accessors, along with a receiver: either a variable or a
+ * reference (explicit or implicit) to {@code this}. Fields and autovalue accessors are stored as
+ * strings, with a "()" appended to accessor names to distinguish them from fields of the same name
  *
  * <p>For example:
  *
  * <p>{@code x.f.g}, the {@code g} field of the {@code f} field of the local variable {@code x} is
- * represented by {base = Some x, fields = g :: f :: nil}
+ * represented by {base = Some x, fields = "g" :: "f" :: nil}
  *
  * <p>{@code foo.bar}, the {@code bar} field of the {@code foo} field of the implicit {@code this}
- * is represented by {base = None, fields = bar :: foo :: nil}
+ * is represented by {base = None, fields = "bar" :: "foo" :: nil}
+ *
+ * <p>{@code x.foo().foo}, the {@code foo} field of the {@code foo()} autovalue accessor of the
+ * local variable {@code x} is represented by {base = Some x, fields = "foo" :: "foo()" :: nil}
  *
  * @author bennostein@google.com (Benno Stein)
  */
@@ -49,28 +62,75 @@ public abstract class AccessPath {
 
   /** If present, base of access path is contained Element; if absent, base is `this` */
   public abstract @Nullable Element base();
-  // TODO(b/110226434): Support getters (or arbitrary nullary pure methods) as well as fields?
-  public abstract ImmutableList<String> fields();
 
-  private static AccessPath create(@Nullable Element base, ImmutableList<String> fields) {
-    return new AutoValue_AccessPath(base, fields);
+  public abstract ImmutableList<String> path();
+
+  private static AccessPath create(@Nullable Element base, ImmutableList<String> path) {
+    return new AutoValue_AccessPath(base, path);
   }
 
   /**
-   * Creates an AccessPath from field accesses we can track and returns null otherwise (for example,
-   * when the receiver of the field access contains a method call or array access)
+   * Check whether {@code tree} is an AutoValue accessor. A tree is an AutoValue accessor iff:
+   *
+   * <ul>
+   *   <li>it is a method invocation
+   *   <li>of an abstract method
+   *   <li>with 0 arguments
+   *   <li>defined on a class annotated @AutoValue
+   * </ul>
+   *
+   * <p>Public visibility for use in NullnessPropagationTransfer#returnValueNullness
+   */
+  public static boolean isAutoValueAccessor(Tree tree) {
+    if (!(tree instanceof MethodInvocationTree)) {
+      return false;
+    }
+
+    JCMethodInvocation invocationTree = (JCMethodInvocation) tree;
+    // methodSelect is always either a field access (e.g. `obj.foo()`) or identifier (e.g. `foo()`)
+    JCExpression methodSelect = invocationTree.getMethodSelect();
+    Type rcvrType =
+        (methodSelect instanceof JCFieldAccess)
+            ? ((JCFieldAccess) methodSelect).selected.type
+            : ((JCIdent) methodSelect).sym.owner.type;
+
+    return invocationTree.getArguments().isEmpty() // 0 arguments
+        && // abstract
+        TreeUtils.elementFromUse(invocationTree).getModifiers().contains(Modifier.ABSTRACT)
+        && // class, not interface
+        rcvrType.tsym.getKind() == ElementKind.CLASS
+        && // annotated @AutoValue
+        MoreAnnotations.getDeclarationAndTypeAttributes(rcvrType.tsym)
+            .map(Object::toString)
+            .anyMatch(("@" + AutoValue.class.getCanonicalName())::equals);
+  }
+
+  /**
+   * Creates an AccessPath from field reads / AutoValue accessor we can track and returns null
+   * otherwise (for example, when the receiver of the field access contains an array access or
+   * non-AutoValue method call.
    */
   @Nullable
   public static AccessPath fromFieldAccess(FieldAccessNode fieldAccess) {
     ImmutableList.Builder<String> pathBuilder = ImmutableList.builder();
 
     Tree tree = fieldAccess.getTree();
-    while (TreeUtils.isFieldAccess(tree)) {
-      pathBuilder.add(TreeUtils.getFieldName(tree));
+    boolean isFieldAccess;
+    while ((isFieldAccess = TreeUtils.isFieldAccess(tree)) || isAutoValueAccessor(tree)) {
+      if (isFieldAccess) {
+        pathBuilder.add(TreeUtils.getFieldName(tree));
+      } else {
+        // Must be an AutoValue accessor, since the `while` condition held but the `if` didn't.
+        // Unwrap the method select from the call
+        tree = ((MethodInvocationTree) tree).getMethodSelect();
+        pathBuilder.add(TreeUtils.getMethodName(tree) + "()");
+      }
+
       if (tree.getKind() == Kind.IDENTIFIER) {
         // Implicit `this` receiver
         return AccessPath.create(/*base=*/ null, pathBuilder.build());
       }
+
       tree = ((MemberSelectTree) tree).getExpression();
     }
 
