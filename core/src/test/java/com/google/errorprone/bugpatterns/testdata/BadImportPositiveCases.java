62c2f91a2838a7ee46bb3a5fda0c48db1a8eca31
awturner <awturner@google.com>
2018-08-03 17:13:16 -0700

    BadImport: ignore enum case labels when the identifier is otherwise statically imported
    RELNOTES: Ignore enum case labels in BadImport check
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=207349366

--- core/src/test/java/com/google/errorprone/bugpatterns/testdata/BadImportPositiveCases.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/testdata/BadImportPositiveCases.java
@@ -15,6 +15,8 @@
  */
 package com.google.errorprone.bugpatterns.testdata;
 
+import static com.google.errorprone.bugpatterns.testdata.BadImportPositiveCases.Example.INSTANCE;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
 
@@ -57,5 +59,19 @@ class BadImportPositiveCases {
 
   void classLiteral() {
     System.out.println(Builder.class);
+  }
+
+  public void enumSwitch() {
+    // BUG: Diagnostic contains: Example.INSTANCE
+    Example object = INSTANCE;
+
+    switch (object) {
+      case INSTANCE: // This line should be left alone.
+        break;
+    }
+  }
+
+  enum Example {
+    INSTANCE
   }
 }
