899169b7ddf440d9b8ae3d2e72213d84c1e994a5
ghm <ghm@google.com>
2018-08-10 04:05:12 -0700

    Make ProtoFieldNullComparison match checkNotNull (+ similar) and Lite protos behind flags.
    We can either add the extra functionality of matching via final variables into ProtoFieldPreconditionsCheckNotNull or combine the checks into one. The latter makes more sense to me; they were duplicating most of their code anyway.
    
    RELNOTES: Make ProtoFieldNullComparison match checkNotNull & lite protos
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=208190260

--- core/src/test/java/com/google/errorprone/bugpatterns/ProtoFieldNullComparisonTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/ProtoFieldNullComparisonTest.java
@@ -263,4 +263,128 @@ public final class ProtoFieldNullComparisonTest {
             "}")
         .doTest();
   }
+
+  @Test
+  public void preconditions() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import static com.google.common.base.Preconditions.checkNotNull;",
+            "import static com.google.common.base.Verify.verifyNotNull;",
+            "import static java.util.Objects.requireNonNull;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestProtoMessage;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestFieldProtoMessage;",
+            "import java.util.List;",
+            "class Test {",
+            "  void test() {",
+            "    TestProtoMessage message = TestProtoMessage.newBuilder().build();",
+            "    TestFieldProtoMessage field = message.getMessage();",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(field);",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(message.getMessage());",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    verifyNotNull(message.getMessage());",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(message.getMultiFieldList());",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(message.getMessage(), new Object());",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(message.getMultiFieldList(), new Object());",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(message.getMessage(), \"%s\", new Object());",
+            "    // BUG: Diagnostic contains: Protobuf fields cannot be null",
+            "    // remove this line",
+            "    checkNotNull(message.getMultiFieldList(), \"%s\", new Object());",
+            "    // BUG: Diagnostic contains: fieldMessage = message.getMessage();",
+            "    TestFieldProtoMessage fieldMessage = checkNotNull(message.getMessage());",
+            "    // BUG: Diagnostic contains: fieldMessage2 = message.getMessage()",
+            "    TestFieldProtoMessage fieldMessage2 = checkNotNull(message.getMessage(), \"M\");",
+            "    // BUG: Diagnostic contains: message.getMessage().toString();",
+            "    checkNotNull(message.getMessage()).toString();",
+            "    // BUG: Diagnostic contains: message.getMessage().toString();",
+            "    checkNotNull(message.getMessage(), \"Message\").toString();",
+            "    // BUG: Diagnostic contains: TestFieldProtoMessage fieldCopy = field;",
+            "    TestFieldProtoMessage fieldCopy = requireNonNull(field);",
+            "  }",
+            "}")
+        .setArgs(
+            ImmutableList.of(
+                "-XepOpt:ProtoFieldNullComparison:TrackAssignments",
+                "-XepOpt:ProtoFieldNullComparison:MatchCheckNotNull"))
+        .doTest();
+  }
+
+  @Test
+  public void assertions() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import static com.google.common.truth.Truth.assertThat;",
+            "import static org.junit.Assert.assertNotNull;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestProtoMessage;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestFieldProtoMessage;",
+            "import java.util.List;",
+            "class Test {",
+            "  void test() {",
+            "    TestProtoMessage message = TestProtoMessage.newBuilder().build();",
+            "    TestFieldProtoMessage field = message.getMessage();",
+            "    // BUG: Diagnostic contains: assertTrue(\"Message\", message.hasMessage());",
+            "    assertNotNull(\"Message\", message.getMessage());",
+            "    // BUG: Diagnostic contains: assertThat(message.hasMessage()).isTrue()",
+            "    assertThat(message.getMessage()).isNotNull();",
+            "  }",
+            "}")
+        .setArgs(ImmutableList.of("-XepOpt:ProtoFieldNullComparison:MatchTestAssertions"))
+        .doTest();
+  }
+
+  @Test
+  public void preconditions_negative() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestProtoMessage;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestFieldProtoMessage;",
+            "import java.util.List;",
+            "class Test {",
+            "  void test() {",
+            "    TestProtoMessage message = TestProtoMessage.newBuilder().build();",
+            "    TestFieldProtoMessage field = message.getMessage();",
+            "    Preconditions.checkNotNull(field);",
+            "  }",
+            "}",
+            "class Preconditions {",
+            "  static void checkNotNull(Object o) {}",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void assertions_negative() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import static com.google.common.truth.Truth.assertThat;",
+            "import static org.junit.Assert.assertNotNull;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestProtoMessage;",
+            "import com.google.errorprone.bugpatterns.proto.ProtoTest.TestFieldProtoMessage;",
+            "import java.util.List;",
+            "class Test {",
+            "  void test() {",
+            "    TestProtoMessage message = TestProtoMessage.newBuilder().build();",
+            "    TestFieldProtoMessage field = message.getMessage();",
+            "    assertNotNull(\"Message\", message.getMessage());",
+            "    assertThat(message.getMessage()).isNotNull();",
+            "  }",
+            "}")
+        .doTest();
+  }
 }
