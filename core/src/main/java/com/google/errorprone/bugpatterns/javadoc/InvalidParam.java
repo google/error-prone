6a2809f44ebc2bd2cac67995a5dc07b91d9fe5dc
ghm <ghm@google.com>
2018-09-13 09:57:59 -0700

    Automated rollback of fd357c3849fe0f66e98eda7b7ac1edfd781808fc.
    *** Reason for rollback ***
    
    Roll forward with licence headers.
    
    Unsure why this wasn't caught _pre_submit.
    
    *** Original change description ***
    
    Automated rollback of changelist 7c2f8c6d91a89a014dba31d3221233b4793248e6.
    
    *** Reason for rollback ***
    
    Forgot licence headers.
    
    *** Original change description ***
    
    Add bugpatterns to suggest fixes for common Javadoc errors.
    
    This totally duplicates some of the functionality of `-Xdoclint`, but by reimplementing some of it we can actually provide convenient fixes.
    
    RELNOTES: add bugpatterns for linting Javadocs, and providing fixes
    
    ***
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=212829552

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/javadoc/InvalidParam.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns.javadoc;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.bugpatterns.javadoc.Utils.getBestMatch;
+import static com.google.errorprone.bugpatterns.javadoc.Utils.getDocComment;
+import static com.google.errorprone.bugpatterns.javadoc.Utils.getDocTreePath;
+import static com.google.errorprone.bugpatterns.javadoc.Utils.replace;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.ProvidesFix;
+import com.google.errorprone.BugPattern.StandardTags;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.ClassTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.doctree.DocTree;
+import com.sun.source.doctree.ParamTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.util.DocTreePath;
+import com.sun.source.util.DocTreePathScanner;
+import com.sun.tools.javac.tree.DCTree.DCDocComment;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Matches incorrect Javadoc {@literal @}param tags.
+ *
+ * @author ghm@google.com (Graeme Morgan)
+ */
+@BugPattern(
+    name = "InvalidParam",
+    summary = "This @param tag doesn't refer to a parameter of the method.",
+    severity = WARNING,
+    tags = StandardTags.STYLE,
+    providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
+public final class InvalidParam extends BugChecker implements ClassTreeMatcher, MethodTreeMatcher {
+
+  @Override
+  public Description matchClass(ClassTree classTree, VisitorState state) {
+    DocTreePath path = getDocTreePath(state);
+    if (path != null) {
+      ImmutableSet<String> parameters = ImmutableSet.of();
+      ImmutableSet<String> typeParameters =
+          classTree.getTypeParameters().stream()
+              .map(t -> t.getName().toString())
+              .collect(toImmutableSet());
+      new ParamsChecker(state, classTree, parameters, typeParameters).scan(path, null);
+    }
+    return Description.NO_MATCH;
+  }
+
+  @Override
+  public Description matchMethod(MethodTree methodTree, VisitorState state) {
+    DocTreePath path = getDocTreePath(state);
+    if (path != null) {
+      ImmutableSet<String> parameters =
+          methodTree.getParameters().stream()
+              .map(v -> v.getName().toString())
+              .collect(toImmutableSet());
+      ImmutableSet<String> typeParameters =
+          methodTree.getTypeParameters().stream()
+              .map(t -> t.getName().toString())
+              .collect(toImmutableSet());
+      new ParamsChecker(state, methodTree, parameters, typeParameters).scan(path, null);
+    }
+    return Description.NO_MATCH;
+  }
+
+  /** Checks that documented parameters match the method's parameter list. */
+  private final class ParamsChecker extends DocTreePathScanner<Void, Void> {
+    private final VisitorState state;
+
+    private final ImmutableSet<String> documentedParameters;
+    private final ImmutableSet<String> documentedTypeParameters;
+
+    private final ImmutableSet<String> parameters;
+    private final ImmutableSet<String> typeParameters;
+    private final Tree tree;
+
+    private ParamsChecker(
+        VisitorState state,
+        Tree tree,
+        ImmutableSet<String> parameters,
+        ImmutableSet<String> typeParameters) {
+      this.state = state;
+      this.tree = tree;
+      DCDocComment dcDocComment = getDocComment(state, tree);
+      this.documentedParameters =
+          extractDocumentedParams(dcDocComment, /* isTypeParameter= */ false);
+      this.documentedTypeParameters =
+          extractDocumentedParams(dcDocComment, /* isTypeParameter= */ true);
+      this.parameters = parameters;
+      this.typeParameters = typeParameters;
+    }
+
+    @Override
+    public Void visitParam(ParamTree paramTree, Void unused) {
+      ImmutableSet<String> paramNames = paramTree.isTypeParameter() ? typeParameters : parameters;
+      if (!paramNames.contains(paramTree.getName().toString())) {
+        ImmutableSet<String> documentedParamNames =
+            paramTree.isTypeParameter() ? documentedTypeParameters : documentedParameters;
+        Set<String> undocumentedParameters = Sets.difference(paramNames, documentedParamNames);
+        Optional<String> bestMatch =
+            getBestMatch(paramTree.getName().toString(), undocumentedParameters);
+        String message =
+            String.format("Parameter name `%s` is unknown.", paramTree.getName().toString());
+        state.reportMatch(
+            bestMatch
+                .map(
+                    bm ->
+                        buildDescription(tree)
+                            .setMessage(message + String.format(" Did you mean %s?", bm))
+                            .addFix(replace(paramTree.getName(), bm, state))
+                            .build())
+                .orElse(
+                    buildDescription(tree)
+                        .setMessage(message)
+                        .addFix(replace(paramTree, "", state))
+                        .build()));
+      }
+      return super.visitParam(paramTree, null);
+    }
+  }
+
+  private static ImmutableSet<String> extractDocumentedParams(
+      DCDocComment docCommentTree, boolean isTypeParameter) {
+    ImmutableSet.Builder<String> parameters = ImmutableSet.builder();
+    for (DocTree docTree : docCommentTree.getBlockTags()) {
+      if (!(docTree instanceof ParamTree)) {
+        continue;
+      }
+      ParamTree paramTree = (ParamTree) docTree;
+      if (paramTree.isTypeParameter() == isTypeParameter) {
+        parameters.add(paramTree.getName().getName().toString());
+      }
+    }
+    return parameters.build();
+  }
+}
---
+++ core/src/main/java/com/google/errorprone/bugpatterns/javadoc/ReturnFromVoid.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns.javadoc;
+
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.util.ASTHelpers.getType;
+import static com.google.errorprone.util.ASTHelpers.isSameType;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.ProvidesFix;
+import com.google.errorprone.BugPattern.StandardTags;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker;
+import com.google.errorprone.bugpatterns.BugChecker.MethodTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.doctree.ReturnTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.util.DocTreePath;
+import com.sun.source.util.DocTreePathScanner;
+
+/**
+ * Finds common Javadoc errors, and tries to suggest useful fixes.
+ *
+ * @author ghm@google.com (Graeme Morgan)
+ */
+@BugPattern(
+    name = "ReturnFromVoid",
+    summary = "Void methods should not have a @return tag.",
+    severity = WARNING,
+    tags = StandardTags.STYLE,
+    providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
+public final class ReturnFromVoid extends BugChecker implements MethodTreeMatcher {
+
+  @Override
+  public Description matchMethod(MethodTree methodTree, VisitorState state) {
+    DocTreePath path = Utils.getDocTreePath(state);
+    if (path != null) {
+      new VoidReturnTypeChecker(state, methodTree).scan(path, null);
+    }
+    return Description.NO_MATCH;
+  }
+
+  private final class VoidReturnTypeChecker extends DocTreePathScanner<Void, Void> {
+    private final VisitorState state;
+    private final MethodTree methodTree;
+
+    private VoidReturnTypeChecker(VisitorState state, MethodTree methodTree) {
+      this.state = state;
+      this.methodTree = methodTree;
+    }
+
+    @Override
+    public Void visitReturn(ReturnTree returnTree, Void unused) {
+      if (isSameType(getType(methodTree.getReturnType()), state.getSymtab().voidType, state)) {
+        state.reportMatch(describeMatch(methodTree, Utils.replace(returnTree, "", state)));
+      }
+      return super.visitReturn(returnTree, null);
+    }
+  }
+}
