4708b0069dfacade981ddb7c629da22b3bd5c19b
lundmark <lundmark@google.com>
2018-10-16 13:05:25 -0700

    Fix a few issues with the MutableMethodReturnType checker where the checker would crash on certain methods.
    See test file for added regression tests with examples.
    
    RELNOTES: Fix a few issues with the MutableMethodReturnType checker where the checker would crash on certain methods.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=217370430

--- core/src/test/java/com/google/errorprone/bugpatterns/MutableMethodReturnTypeTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/MutableMethodReturnTypeTest.java
@@ -329,4 +329,68 @@ public class MutableMethodReturnTypeTest {
             "}")
         .doTest();
   }
+
+  @Test
+  public void returnTypeList_insideAnonymousNested_suggestsImmutableList() {
+    testHelper
+        .addSourceLines(
+            "Test.java",
+            "import com.google.common.collect.ImmutableList;",
+            "import com.google.inject.Provider;",
+            "import java.util.List;",
+            "class Test {",
+            "  private static final int getFooLength() {",
+            "    final Provider<List<String>> fooProvider = ",
+            "      new Provider<List<String>>() {",
+            "        @Override",
+            "        // BUG: Diagnostic contains: ImmutableList<String> get",
+            "        public List<String> get() {",
+            "          return ImmutableList.of(\"foo\", \"bar\");",
+            "        }",
+            "      };",
+            "    List<String> foo = fooProvider.get();",
+            "    return foo.size();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void returnTypeList_anonymousLambda_suggestsNothing() {
+    testHelper
+        .addSourceLines(
+            "ApplyInterface.java",
+            "import java.util.function.Function;",
+            "import java.util.List;",
+            "public interface ApplyInterface {",
+            "  int applyAndGetSize(Function<String, List<String>> fun);",
+            "}")
+        .addSourceLines(
+            "ApplyImpl.java",
+            "import com.google.common.collect.ImmutableList;",
+            "import java.util.function.Function;",
+            "import java.util.List;",
+            "public class ApplyImpl implements ApplyInterface {",
+            "  public int applyAndGetSize(Function<String, List<String>> fun) {",
+            "    List<String> result = fun.apply(\"foo,bar\");",
+            "    return result.size();",
+            "  }",
+            "}")
+        .addSourceLines(
+            "Test.java",
+            "import com.google.common.collect.ImmutableList;",
+            "import java.util.function.Function;",
+            "import java.util.List;",
+            "class Test {",
+            "  private static final ApplyInterface APPLY = new ApplyImpl();",
+            "  private int doApply() {",
+            "    int result = APPLY.applyAndGetSize(str -> {",
+            "        ImmutableList<String> res = ImmutableList.of(str, str);",
+            "        return res;",
+            "      });",
+            "    return result;",
+            "  }",
+            "}")
+        .doTest();
+  }
 }
