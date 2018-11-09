0bfb9d09c9395c5d93372ac1a9928060f58c5216
glorioso <glorioso@google.com>
2018-11-09 09:40:08 -0800

    Move package declarations back to their own line, as opposed to being part of the license header comment.
    RELNOTES: n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=220816219

--- core/src/main/java/com/google/errorprone/bugpatterns/time/JavaDurationWithSeconds.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/time/JavaDurationWithSeconds.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ package com.google.errorprone.bugpatterns.time;
+ */
+package com.google.errorprone.bugpatterns.time;
 
 import static com.google.errorprone.BugPattern.ProvidesFix.REQUIRES_HUMAN_ATTENTION;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
--- core/src/test/java/com/google/errorprone/bugpatterns/time/JodaPlusMinusLongTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/time/JodaPlusMinusLongTest.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ package com.google.errorprone.bugpatterns.time;
+ */
+package com.google.errorprone.bugpatterns.time;
 
 import com.google.errorprone.CompilationTestHelper;
 import org.junit.Test;
