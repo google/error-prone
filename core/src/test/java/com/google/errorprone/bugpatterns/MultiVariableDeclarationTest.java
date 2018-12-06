c5a61a282c2e9de9cee7c474e7d5147f6531774f
cushon <cushon@google.com>
2018-12-05 17:47:08 -0800

    Omit empty `()` from marker annotations in MultiVariableDeclaration
    The fix relies on pretty-printing the desugared AST nodes for the individual
    variable declarations. Normally we try to avoid pretty-printing and manipulate
    source text directly, but in this case the alternative is to re-implement
    parsing and desugaring multi-variable declarations.
    
    RELNOTES: N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=224253024

--- core/src/test/java/com/google/errorprone/bugpatterns/MultiVariableDeclarationTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/MultiVariableDeclarationTest.java
@@ -173,12 +173,8 @@ public class MultiVariableDeclarationTest {
             "out/A.java",
             "package a;",
             "public class A {",
-            // javac's pretty printer uses the system line separator
-            "  @Deprecated()"
-                + System.lineSeparator()
-                + "int x = 1; @Deprecated()"
-                + System.lineSeparator()
-                + "int y = 2;",
+            "  @Deprecated int x = 1;",
+            "  @Deprecated int y = 2;",
             "}")
         .doTest(TEXT_MATCH);
   }
