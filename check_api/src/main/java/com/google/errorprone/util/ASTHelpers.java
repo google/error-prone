c0c838e1c8606fcf7fa2d1454adfb1e1448250ca
ghm <ghm@google.com>
2019-01-18 04:00:16 -0800

    Add a bugpattern to detect methods which return a mixture of immutable and mutable types.
    RELNOTES: [MixedMutabilityReturnType] flag methods which return both mutable and immutable types
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=229905757

--- check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
+++ check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
@@ -855,6 +855,23 @@ public class ASTHelpers {
     return sym == null ? null : sym.name.toString();
   }
 
+  /** Returns the erasure of the given type tree, i.e. {@code List} for {@code List<Foo>}. */
+  public static Tree getErasedTypeTree(Tree tree) {
+    return tree.accept(
+        new SimpleTreeVisitor<Tree, Void>() {
+          @Override
+          public Tree visitIdentifier(IdentifierTree tree, Void unused) {
+            return tree;
+          }
+
+          @Override
+          public Tree visitParameterizedType(ParameterizedTypeTree tree, Void unused) {
+            return tree.getType();
+          }
+        },
+        null);
+  }
+
   /** Return the enclosing {@code ClassSymbol} of the given symbol, or {@code null}. */
   public static ClassSymbol enclosingClass(Symbol sym) {
     return sym.owner.enclClass();
