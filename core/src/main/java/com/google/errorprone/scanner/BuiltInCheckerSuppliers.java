8e821c8e4f0d587c6d64de24b8dfd562e4a8aea7
tvanderlippe <tvanderlippe@google.com>
2019-04-18 03:49:18 -0700

    Move MockitoInternalUsage checker to the Mockito open source errorprone plugins
    This checker is opensourced at https://github.com/mockito/mockito/pull/1690
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=244160840

--- core/src/main/java/com/google/errorprone/scanner/BuiltInCheckerSuppliers.java
+++ core/src/main/java/com/google/errorprone/scanner/BuiltInCheckerSuppliers.java
@@ -172,7 +172,6 @@ import com.google.errorprone.bugpatterns.MixedArrayDimensions;
 import com.google.errorprone.bugpatterns.MixedDescriptors;
 import com.google.errorprone.bugpatterns.MixedMutabilityReturnType;
 import com.google.errorprone.bugpatterns.MockitoCast;
-import com.google.errorprone.bugpatterns.MockitoInternalUsage;
 import com.google.errorprone.bugpatterns.MockitoUsage;
 import com.google.errorprone.bugpatterns.ModifiedButNotUsed;
 import com.google.errorprone.bugpatterns.ModifyCollectionInEnhancedForLoop;
@@ -677,7 +676,6 @@ public class BuiltInCheckerSuppliers {
           MissingOverride.class,
           MixedDescriptors.class,
           MixedMutabilityReturnType.class,
-          MockitoInternalUsage.class,
           ModifiedButNotUsed.class,
           ModifyCollectionInEnhancedForLoop.class,
           ModifySourceCollectionInStream.class,
