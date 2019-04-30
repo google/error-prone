a4b9dce19c5a1df745091aa4335bf819ca7e8c6a
ghm <ghm@google.com>
2019-04-30 03:51:45 -0700

    SameNameButDifferent: suggest qualifying cases where the same simple name is used to refer to different types within a file.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=245919677

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/SameNameButDifferent.java
@@ -0,0 +1,230 @@
+/*
+ * Copyright 2019 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.ProvidesFix.REQUIRES_HUMAN_ATTENTION;
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static com.google.errorprone.util.ASTHelpers.findPathFromEnclosingNodeToTopLevel;
+import static com.google.errorprone.util.ASTHelpers.getSymbol;
+import static com.google.errorprone.util.FindIdentifiers.findIdent;
+import static java.util.stream.Collectors.joining;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.CompilationUnitTreeMatcher;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.matchers.Description;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.util.TreePath;
+import com.sun.source.util.TreePathScanner;
+import com.sun.tools.javac.code.Kinds.KindSelector;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.code.Symbol.ClassSymbol;
+import com.sun.tools.javac.code.Symbol.TypeSymbol;
+import com.sun.tools.javac.util.Position;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.lang.model.element.ElementKind;
+
+/** Looks for types being shadowed by other types in a way that may be confusing. */
+@BugPattern(
+    name = "SameNameButDifferent",
+    summary = "This type name shadows another in a way that may be confusing.",
+    providesFix = REQUIRES_HUMAN_ATTENTION,
+    severity = WARNING)
+public final class SameNameButDifferent extends BugChecker implements CompilationUnitTreeMatcher {
+  @Override
+  public Description matchCompilationUnit(CompilationUnitTree tree, VisitorState state) {
+    Table<String, TypeSymbol, List<TreePath>> table = HashBasedTable.create();
+    new TreePathScanner<Void, Void>() {
+      @Override
+      public Void visitClass(ClassTree classTree, Void unused) {
+        return isSuppressed(classTree) ? null : super.visitClass(classTree, null);
+      }
+
+      @Override
+      public Void visitMethod(MethodTree methodTree, Void unused) {
+        return isSuppressed(methodTree) ? null : super.visitMethod(methodTree, null);
+      }
+
+      @Override
+      public Void visitVariable(VariableTree variableTree, Void unused) {
+        return isSuppressed(variableTree) ? null : super.visitVariable(variableTree, null);
+      }
+
+      @Override
+      public Void visitMemberSelect(MemberSelectTree memberSelectTree, Void unused) {
+        if (!shouldIgnore()) {
+          handle(memberSelectTree);
+        }
+        return super.visitMemberSelect(memberSelectTree, null);
+      }
+
+      @Override
+      public Void visitIdentifier(IdentifierTree identifierTree, Void unused) {
+        if (shouldIgnore()) {
+          return null;
+        }
+        if (!(getSymbol(identifierTree) instanceof ClassSymbol)) {
+          return null;
+        }
+        TreePath enclosingClass =
+            findPathFromEnclosingNodeToTopLevel(getCurrentPath(), ClassTree.class);
+        if (enclosingClass != null
+            && getSymbol(enclosingClass.getLeaf()) == getSymbol(identifierTree)) {
+          return null;
+        }
+        handle(identifierTree);
+        return null;
+      }
+
+      private boolean shouldIgnore() {
+        // Don't report duplicate hits if we're not at the tail of a series of member selects on
+        // classes.
+        Tree parentTree = getCurrentPath().getParentPath().getLeaf();
+        return parentTree instanceof MemberSelectTree
+            && getSymbol(parentTree) instanceof ClassSymbol;
+      }
+
+      private void handle(Tree tree) {
+        if (tree.toString().equals("Builder")) {
+          return;
+        }
+        Symbol symbol = getSymbol(tree);
+        if (symbol instanceof ClassSymbol) {
+          List<TreePath> treePaths = table.get(tree.toString(), symbol.type.tsym);
+          if (treePaths == null) {
+            treePaths = new ArrayList<>();
+            table.put(tree.toString(), symbol.type.tsym, treePaths);
+          }
+          if (state.getEndPosition(tree) != Position.NOPOS) {
+            treePaths.add(getCurrentPath());
+          }
+        }
+      }
+    }.scan(state.getPath(), null);
+
+    // Remove any (simpleName, typeSymbol) entries which don't contain any occurrences which
+    // shadow a class name outside the enclosing class.
+    for (Map.Entry<String, Map<TypeSymbol, List<TreePath>>> row : table.rowMap().entrySet()) {
+      Map<TypeSymbol, List<TreePath>> columns = row.getValue();
+      if (columns.size() <= 1) {
+        continue;
+      }
+      columns
+          .entrySet()
+          .removeIf(
+              cell ->
+                  cell.getValue().stream().noneMatch(treePath -> shadowsClass(state, treePath)));
+    }
+
+    for (Map.Entry<String, Map<TypeSymbol, List<TreePath>>> row : table.rowMap().entrySet()) {
+      String simpleName = row.getKey();
+      Map<TypeSymbol, List<TreePath>> columns = row.getValue();
+
+      SuggestedFix.Builder fix = SuggestedFix.builder();
+      if (columns.size() > 1) {
+        for (Map.Entry<TypeSymbol, List<TreePath>> cell : columns.entrySet()) {
+          for (TreePath treePath : cell.getValue()) {
+            TypeSymbol typeSymbol = cell.getKey();
+            getBetterImport(typeSymbol, simpleName)
+                .ifPresent(
+                    imp -> {
+                      String qualifiedName = qualifyType(state.withPath(treePath), fix, imp);
+                      String newSimpleName = qualifiedName + "." + simpleName;
+                      fix.replace(treePath.getLeaf(), newSimpleName);
+                    });
+          }
+        }
+        String message =
+            String.format(
+                "The name `%s` refers to %s within this file. It may be confusing to have the same"
+                    + " name refer to multiple types. Consider qualifying them for clarity.",
+                simpleName,
+                columns.keySet().stream()
+                    .map(t -> t.getQualifiedName().toString())
+                    .collect(joining(", ", "[", "]")));
+        for (List<TreePath> treePaths : table.row(simpleName).values()) {
+          for (TreePath treePath : treePaths) {
+            state.reportMatch(
+                buildDescription(treePath.getLeaf())
+                    .setMessage(message)
+                    .addFix(fix.build())
+                    .build());
+          }
+        }
+      }
+    }
+    return NO_MATCH;
+  }
+
+  private static boolean shadowsClass(VisitorState state, TreePath treePath) {
+    if (!(treePath.getLeaf() instanceof IdentifierTree)) {
+      return true;
+    }
+
+    TreePath enclosingClass = findPathFromEnclosingNodeToTopLevel(treePath, ClassTree.class);
+    String name = ((IdentifierTree) treePath.getLeaf()).getName().toString();
+    return findIdent(name, state.withPath(enclosingClass), KindSelector.VAL_TYP) != null;
+  }
+
+  private static Optional<Symbol> getBetterImport(TypeSymbol classSymbol, String simpleName) {
+    Symbol owner = classSymbol;
+    long dots = simpleName.chars().filter(c -> c == '.').count();
+    for (long i = 0; i < dots + 1; ++i) {
+      owner = owner.owner;
+    }
+    if (owner instanceof ClassSymbol) {
+      return Optional.of(owner);
+    }
+    return Optional.empty();
+  }
+
+  /** Try to qualify the type, or return the full name. */
+  public static String qualifyType(VisitorState state, SuggestedFix.Builder fix, Symbol sym) {
+    Deque<String> names = new ArrayDeque<>();
+    for (Symbol curr = sym; curr != null; curr = curr.owner) {
+      names.addFirst(curr.getSimpleName().toString());
+      Symbol found = findIdent(curr.getSimpleName().toString(), state, KindSelector.VAL_TYP);
+      if (found == curr) {
+        break;
+      }
+      if (curr.getKind() == ElementKind.PACKAGE) {
+        return sym.getQualifiedName().toString();
+      }
+      if (found == null) {
+        fix.addImport(curr.getQualifiedName().toString());
+        break;
+      }
+    }
+    return Joiner.on('.').join(names);
+  }
+}
---
+++ core/src/test/java/com/google/errorprone/bugpatterns/SameNameButDifferentTest.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2019 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link SameNameButDifferent}. */
+@RunWith(JUnit4.class)
+public final class SameNameButDifferentTest {
+
+  private final CompilationTestHelper helper =
+      CompilationTestHelper.newInstance(SameNameButDifferent.class, getClass());
+
+  private final BugCheckerRefactoringTestHelper refactoring =
+      BugCheckerRefactoringTestHelper.newInstance(new SameNameButDifferent(), getClass());
+
+  @Test
+  public void simpleNameClash() {
+    helper
+        .addSourceLines(
+            "A.java", //
+            "class A {",
+            "  class Supplier {}",
+            "}")
+        .addSourceLines(
+            "B.java",
+            "import java.util.function.Supplier;",
+            "class B {",
+            "  // BUG: Diagnostic contains:",
+            "  Supplier<Integer> supplier = () -> 1;",
+            "  class C extends A {",
+            "    // BUG: Diagnostic contains:",
+            "    Supplier supplier2 = new Supplier();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void simpleNameRefactoring() {
+    refactoring
+        .addInputLines(
+            "A.java", //
+            "package foo;",
+            "class A {",
+            "  class Supplier {}",
+            "}")
+        .expectUnchanged()
+        .addInputLines(
+            "B.java",
+            "package foo;",
+            "import java.util.function.Supplier;",
+            "class B {",
+            "  Supplier<Integer> supplier = () -> 1;",
+            "  class C extends A {",
+            "    Supplier supplier2 = new Supplier();",
+            "    Class<Supplier> clazz = Supplier.class;",
+            "  }",
+            "}")
+        .addOutputLines(
+            "B.java",
+            "package foo;",
+            "import java.util.function.Supplier;",
+            "class B {",
+            "  Supplier<Integer> supplier = () -> 1;",
+            "  class C extends A {",
+            "    A.Supplier supplier2 = new A.Supplier();",
+            "    Class<A.Supplier> clazz = A.Supplier.class;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void nestedClassNameClash() {
+    refactoring
+        .addInputLines(
+            "A.java", //
+            "package foo;",
+            "class A {",
+            "  static class B {",
+            "    static class C {}",
+            "  }",
+            "}")
+        .expectUnchanged()
+        .addInputLines(
+            "D.java",
+            "package foo;",
+            "class D {",
+            "  static class B {",
+            "    static class C {}",
+            "  }",
+            "}")
+        .expectUnchanged()
+        .addInputLines(
+            "E.java",
+            "package foo;",
+            "import foo.A.B;",
+            "class E {",
+            "  B.C foo = new B.C();",
+            "  class C extends D {",
+            "    B.C bar = new B.C();",
+            "  }",
+            "}")
+        .addOutputLines(
+            "E.java",
+            "package foo;",
+            "import foo.A.B;",
+            "class E {",
+            "  A.B.C foo = new A.B.C();",
+            "  class C extends D {",
+            "    D.B.C bar = new D.B.C();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void negativeAlreadyQualified() {
+    helper
+        .addSourceLines(
+            "A.java", //
+            "class A {",
+            "  class Supplier {}",
+            "}")
+        .addSourceLines(
+            "B.java",
+            "import java.util.function.Supplier;",
+            "class B {",
+            "  Supplier<Integer> supplier = () -> 1;",
+            "  class C extends A {",
+            "    A.Supplier supplier2 = new A.Supplier();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void neverShadowing() {
+    helper
+        .addSourceLines(
+            "A.java",
+            "class A {",
+            "  public void foo() {",
+            "    class B {}",
+            "    B b = new B();",
+            "  }",
+            "  public void bar() {",
+            "    class B {}",
+            "    B b = new B();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void referencesSelf() {
+    helper
+        .addSourceLines(
+            "B.java",
+            "import java.util.function.Supplier;",
+            "class B {",
+            "  Supplier<Integer> supplier = () -> 1;",
+            "  class C {",
+            "  class Supplier {",
+            "    Supplier s = new Supplier();",
+            "  }",
+            "  }",
+            "}")
+        .doTest();
+  }
+}
