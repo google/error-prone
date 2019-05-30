2852f861ab71f4439a576a44942524c6b0679b97
tvanderlippe <tvanderlippe@google.com>
2019-05-30 03:50:57 -0700

    Migrate org.mockito.Matchers#any* to org.mockito.ArgumentMatchers
    The former is deprecated and replaced by the latter in Mockito 2. However, there is a
    functional difference: ArgumentMatchers will reject `null` and check the type
    if the matcher specified a type (e.g. `any(Class)` or `anyInt()`). `any()` will
    remain to accept anything.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=250661666

--- check_api/src/test/java/com/google/errorprone/fixes/AppliedFixTest.java
+++ check_api/src/test/java/com/google/errorprone/fixes/AppliedFixTest.java
@@ -20,7 +20,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertThrows;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
