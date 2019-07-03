969c5021db347c260786e6f34642a95c45aa4e3d
cushon <cushon@google.com>
2019-07-02 17:34:52 -0700

    Handle static equals methods in TypeEqualsChecker
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=256273477

--- core/src/main/java/com/google/errorprone/bugpatterns/TypeEqualsChecker.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/TypeEqualsChecker.java
@@ -18,14 +18,21 @@ package com.google.errorprone.bugpatterns;
 
 import static com.google.errorprone.BugPattern.ProvidesFix.NO_FIX;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.argument;
+import static com.google.errorprone.matchers.Matchers.instanceMethod;
+import static com.google.errorprone.matchers.Matchers.staticEqualsInvocation;
+import static com.google.errorprone.matchers.Matchers.toType;
+import static com.google.errorprone.matchers.Matchers.typePredicateMatcher;
+import static com.google.errorprone.predicates.TypePredicates.isDescendantOf;
 
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
-import com.google.errorprone.matchers.method.MethodMatchers;
-import com.sun.source.tree.ExpressionTree;
+import com.google.errorprone.predicates.TypePredicate;
 import com.sun.source.tree.MethodInvocationTree;
 
 /**
@@ -43,10 +50,16 @@ import com.sun.source.tree.MethodInvocationTree;
     providesFix = NO_FIX)
 public class TypeEqualsChecker extends BugChecker implements MethodInvocationTreeMatcher {
 
-  private static final Matcher<ExpressionTree> TYPE_EQUALS =
-      MethodMatchers.instanceMethod()
-          .onDescendantOf("javax.lang.model.type.TypeMirror")
-          .named("equals");
+  private static final TypePredicate TYPE_MIRROR =
+      isDescendantOf("javax.lang.model.type.TypeMirror");
+
+  private static final Matcher<MethodInvocationTree> TYPE_EQUALS =
+      anyOf(
+          toType(MethodInvocationTree.class, instanceMethod().onClass(TYPE_MIRROR).named("equals")),
+          allOf(
+              staticEqualsInvocation(),
+              argument(0, typePredicateMatcher(TYPE_MIRROR)),
+              argument(1, typePredicateMatcher(TYPE_MIRROR))));
 
   @Override
   public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
