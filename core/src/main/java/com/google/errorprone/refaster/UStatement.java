8971f976dd06a1083a577d0f5310a099c9b1d4a9
bhagwani <bhagwani@google.com>
2019-07-30 15:44:31 -0700

    Fix some AutoValueImmutableFields warning in refaster code (3/N)
    AutoValue instances should be deeply immutable. Therefore, we recommend returning ImmutableList instead. Read more at https://github.com/google/auto/blob/master/value/userguide/builders-howto.md#collection.
    
    See https://errorprone.info/bugpattern/AutoValueImmutableFields.
    
    RELNOTES: Fix some AutoValueImmutableFields warning in refaster code
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=260811556

--- core/src/main/java/com/google/errorprone/refaster/UStatement.java
+++ core/src/main/java/com/google/errorprone/refaster/UStatement.java
@@ -16,6 +16,7 @@ package com.google.errorprone.refaster;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
 import com.google.errorprone.refaster.UStatement.UnifierWithUnconsumedStatements;
 import com.sun.source.tree.StatementTree;
 import com.sun.tools.javac.tree.JCTree.JCStatement;
@@ -37,12 +38,12 @@ public interface UStatement
     public static UnifierWithUnconsumedStatements create(
         Unifier unifier, List<? extends StatementTree> unconsumedStatements) {
       return new AutoValue_UStatement_UnifierWithUnconsumedStatements(
-          unifier, unconsumedStatements);
+          unifier, ImmutableList.copyOf(unconsumedStatements));
     }
 
     public abstract Unifier unifier();
 
-    public abstract List<? extends StatementTree> unconsumedStatements();
+    public abstract ImmutableList<? extends StatementTree> unconsumedStatements();
   }
 
   com.sun.tools.javac.util.List<JCStatement> inlineStatements(Inliner inliner)
