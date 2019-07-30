433b287bcccfbfc4669491800cb70da168e03ada
cushon <cushon@google.com>
2019-07-29 20:04:26 -0700

    Consolidate stripParentheses implementations
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=260634412

--- check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
+++ check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
@@ -321,10 +321,7 @@ public class ASTHelpers {
 
   /** Removes any enclosing parentheses from the tree. */
   public static Tree stripParentheses(Tree tree) {
-    while (tree instanceof ParenthesizedTree) {
-      tree = ((ParenthesizedTree) tree).getExpression();
-    }
-    return tree;
+    return tree instanceof ExpressionTree ? stripParentheses((ExpressionTree) tree) : tree;
   }
 
   /** Given an ExpressionTree, removes any enclosing parentheses. */
