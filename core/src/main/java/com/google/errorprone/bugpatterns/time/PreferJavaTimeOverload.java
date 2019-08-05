47f4c207d973c755563723c7970529fc41c54de8
kak <kak@google.com>
2019-08-05 12:30:28 -0700

    Split PreferJavaTimeOverload explanation into its own external markdown file (also fix a bug where "<long, TimeUnit>" was being rendered as HTML).
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=261743439

--- core/src/main/java/com/google/errorprone/bugpatterns/time/PreferJavaTimeOverload.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/time/PreferJavaTimeOverload.java
@@ -73,15 +73,6 @@ import java.util.concurrent.TimeUnit;
             + " not and cannot guarantee that the overloads have equivalent semantics, but that is"
             + " generally the case with overloaded methods.",
     severity = WARNING,
-    explanation =
-        "APIs that accept a java.time.Duration/Instant should be preferred, when available."
-            + " JodaTime is now considered a legacy library for Java 8+ users. Representing"
-            + " date/time concepts as numeric primitives is strongly discouraged (e.g., long"
-            + " timeout). APIs that require a <long, TimeUnit> pair suffer from a number of"
-            + " problems: 1) they may require plumbing 2 parameters through various layers of your"
-            + " application; 2) overflows are possible when doing any duration math; 3) they lack"
-            + " semantic meaning; 4) decomposing a duration into a <long, TimeUnit> is dangerous"
-            + " because of unit mismatch and/or excessive truncation.",
     providesFix = REQUIRES_HUMAN_ATTENTION)
 public final class PreferJavaTimeOverload extends BugChecker
     implements MethodInvocationTreeMatcher {
