aa16f633f4c19c6742441d6ae38f1289a69e63a5
amalloy <amalloy@google.com>
2019-08-08 13:55:49 -0700

    Avoid calling filter(x -> true)
    By hoisting out the check for skipInterfaces, we can avoid a useless filter.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=262426287

--- check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
+++ check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
@@ -587,8 +587,11 @@ public class ASTHelpers {
   private static Stream<MethodSymbol> findSuperMethods(
       MethodSymbol methodSymbol, Types types, boolean skipInterfaces) {
     TypeSymbol owner = (TypeSymbol) methodSymbol.owner;
-    return types.closure(owner.type).stream()
-        .filter(closureTypes -> skipInterfaces ? !closureTypes.isInterface() : true)
+    Stream<Type> typeStream = types.closure(owner.type).stream();
+    if (skipInterfaces) {
+      typeStream = typeStream.filter(type -> !type.isInterface());
+    }
+    return typeStream
         .map(type -> findSuperMethodInType(methodSymbol, type, types))
         .filter(Objects::nonNull);
   }
