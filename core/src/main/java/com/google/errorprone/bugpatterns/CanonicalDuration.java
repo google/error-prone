8d0b9a1a0cdb961088b78a8e1d08087bc226594e
awturner <awturner@google.com>
2019-09-27 00:44:31 -0700

    CanonicalDuration: inspect the containing expression to ensure that all usages of the same method can be similarly canonicalized. Otherwise, changing just one is probably just irritating inconsistency.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=271518159

--- core/src/main/java/com/google/errorprone/bugpatterns/CanonicalDuration.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/CanonicalDuration.java
@@ -25,6 +25,7 @@ import static com.google.errorprone.matchers.method.MethodMatchers.staticMethod;
 import static com.google.errorprone.util.ASTHelpers.constValue;
 import static com.google.errorprone.util.ASTHelpers.getReceiver;
 import static com.google.errorprone.util.ASTHelpers.getSymbol;
+import static java.util.stream.Collectors.toList;
 
 import com.google.common.base.Converter;
 import com.google.common.collect.ImmutableMap;
@@ -35,16 +36,22 @@ import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
 import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LiteralTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.Tree;
+import com.sun.source.util.TreePath;
+import com.sun.source.util.TreeScanner;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /** @author cushon@google.com (Liam Miller-Cushon) */
 @BugPattern(
@@ -126,19 +133,96 @@ public class CanonicalDuration extends BugChecker implements MethodInvocationTre
       // TODO(cushon): ofSeconds w/ nano adjustment?
       return NO_MATCH;
     }
-    Tree arg = getOnlyElement(tree.getArguments());
-    if (!(arg instanceof LiteralTree)) {
-      // don't inline constants
+
+    List<MethodInvocationTree> allInvocationsInParentExpression =
+        getAllInvocationsInParentExpression(state);
+    if (allInvocationsInParentExpression.isEmpty()) {
       return NO_MATCH;
     }
-    Number constValue = constValue(arg, Number.class);
-    if (constValue == null) {
+
+    List<Number> constValues =
+        allInvocationsInParentExpression.stream()
+            .map(t -> getOnlyElement(t.getArguments()))
+            .map(arg -> (arg instanceof LiteralTree) ? arg : null)
+            .map(arg -> constValue(arg, Number.class))
+            .collect(toList());
+
+    if (constValues.stream().anyMatch(Objects::isNull)) {
       return NO_MATCH;
     }
-    long value = constValue.longValue();
-    if (value == 0) {
-      switch (api) {
-        case JODA:
+
+    if (constValues.stream().mapToLong(Number::longValue).allMatch(v -> v == 0L)) {
+      return handleAllZeros(state, api, allInvocationsInParentExpression);
+    }
+
+    MethodSymbol sym = getSymbol(tree);
+    if (!METHOD_NAME_TO_UNIT.containsKey(sym.getSimpleName().toString())) {
+      return NO_MATCH;
+    }
+    TemporalUnit unit = METHOD_NAME_TO_UNIT.get(sym.getSimpleName().toString());
+    Long banListValue = BANLIST.get(unit);
+    if (banListValue != null
+        && constValues.stream()
+            .anyMatch(value -> Objects.equals(banListValue, value.longValue()))) {
+      return NO_MATCH;
+    }
+
+    List<Duration> durations =
+        constValues.stream().map(value -> Duration.of(value.longValue(), unit)).collect(toList());
+    // Iterate over all possible units from largest to smallest (days to nanos) until we find the
+    // largest unit that can be used to exactly express the duration.
+    for (Map.Entry<ChronoUnit, Converter<Duration, Long>> entry : CONVERTERS.entrySet()) {
+      ChronoUnit nextUnit = entry.getKey();
+      if (unit.equals(nextUnit)) {
+        // We reached the original unit, no simplification is possible.
+        break;
+      }
+      Converter<Duration, Long> converter = entry.getValue();
+
+      List<Duration> roundTripped =
+          durations.stream()
+              .map(converter::convert)
+              .map(converter.reverse()::convert)
+              .collect(toList());
+
+      if (roundTripped.equals(durations)) {
+        // We reached a larger than original unit that precisely expresses the duration, rewrite to
+        // use it instead.
+        for (int i = 0; i < allInvocationsInParentExpression.size(); i++) {
+          MethodInvocationTree m = allInvocationsInParentExpression.get(i);
+          long nextValue = converter.convert(durations.get(i));
+          String name = FACTORIES.get(api, nextUnit);
+          String replacement =
+              String.format("%s(%d%s)", name, nextValue, nextValue == ((int) nextValue) ? "" : "L");
+          ExpressionTree receiver = getReceiver(m);
+          if (receiver == null) { // static import of the method
+            SuggestedFix fix =
+                SuggestedFix.builder()
+                    .addStaticImport(api.getDurationFullyQualifiedName() + "." + name)
+                    .replace(m, replacement)
+                    .build();
+            state.reportMatch(describeMatch(m, fix));
+          } else {
+            state.reportMatch(
+                describeMatch(
+                    m,
+                    SuggestedFix.replace(
+                        state.getEndPosition(receiver),
+                        state.getEndPosition(m),
+                        "." + replacement)));
+          }
+        }
+        return Description.NO_MATCH;
+      }
+    }
+    return NO_MATCH;
+  }
+
+  private Description handleAllZeros(
+      VisitorState state, Api api, List<MethodInvocationTree> allInvocationsInParentExpression) {
+    switch (api) {
+      case JODA:
+        for (MethodInvocationTree tree : allInvocationsInParentExpression) {
           ExpressionTree receiver = getReceiver(tree);
           SuggestedFix fix;
           if (receiver == null) { // static import of the method
@@ -152,57 +236,75 @@ public class CanonicalDuration extends BugChecker implements MethodInvocationTre
                 SuggestedFix.replace(
                     state.getEndPosition(getReceiver(tree)), state.getEndPosition(tree), ".ZERO");
           }
-          return buildDescription(tree)
-              .setMessage("Duration can be expressed more clearly without units, as Duration.ZERO")
-              .addFix(fix)
-              .build();
-        case JAVA:
-          // don't rewrite e.g. `ofMillis(0)` to `ofDays(0)`
-          return NO_MATCH;
-      }
-      throw new AssertionError(api);
+          state.reportMatch(
+              buildDescription(tree)
+                  .setMessage(
+                      "Duration can be expressed more clearly without units, as Duration.ZERO")
+                  .addFix(fix)
+                  .build());
+        }
+        return NO_MATCH;
+      case JAVA:
+        // don't rewrite e.g. `ofMillis(0)` to `ofDays(0)`
+        return NO_MATCH;
     }
-    MethodSymbol sym = getSymbol(tree);
-    if (!METHOD_NAME_TO_UNIT.containsKey(sym.getSimpleName().toString())) {
-      return NO_MATCH;
-    }
-    TemporalUnit unit = METHOD_NAME_TO_UNIT.get(sym.getSimpleName().toString());
-    if (Objects.equals(BANLIST.get(unit), value)) {
-      return NO_MATCH;
-    }
-    Duration duration = Duration.of(value, unit);
-    // Iterate over all possible units from largest to smallest (days to nanos) until we find the
-    // largest unit that can be used to exactly express the duration.
-    for (Map.Entry<ChronoUnit, Converter<Duration, Long>> entry : CONVERTERS.entrySet()) {
-      ChronoUnit nextUnit = entry.getKey();
-      if (unit.equals(nextUnit)) {
-        // We reached the original unit, no simplification is possible.
+    throw new AssertionError(api);
+  }
+
+  private static List<MethodInvocationTree> getAllInvocationsInParentExpression(
+      VisitorState state) {
+    // Walk up the tree path until the parent tree is no longer an expression.
+    TreePath expressionPath = state.getPath();
+    while (true) {
+      TreePath parentPath = expressionPath.getParentPath();
+      if (parentPath == null) {
         break;
       }
-      Converter<Duration, Long> converter = entry.getValue();
-      long nextValue = converter.convert(duration);
-      if (converter.reverse().convert(nextValue).equals(duration)) {
-        // We reached a larger than original unit that precisely expresses the duration, rewrite to
-        // use it instead.
-        String name = FACTORIES.get(api, nextUnit);
-        String replacement =
-            String.format("%s(%d%s)", name, nextValue, nextValue == ((int) nextValue) ? "" : "L");
-        ExpressionTree receiver = getReceiver(tree);
-        if (receiver == null) { // static import of the method
-          SuggestedFix fix =
-              SuggestedFix.builder()
-                  .addStaticImport(api.getDurationFullyQualifiedName() + "." + name)
-                  .replace(tree, replacement)
-                  .build();
-          return describeMatch(tree, fix);
-        } else {
-          return describeMatch(
-              tree,
-              SuggestedFix.replace(
-                  state.getEndPosition(receiver), state.getEndPosition(tree), "." + replacement));
-        }
+      if (!(expressionPath.getParentPath().getLeaf() instanceof ExpressionTree)) {
+        break;
       }
+      expressionPath = parentPath;
     }
-    return NO_MATCH;
+
+    // This check gets run on all invocations of the duration methods, but we propose fixes for
+    // all things in the same expression. As such, we detect if we are the first invocation of the
+    // method, and if not, we don't check any further, because all trees will be checked when the
+    // first invocation is checked.
+    AtomicBoolean notFirst = new AtomicBoolean();
+
+    // Scan the tree for invocations of the same method.
+    MethodInvocationTree tree = (MethodInvocationTree) state.getPath().getLeaf();
+    MethodSymbol methodSymbol = getSymbol(tree);
+    List<MethodInvocationTree> sameMethodInvocations = new ArrayList<>();
+    new TreeScanner<Void, Void>() {
+      @Override
+      public Void scan(Tree node, Void aVoid) {
+        if (notFirst.get()) {
+          return null;
+        }
+        return super.scan(node, aVoid);
+      }
+
+      @Override
+      public Void visitMethodInvocation(MethodInvocationTree node, Void aVoid) {
+        if (Objects.equals(methodSymbol, ASTHelpers.getSymbol(node))) {
+          if (sameMethodInvocations.isEmpty()) {
+            if (!Objects.equals(node, tree)) {
+              notFirst.set(true);
+              return null;
+            }
+          }
+
+          sameMethodInvocations.add(node);
+          return super.visitMethodInvocation(node, aVoid);
+        }
+        return super.visitMethodInvocation(node, aVoid);
+      }
+    }.scan(expressionPath.getLeaf(), null);
+
+    if (notFirst.get()) {
+      sameMethodInvocations.clear();
+    }
+    return sameMethodInvocations;
   }
 }
