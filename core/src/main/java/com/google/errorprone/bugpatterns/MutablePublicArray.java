beb819b6b5d10030791792e049be60e1bd4a70a2
abhatiya <abhatiya@google.com>
2020-02-05 03:26:17 -0800

    Add ArrayPublicStaticFinalDeclaration check.
    Non-empty arrays that are declared public, static, and final remain mutable. This check warns for such cases.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=293334789

---
+++ core/src/main/java/com/google/errorprone/bugpatterns/MutablePublicArray.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2020 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.hasModifier;
+import static com.google.errorprone.matchers.Matchers.isArrayType;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.BugPattern.ProvidesFix;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.VariableTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.Tree.Kind;
+import com.sun.source.tree.VariableTree;
+import com.sun.tools.javac.tree.JCTree.JCLiteral;
+import com.sun.tools.javac.tree.JCTree.JCNewArray;
+import javax.lang.model.element.Modifier;
+
+/** Check for public static final declaration of Arrays. */
+@BugPattern(
+    name = "MutablePublicArray",
+    summary =
+        "Nonzero-length arrays are mutable, so this `public static final` array is not a constant"
+            + " and can be modified by clients of this class.  Prefer an ImmutableList, or provide"
+            + " an accessor method that returns a defensive copy.",
+    severity = WARNING,
+    providesFix = ProvidesFix.REQUIRES_HUMAN_ATTENTION)
+public class MutablePublicArray extends BugChecker implements VariableTreeMatcher {
+
+  private static final Matcher<VariableTree> NON_EMPTY_PUBLIC_STATIC_FINAL_ARRAY_DECLARATION =
+      allOf(
+          hasModifier(Modifier.PUBLIC),
+          hasModifier(Modifier.STATIC),
+          hasModifier(Modifier.FINAL),
+          new NonEmptyArrayMatcher());
+
+  @Override
+  public Description matchVariable(VariableTree tree, VisitorState state) {
+    if (NON_EMPTY_PUBLIC_STATIC_FINAL_ARRAY_DECLARATION.matches(tree, state)) {
+      return describeMatch(tree);
+    }
+    return Description.NO_MATCH;
+  }
+
+  private static class NonEmptyArrayMatcher implements Matcher<VariableTree> {
+    @Override
+    public boolean matches(VariableTree arrayExpression, VisitorState state) {
+      if (isArrayType().matches(arrayExpression, state)) {
+        JCNewArray newArray = (JCNewArray) arrayExpression.getInitializer();
+        if (!newArray.getDimensions().isEmpty()) {
+          return newArray.getDimensions().stream()
+              .allMatch(
+                  jcExpression -> {
+                    JCLiteral literal = (JCLiteral) jcExpression;
+                    return literal.getKind() == Kind.INT_LITERAL
+                        && (Integer) literal.getValue() > 0;
+                  });
+        } else {
+          // For in line array initializer.
+          return newArray.getInitializers() != null && !newArray.getInitializers().isEmpty();
+        }
+      }
+      return false;
+    }
+  }
+}
---
+++ docs/bugpattern/MutablePublicArray.md
@@ -0,0 +1,12 @@
+Nonzero-length arrays are mutable. Declaring one `public static final` indicates
+that the developer expects it to be a constant, which is not the case. Making it
+`public` is especially dangerous since clients of this code can modify the
+contents of the array.
+
+There are two ways to fix this problem:
+
+1.  Refactor the array to an `ImmutableList`.
+2.  Make the array `private` and add a `public` method that returns a copy of
+    the `private` array.
+
+See Effective Java 3rd Edition, Item 15, for more details.
