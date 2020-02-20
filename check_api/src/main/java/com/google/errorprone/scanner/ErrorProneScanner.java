33cb1c9401d3fe44340867e40ecde85951417855
kmclarnon <kmclarnon@hubspot.com>
2020-02-20 12:56:54 -0500

    space

--- check_api/src/main/java/com/google/errorprone/scanner/ErrorProneScanner.java
+++ check_api/src/main/java/com/google/errorprone/scanner/ErrorProneScanner.java
@@ -930,7 +930,7 @@ public class ErrorProneScanner extends Scanner {
    * error and continue.
    */
   @Override
-  protected void handleError(Suppressible s, Throwable t)  {
+  protected void handleError(Suppressible s, Throwable t) {
     if (t instanceof ErrorProneError) {
       throw (ErrorProneError) t;
     }
