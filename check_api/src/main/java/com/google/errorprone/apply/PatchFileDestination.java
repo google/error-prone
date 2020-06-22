753485d357ae277ce6c0af19f852ef50fdae0cd0
Error Prone Team <java-team-github-bot@google.com>
2020-06-22 04:20:35 -0700

    Migrate java-diff-utils version to 4.0
    The source of java diff utils 4.0 was added at unknown commit.
    
    The old java-diff-utils library is no longer maintained and with a problematic LICENSE file [1].
    The new version is a fork of the original version which is under maintenance and fixed the LICENSE problem.
    
    [1] https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=845471#52
    
    conga-relnote: n/a
    RELNOTES: None
    PiperOrigin-RevId: 317628549

--- check_api/src/main/java/com/google/errorprone/apply/PatchFileDestination.java
+++ check_api/src/main/java/com/google/errorprone/apply/PatchFileDestination.java
@@ -18,10 +18,12 @@ package com.google.errorprone.apply;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.github.difflib.DiffUtils;
+import com.github.difflib.UnifiedDiffUtils;
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.patch.Patch;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
-import difflib.DiffUtils;
-import difflib.Patch;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
@@ -58,11 +60,15 @@ public final class PatchFileDestination implements FileDestination {
     if (!oldSource.equals(newSource)) {
       List<String> originalLines = LINE_SPLITTER.splitToList(oldSource);
 
-      Patch<String> diff = DiffUtils.diff(originalLines, LINE_SPLITTER.splitToList(newSource));
+      Patch<String> diff = null;
+      try {
+        diff = DiffUtils.diff(originalLines, LINE_SPLITTER.splitToList(newSource));
+      } catch (DiffException e) {
+        throw new AssertionError("DiffUtils.diff should not fail", e);
+      }
       String relativePath = baseDir.relativize(sourceFilePath).toString();
       List<String> unifiedDiff =
-          DiffUtils.generateUnifiedDiff(relativePath, relativePath, originalLines, diff, 2);
-
+          UnifiedDiffUtils.generateUnifiedDiff(relativePath, relativePath, originalLines, diff, 2);
       String diffString = Joiner.on("\n").join(unifiedDiff) + "\n";
       diffByFile.put(sourceFilePath.toUri(), diffString);
     }
