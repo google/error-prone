e1610e37382490ca99fec638a324b23a8dce3bfa
ghm <ghm@google.com>
2020-07-24 10:51:35 -0700

    Extend FieldCanBeStatic to handle way more cases: known immutable types being instantiated with pure things.
    PiperOrigin-RevId: 323026767

--- core/src/main/java/com/google/errorprone/bugpatterns/FieldCanBeStatic.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/FieldCanBeStatic.java
@@ -16,70 +16,268 @@
 
 package com.google.errorprone.bugpatterns;
 
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
+import static com.google.errorprone.fixes.SuggestedFixes.addModifiers;
 import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.method.MethodMatchers.constructor;
+import static com.google.errorprone.matchers.method.MethodMatchers.staticMethod;
+import static com.google.errorprone.util.ASTHelpers.getSymbol;
+import static com.google.errorprone.util.ASTHelpers.getType;
+import static com.google.errorprone.util.ASTHelpers.isConsideredFinal;
+import static javax.lang.model.element.ElementKind.FIELD;
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.STATIC;
 
-import com.google.common.base.Ascii;
-import com.google.common.base.CaseFormat;
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
 import com.google.errorprone.BugPattern;
+import com.google.errorprone.ErrorProneFlags;
 import com.google.errorprone.VisitorState;
+import com.google.errorprone.annotations.Immutable;
 import com.google.errorprone.bugpatterns.BugChecker.VariableTreeMatcher;
+import com.google.errorprone.bugpatterns.threadsafety.ThreadSafety;
+import com.google.errorprone.bugpatterns.threadsafety.WellKnownMutability;
 import com.google.errorprone.fixes.SuggestedFix;
-import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.Description;
-import com.google.errorprone.util.ASTHelpers;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.ParenthesizedTree;
+import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
+import com.sun.source.util.TreeScanner;
+import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symbol.VarSymbol;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.Modifier;
+import com.sun.tools.javac.code.Type;
+import com.sun.tools.javac.tree.JCTree;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Pattern;
 
-/** @author andrew@gaul.org (Andrew Gaul) */
+/** Finds fields which can be safely made static. */
 @BugPattern(
     name = "FieldCanBeStatic",
     summary =
         "A final field initialized at compile-time with an instance of an immutable type can be"
             + " static.",
     severity = SUGGESTION)
-public class FieldCanBeStatic extends BugChecker implements VariableTreeMatcher {
+public final class FieldCanBeStatic extends BugChecker implements VariableTreeMatcher {
+
+  private static final Matcher<ExpressionTree> PURE_METHODS =
+      anyOf(
+          staticMethod().onClass("com.google.common.base.Optional"),
+          staticMethod().onClass("com.google.common.base.Pair"),
+          staticMethod().onClass("com.google.common.base.Splitter"),
+          staticMethod().onClass("com.google.common.collect.ImmutableBiMap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableCollection"),
+          staticMethod().onClass("com.google.common.collect.ImmutableList"),
+          staticMethod().onClass("com.google.common.collect.ImmutableListMultimap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableMap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableMultimap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableMultiset"),
+          staticMethod().onClass("com.google.common.collect.ImmutableRangeMap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableRangeSet"),
+          staticMethod().onClass("com.google.common.collect.ImmutableSet"),
+          staticMethod().onClass("com.google.common.collect.ImmutableSetMultimap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableSortedMap"),
+          staticMethod().onClass("com.google.common.collect.ImmutableSortedMultiset"),
+          staticMethod().onClass("com.google.common.collect.ImmutableSortedSet"),
+          staticMethod().onClass("com.google.common.collect.ImmutableTable"),
+          staticMethod().onClass("com.google.common.collect.Range"),
+          staticMethod().onClass("com.google.protobuf.GeneratedMessage"),
+          staticMethod()
+              .onClass("java.time.Duration")
+              .namedAnyOf("ofNanos", "ofMillis", "ofSeconds", "ofMinutes", "ofHours", "ofDays")
+              .withParameters("long"),
+          staticMethod()
+              .onClass("java.time.Instant")
+              .namedAnyOf("ofEpochMilli", "ofEpochSecond")
+              .withParameters("long"),
+          staticMethod()
+              .onClass("com.google.protobuf.util.Timestamps")
+              .namedAnyOf("fromNanos", "fromMicros", "fromMillis", "fromSeconds"),
+          staticMethod()
+              .onClass("com.google.protobuf.util.Durations")
+              .namedAnyOf(
+                  "fromNanos",
+                  "fromMicros",
+                  "fromMillis",
+                  "fromSeconds",
+                  "fromMinutes",
+                  "fromHours",
+                  "fromDays"),
+          staticMethod()
+              .onClass("org.joda.time.Duration")
+              .namedAnyOf(
+                  "millis", "standardSeconds", "standardMinutes", "standardHours", "standardDays")
+              .withParameters("long"),
+          constructor().forClass("org.joda.time.Instant").withParameters("long"),
+          constructor().forClass("org.joda.time.DateTime").withParameters("long"),
+          staticMethod()
+              .onClass("java.time.LocalDate")
+              .withNameMatching(Pattern.compile("^?!(now)")),
+          staticMethod()
+              .onClass("java.time.LocalDateTime")
+              .withNameMatching(Pattern.compile("^?!(now)")),
+          staticMethod()
+              .onClass("java.time.LocalTime")
+              .withNameMatching(Pattern.compile("^?!(now)")),
+          staticMethod().onClass("java.time.MonthDay"),
+          staticMethod()
+              .onClass("java.time.OffsetDateTime")
+              .withNameMatching(Pattern.compile("^?!(now)")),
+          staticMethod()
+              .onClass("java.time.OffsetTime")
+              .withNameMatching(Pattern.compile("^?!(now)")),
+          staticMethod().onClass("java.time.Period"),
+          staticMethod().onClass("java.time.Year"),
+          staticMethod().onClass("java.time.YearMonth"),
+          staticMethod().onClass("java.time.ZoneId"),
+          staticMethod().onClass("java.time.ZoneOffset"),
+          staticMethod()
+              .onClass("java.time.ZonedDateTime")
+              .withNameMatching(Pattern.compile("^?!(now)")),
+          staticMethod().onClass("java.util.Optional"),
+          staticMethod().onClass("java.util.OptionalDouble"),
+          staticMethod().onClass("java.util.OptionalInt"),
+          staticMethod().onClass("java.util.OptionalLong"),
+          staticMethod().onClass("java.util.regex.Pattern"),
+          staticMethod().onClass("org.joda.time.DateTime"),
+          staticMethod().onClass("org.joda.time.DateTimeZone"),
+          staticMethod().onClass("org.joda.time.Days"),
+          staticMethod().onClass("org.joda.time.Duration"),
+          staticMethod().onClass("org.joda.time.Instant"),
+          staticMethod().onClass("org.joda.time.Interval"),
+          staticMethod().onClass("org.joda.time.LocalDate"),
+          staticMethod().onClass("org.joda.time.LocalDateTime"),
+          staticMethod().onClass("org.joda.time.Period"),
+          staticMethod().onClass("org.joda.time.format.DateTimeFormatter"));
+
+  private final WellKnownMutability wellKnownMutability;
+
+  public FieldCanBeStatic(ErrorProneFlags flags) {
+    this.wellKnownMutability = WellKnownMutability.fromFlags(flags);
+  }
 
   @Override
   public Description matchVariable(VariableTree tree, VisitorState state) {
-    VarSymbol sym = ASTHelpers.getSymbol(tree);
-    if (!canBeStatic(sym)) {
+    VarSymbol symbol = getSymbol(tree);
+    if (symbol == null
+        || !symbol.isPrivate()
+        || !tree.getModifiers().getFlags().contains(FINAL)
+        || symbol.isStatic()
+        || !symbol.getKind().equals(FIELD)) {
       return NO_MATCH;
     }
-    SuggestedFix.Builder fix = SuggestedFix.builder();
-    SuggestedFixes.addModifiers(tree, state, Modifier.STATIC).ifPresent(fix::merge);
-    String name = tree.getName().toString();
-    if (!name.equals(Ascii.toUpperCase(name))) {
-      String renamed = CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, name);
-      fix.merge(SuggestedFixes.renameVariable(tree, renamed, state));
+    if (!isTypeKnownImmutable(getType(tree), state)) {
+      return NO_MATCH;
     }
-    return describeMatch(tree.getModifiers(), fix.build());
+    if (!isPure(tree.getInitializer(), state)) {
+      return NO_MATCH;
+    }
+    SuggestedFix fix =
+        SuggestedFix.builder()
+            .merge(renameVariable(tree, state))
+            .merge(addModifiers(tree, state, STATIC).orElse(SuggestedFix.builder().build()))
+            .build();
+    return describeMatch(tree, fix);
   }
 
-  private static boolean canBeStatic(VarSymbol sym) {
-    if (sym == null) {
-      return false;
+  /**
+   * Renames the variable, clobbering any qualifying (like {@code this.}). This is a tad unsafe, but
+   * we need to somehow remove any qualification with an instance.
+   */
+  private SuggestedFix renameVariable(VariableTree variableTree, VisitorState state) {
+    String name = variableTree.getName().toString();
+    if (!LOWER_CAMEL_PATTERN.matcher(name).matches()) {
+      return SuggestedFix.builder().build();
     }
-    if (!sym.getKind().equals(ElementKind.FIELD)) {
-      return false;
-    }
-    if (!sym.getModifiers().contains(Modifier.FINAL)) {
-      return false;
-    }
-    if (!sym.isPrivate()) {
-      return false;
-    }
-    if (sym.isStatic()) {
-      return false;
-    }
-    if (sym.getConstantValue() == null) {
-      return false;
-    }
-    if (sym.hasAnnotations()) {
-      return false;
-    }
-    return true;
+    String replacement = LOWER_CAMEL.to(UPPER_UNDERSCORE, variableTree.getName().toString());
+    int typeEndPos = state.getEndPosition(variableTree.getType());
+    int searchOffset = typeEndPos - ((JCTree) variableTree).getStartPosition();
+    int pos =
+        ((JCTree) variableTree).getStartPosition()
+            + state.getSourceForNode(variableTree).indexOf(name, searchOffset);
+    SuggestedFix.Builder fix =
+        SuggestedFix.builder().replace(pos, pos + name.length(), replacement);
+    VarSymbol sym = getSymbol(variableTree);
+    new TreeScanner<Void, Void>() {
+      @Override
+      public Void visitIdentifier(IdentifierTree tree, Void unused) {
+        handle(tree);
+        return super.visitIdentifier(tree, null);
+      }
+
+      @Override
+      public Void visitMemberSelect(MemberSelectTree tree, Void unused) {
+        handle(tree);
+        return super.visitMemberSelect(tree, null);
+      }
+
+      private void handle(Tree tree) {
+        if (sym.equals(getSymbol(tree))) {
+          fix.replace(tree, replacement);
+        }
+      }
+    }.scan(state.getPath().getCompilationUnit(), null);
+    return fix.build();
+  }
+
+  private static final Pattern LOWER_CAMEL_PATTERN = Pattern.compile("[a-z][a-zA-Z0-9]+");
+
+  /**
+   * Tries to establish whether an expression is pure. For example, literals and invocations of
+   * known-pure functions are pure.
+   */
+  private boolean isPure(ExpressionTree initializer, VisitorState state) {
+    AtomicBoolean isPure = new AtomicBoolean(true);
+    new TreeScanner<Void, Void>() {
+      @Override
+      public Void scan(Tree tree, Void unused) {
+        if (tree instanceof MethodInvocationTree) {
+          if (!PURE_METHODS.matches((ExpressionTree) tree, state)) {
+            isPure.set(false);
+          }
+          return super.scan(tree, null);
+        } else if (tree instanceof BinaryTree
+            || tree instanceof LiteralTree
+            || tree instanceof ParenthesizedTree) {
+          return super.scan(tree, null);
+        } else if (tree instanceof IdentifierTree || tree instanceof MemberSelectTree) {
+          Symbol symbol = getSymbol(tree);
+          if (symbol instanceof VarSymbol && !(symbol.isStatic() && isConsideredFinal(symbol))) {
+            isPure.set(false);
+          }
+          return super.scan(tree, null);
+        } else {
+          isPure.set(false);
+          return null;
+        }
+      }
+    }.scan(initializer, null);
+    return isPure.get();
+  }
+
+  private boolean isTypeKnownImmutable(Type type, VisitorState state) {
+    ThreadSafety threadSafety =
+        ThreadSafety.builder()
+            .setPurpose(ThreadSafety.Purpose.FOR_IMMUTABLE_CHECKER)
+            .knownTypes(wellKnownMutability)
+            .acceptedAnnotations(
+                ImmutableSet.of(Immutable.class.getName(), AutoValue.class.getName()))
+            .markerAnnotations(ImmutableSet.of())
+            .build(state);
+    return !threadSafety
+        .isThreadSafeType(
+            /* allowContainerTypeParameters= */ true,
+            threadSafety.threadSafeTypeParametersInScope(type.tsym),
+            type)
+        .isPresent();
   }
 }
--- core/src/test/java/com/google/errorprone/bugpatterns/FieldCanBeStaticTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/FieldCanBeStaticTest.java
@@ -18,6 +18,7 @@ package com.google.errorprone.bugpatterns;
 
 import com.google.errorprone.BugCheckerRefactoringTestHelper;
 import com.google.errorprone.CompilationTestHelper;
+import com.google.errorprone.ErrorProneFlags;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -25,10 +26,156 @@ import org.junit.runners.JUnit4;
 /** {@link FieldCanBeStatic}Test */
 @RunWith(JUnit4.class)
 public class FieldCanBeStaticTest {
+  private final BugCheckerRefactoringTestHelper helper =
+      BugCheckerRefactoringTestHelper.newInstance(
+          new FieldCanBeStatic(ErrorProneFlags.empty()), getClass());
+
+  private final CompilationTestHelper compilationHelper =
+      CompilationTestHelper.newInstance(FieldCanBeStatic.class, getClass());
+
+  @Test
+  public void simpleCase() {
+    helper
+        .addInputLines(
+            "Test.java",
+            "import java.time.Duration;",
+            "class Test {",
+            "  private final Duration myDuration = Duration.ofMillis(1);",
+            "  public Duration d() {",
+            "    return this.myDuration;",
+            "  }",
+            "}")
+        .addOutputLines(
+            "Test.java",
+            "import java.time.Duration;",
+            "class Test {",
+            "  private static final Duration MY_DURATION = Duration.ofMillis(1);",
+            "  public Duration d() {",
+            "    return MY_DURATION;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void instantWithPureMethod() {
+    helper
+        .addInputLines(
+            "Test.java",
+            "import java.time.Instant;",
+            "class Test {",
+            "  private final Instant instant = Instant.ofEpochMilli(1);",
+            "}")
+        .addOutputLines(
+            "Test.java",
+            "import java.time.Instant;",
+            "class Test {",
+            "  private static final Instant INSTANT = Instant.ofEpochMilli(1);",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void instantWithNonPureMethod() {
+    helper
+        .addInputLines(
+            "Test.java",
+            "import java.time.Instant;",
+            "class Test {",
+            "  private final Instant instant = Instant.now();",
+            "}")
+        .expectUnchanged()
+        .doTest();
+  }
+
+  @Test
+  public void refersToStaticVariable() {
+    helper
+        .addInputLines(
+            "Test.java",
+            "import java.time.Duration;",
+            "class Test {",
+            "  private static final int millis = 1;",
+            "  private final Duration myDuration = Duration.ofMillis(millis);",
+            "  public Duration d() {",
+            "    return this.myDuration;",
+            "  }",
+            "}")
+        .addOutputLines(
+            "Test.java",
+            "import java.time.Duration;",
+            "class Test {",
+            "  private static final int millis = 1;",
+            "  private static final Duration MY_DURATION = Duration.ofMillis(millis);",
+            "  public Duration d() {",
+            "    return MY_DURATION;",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void refersToInstanceVariable() {
+    helper
+        .addInputLines(
+            "Test.java",
+            "import java.time.Duration;",
+            "class Test {",
+            "  final int millis = 1;",
+            "  private final Duration myDuration = Duration.ofMillis(millis);",
+            "  public Duration d() {",
+            "    return this.myDuration;",
+            "  }",
+            "}")
+        .expectUnchanged()
+        .doTest();
+  }
+
+  @Test
+  public void notPrivate_noMatch() {
+    helper
+        .addInputLines(
+            "Test.java", //
+            "import java.time.Duration;",
+            "class Test {",
+            "  public final Duration d = Duration.ofMillis(1);",
+            "}")
+        .expectUnchanged()
+        .doTest();
+  }
+
+  @Test
+  public void notFinal_noMatch() {
+    helper
+        .addInputLines(
+            "Test.java", //
+            "import java.time.Duration;",
+            "class Test {",
+            "  private Duration d = Duration.ofMillis(1);",
+            "}")
+        .expectUnchanged()
+        .doTest();
+  }
+
+  @Test
+  public void possibleImpureInitializer_noMatch() {
+    helper
+        .addInputLines(
+            "Test.java", //
+            "import java.time.Duration;",
+            "class Test {",
+            "  private final Duration d = getDuration();",
+            "  Duration getDuration() {",
+            "    return null;",
+            "  }",
+            "}")
+        .expectUnchanged()
+        .doTest();
+  }
 
   @Test
   public void positive() {
-    CompilationTestHelper.newInstance(FieldCanBeStatic.class, getClass())
+    compilationHelper
         .addSourceLines(
             "Test.java",
             "class Test {",
@@ -42,7 +189,7 @@ public class FieldCanBeStaticTest {
 
   @Test
   public void negative() {
-    CompilationTestHelper.newInstance(FieldCanBeStatic.class, getClass())
+    compilationHelper
         .addSourceLines(
             "Test.java",
             "class Test {",
@@ -52,12 +199,10 @@ public class FieldCanBeStaticTest {
             "  }",
             "  private static final String staticFinal = \"string\";",
             "  private int nonFinal = 3;",
-            "  private static int staticNontFinal = 4;",
+            "  private static int staticNonFinal = 4;",
             "  private final Object finalMutable = new Object();",
             "  private final int nonLiteral = new java.util.Random().nextInt();",
             "  private final Person pojo = new Person(\"Bob\", 42);",
-            "  private final String nullString = null;",
-            "  @Deprecated private final String annotatedField = \"\";",
             "  private static class Person {",
             "    final String name;",
             "    final int age;",
@@ -72,7 +217,7 @@ public class FieldCanBeStaticTest {
 
   @Test
   public void refactoring() {
-    BugCheckerRefactoringTestHelper.newInstance(new FieldCanBeStatic(), getClass())
+    helper
         .addInputLines(
             "Test.java",
             "class Test {",
