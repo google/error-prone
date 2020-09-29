google/error-prone

commit 005372ab0fe90ac2b55565ee2c339f1cd2ec5930

Turn PredicateIncompatibleType into IsInstanceIncompatibleType.
I managed to accidentally re-implement the ::equals part of this in EqualsIncompatibleType, except more generally than just for Predicates.

So let's generalise the isInstance bit too!

PiperOrigin-RevId: 333258201

core/src/main/java/com/google/errorprone/bugpatterns/IsInstanceIncompatibleType.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright 2017 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static com.google.errorprone.matchers.Matchers.instanceMethod;
+import static com.google.errorprone.util.ASTHelpers.getReceiver;
+import static com.google.errorprone.util.ASTHelpers.getType;
+import static com.google.errorprone.util.ASTHelpers.isCastable;
+import static com.google.errorprone.util.Signatures.prettyType;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.MemberReferenceTreeMatcher;
+import com.google.errorprone.bugpatterns.BugChecker.MethodInvocationTreeMatcher;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.util.ASTHelpers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MemberReferenceTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Type;
+
+/**
+ * @author cushon@google.com (Liam Miller-Cushon)
+ * @author eleanorh@google.com (Eleanor Harris)
+ */
+@BugPattern(
+    name = "IsInstanceIncompatibleType",
+    summary = "This use of isInstance will always evaluate to false.",
+    severity = ERROR)
+public final class IsInstanceIncompatibleType extends BugChecker
+    implements MethodInvocationTreeMatcher, MemberReferenceTreeMatcher {
+  private static final Matcher<ExpressionTree> IS_INSTANCE =
+      instanceMethod().onExactClass("java.lang.Class").named("isInstance");
+
+  @Override
+  public Description matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+    if (!IS_INSTANCE.matches(tree, state)) {
+      return NO_MATCH;
+    }
+
+    Type receiverType = getType(getReceiver(tree)).getTypeArguments().get(0);
+    Type argumentType = getType(tree.getArguments().get(0));
+
+    return isCastable(argumentType, receiverType, state)
+        ? NO_MATCH
+        : buildMessage(argumentType, receiverType, tree, state);
+  }
+
+  @Override
+  public Description matchMemberReference(MemberReferenceTree tree, VisitorState state) {
+    if (!IS_INSTANCE.matches(tree, state)) {
+      return NO_MATCH;
+    }
+
+    Type type = state.getTypes().findDescriptorType(getType(tree));
+    Type receiverType = getType(getReceiver(tree)).getTypeArguments().get(0);
+    Type argumentType = ASTHelpers.getUpperBound(type.getParameterTypes().get(0), state.getTypes());
+
+    return isCastable(argumentType, receiverType, state)
+        ? NO_MATCH
+        : buildMessage(argumentType, receiverType, tree, state);
+  }
+
+  private Description buildMessage(Type type1, Type type2, Tree tree, VisitorState state) {
+    return buildDescription(tree)
+        .setMessage(
+            String.format(
+                "This expression will always evaluate to false because %s cannot be cast to %s",
+                prettyType(state.getTypes().erasure(type1)),
+                prettyType(state.getTypes().erasure(type2))))
+        .build();
+  }
+}

core/src/test/java/com/google/errorprone/bugpatterns/IsInstanceIncompatibleTypeTest.java
@@ -21,90 +21,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** {@link PredicateIncompatibleType}Test */
+/** {@link IsInstanceIncompatibleType}Test */
 @RunWith(JUnit4.class)
-public class PredicateIncompatibleTypeTest {
+public class IsInstanceIncompatibleTypeTest {
 
   private final CompilationTestHelper testHelper =
-      CompilationTestHelper.newInstance(PredicateIncompatibleType.class, getClass());
-
-  @Test
-  public void positive() {
-    testHelper
-        .addSourceLines(
-            "Test.java",
-            "import java.util.List;",
-            "import java.util.stream.Stream;",
-            "class Test {",
-            "  Stream<Integer> f(List<Integer> lx) {",
-            "    // BUG: Diagnostic contains: types String and Integer are incompatible",
-            "    return lx.stream().filter(\"\"::equals);",
-            "  }",
-            "}")
-        .doTest();
-  }
-
-  @Test
-  public void negative() {
-    testHelper
-        .addSourceLines(
-            "Test.java",
-            "import java.util.List;",
-            "import java.util.stream.Stream;",
-            "class Test {",
-            "  Stream<Integer> f(List<Integer> lx) {",
-            "    return lx.stream().filter(Integer.valueOf(42)::equals);",
-            "  }",
-            "}")
-        .doTest();
-  }
-
-  @Test
-  public void negativeHierarchy() {
-    testHelper
-        .addSourceLines(
-            "Test.java",
-            "import java.util.List;",
-            "import java.util.ArrayList;",
-            "import java.util.LinkedList;",
-            "import java.util.stream.Stream;",
-            "class Test {",
-            "  Stream<ArrayList<String>> f(List<ArrayList<String>> a, LinkedList<String> b) {",
-            "    return a.stream().filter(b::equals);",
-            "  }",
-            "}")
-        .doTest();
-  }
-
-  @Test
-  public void positive_guavaPredicate() {
-    testHelper
-        .addSourceLines(
-            "Test.java",
-            "import com.google.common.base.Predicate;",
-            "class Test {",
-            "  void f(Integer x) {",
-            "    // BUG: Diagnostic contains: types Integer and String are incompatible",
-            "    Predicate<String> p = x::equals;",
-            "  }",
-            "}")
-        .doTest();
-  }
-
-  @Test
-  public void notAPredicate() {
-    testHelper
-        .addSourceLines(
-            "Test.java",
-            "import java.util.List;",
-            "import java.util.function.BiFunction;",
-            "class Test {",
-            "  void f() {",
-            "    BiFunction<Object, Object, Boolean> f = Object::equals;",
-            "  }",
-            "}")
-        .doTest();
-  }
+      CompilationTestHelper.newInstance(IsInstanceIncompatibleType.class, getClass());
 
   @Test
   public void positiveInstanceOf() {
@@ -114,8 +36,23 @@ public class PredicateIncompatibleTypeTest {
             "import java.util.Optional;",
             "class Test {",
             "  Optional<String> f(Optional<String> s) {",
-            "    // BUG: Diagnostic contains: types String and Integer are incompatible",
+            "    // BUG: Diagnostic contains: String cannot be cast to Integer",
             "    return s.filter(Integer.class::isInstance);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void positiveInstanceOf_methodCall() {
+    testHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Optional;",
+            "class Test {",
+            "  Optional<String> f(Optional<String> s) {",
+            "    // BUG: Diagnostic contains: String cannot be cast to Integer",
+            "    return s.filter(x -> Integer.class.isInstance(x));",
             "  }",
             "}")
         .doTest();
@@ -130,7 +67,7 @@ public class PredicateIncompatibleTypeTest {
             "import java.util.HashMap;",
             "class Test {",
             "  Optional<HashMap<String,Integer>> f(Optional<HashMap<String,Integer>> m) {",
-            "    // BUG: Diagnostic contains: Predicate will always evaluate to false",
+            "    // BUG: Diagnostic contains: HashMap cannot be cast to Integer",
             "    return m.filter(Integer.class::isInstance);",
             "  }",
             "}")
@@ -146,7 +83,7 @@ public class PredicateIncompatibleTypeTest {
             "import java.lang.Number;",
             "class Test {",
             "  <T extends Number> Optional<T> f(Optional<T> t) {",
-            "    // BUG: Diagnostic contains: types Number and String are incompatible",
+            "    // BUG: Diagnostic contains: Number cannot be cast to String",
             "    return t.filter(String.class::isInstance);",
             "  }",
             "}")
@@ -164,6 +101,22 @@ public class PredicateIncompatibleTypeTest {
             "class Test {",
             "  Optional<HashMap> f(Optional<HashMap> m) {",
             "    return m.filter(LinkedHashMap.class::isInstance);",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void negativeInstanceOf_methodCall() {
+    testHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Optional;",
+            "import java.util.HashMap;",
+            "import java.util.LinkedHashMap;",
+            "class Test {",
+            "  Optional<HashMap> f(Optional<HashMap> m) {",
+            "    return m.filter(x -> LinkedHashMap.class.isInstance(x));",
             "  }",
             "}")
         .doTest();
@@ -194,21 +147,6 @@ public class PredicateIncompatibleTypeTest {
             "class Test {",
             "  <T> Optional<T> f(Optional<T> t) {",
             "    return t.filter(Object.class::isInstance);",
-            "  }",
-            "}")
-        .doTest();
-  }
-
-  /* String.class::isInstance is used as a Function<T, Boolean>, not a Predicate<T>. */
-  @Test
-  public void methodReferenceNotPredicate() {
-    testHelper
-        .addSourceLines(
-            "Test.java",
-            "import java.util.Optional;",
-            "class Test {",
-            "  <T> Optional<Boolean> f(Optional<T> t) {",
-            "    return t.map(String.class::isInstance);",
             "  }",
             "}")
         .doTest();

core/src/main/java/com/google/errorprone/bugpatterns/PredicateIncompatibleType.java
@@ -1,94 +1,0 @@
-/*
- * Copyright 2017 The Error Prone Authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.google.errorprone.bugpatterns;
-
-import static com.google.common.collect.Iterables.getOnlyElement;
-import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
-import static com.google.errorprone.matchers.Description.NO_MATCH;
-import static com.google.errorprone.util.Signatures.prettyType;
-
-import com.google.errorprone.BugPattern;
-import com.google.errorprone.VisitorState;
-import com.google.errorprone.bugpatterns.BugChecker.MemberReferenceTreeMatcher;
-import com.google.errorprone.matchers.Description;
-import com.google.errorprone.util.ASTHelpers;
-import com.sun.source.tree.MemberReferenceTree;
-import com.sun.tools.javac.code.Symbol;
-import com.sun.tools.javac.code.Type;
-
-/**
- * @author cushon@google.com (Liam Miller-Cushon)
- * @author eleanorh@google.com (Eleanor Harris)
- */
-@BugPattern(
-    name = "PredicateIncompatibleType",
-    summary =
-        "Using ::equals or ::isInstance as an incompatible Predicate;"
-            + " the predicate will always return false",
-    severity = ERROR)
-public class PredicateIncompatibleType extends BugChecker implements MemberReferenceTreeMatcher {
-
-  @Override
-  public Description matchMemberReference(MemberReferenceTree tree, VisitorState state) {
-
-    Type predicateType = predicateType(ASTHelpers.getType(tree), state);
-    if (predicateType == null) {
-      return NO_MATCH;
-    }
-    Type receiverType = ASTHelpers.getReceiverType(tree);
-
-    if (tree.getName().contentEquals("equals")
-        && !TypeCompatibilityUtils.compatibilityOfTypes(receiverType, predicateType, state)
-            .compatible()) {
-      return buildMessage(receiverType, predicateType, tree);
-    }
-
-    if (tree.getName().contentEquals("isInstance")
-        && ASTHelpers.isSameType(receiverType, state.getSymtab().classType, state)
-        && !receiverType.getTypeArguments().isEmpty()) {
-      Type argumentType = receiverType.getTypeArguments().get(0);
-      Type upperBound = ASTHelpers.getUpperBound(predicateType, state.getTypes());
-      if (!TypeCompatibilityUtils.compatibilityOfTypes(upperBound, argumentType, state)
-          .compatible()) {
-        return buildMessage(upperBound, argumentType, tree);
-      }
-    }
-
-    return NO_MATCH;
-  }
-
-  private static Type predicateType(Type type, VisitorState state) {
-    Symbol predicate = state.getSymbolFromString(java.util.function.Predicate.class.getName());
-    if (predicate == null) {
-      return null;
-    }
-    Type asPredicate = state.getTypes().asSuper(type, predicate);
-    if (asPredicate == null) {
-      return null;
-    }
-    return getOnlyElement(asPredicate.getTypeArguments(), null);
-  }
-
-  private Description buildMessage(Type type1, Type type2, MemberReferenceTree tree) {
-    return buildDescription(tree)
-        .setMessage(
-            String.format(
-                "Predicate will always evaluate to false because types %s and %s are incompatible",
-                prettyType(type1), prettyType(type2)))
-        .build();
-  }
-}

core/src/main/java/com/google/errorprone/scanner/BuiltInCheckerSuppliers.java
@@ -155,6 +155,7 @@ import com.google.errorprone.bugpatterns.InterruptedExceptionSwallowed;
 import com.google.errorprone.bugpatterns.InvalidPatternSyntax;
 import com.google.errorprone.bugpatterns.InvalidTimeZoneID;
 import com.google.errorprone.bugpatterns.InvalidZoneId;
+import com.google.errorprone.bugpatterns.IsInstanceIncompatibleType;
 import com.google.errorprone.bugpatterns.IsInstanceOfClass;
 import com.google.errorprone.bugpatterns.IterableAndIterator;
 import com.google.errorprone.bugpatterns.IterablePathParameter;
@@ -244,7 +245,6 @@ import com.google.errorprone.bugpatterns.ParameterName;
 import com.google.errorprone.bugpatterns.ParametersButNotParameterized;
 import com.google.errorprone.bugpatterns.PreconditionsCheckNotNullRepeated;
 import com.google.errorprone.bugpatterns.PreconditionsInvalidPlaceholder;
-import com.google.errorprone.bugpatterns.PredicateIncompatibleType;
 import com.google.errorprone.bugpatterns.PrimitiveArrayPassedToVarargsMethod;
 import com.google.errorprone.bugpatterns.PrimitiveAtomicReference;
 import com.google.errorprone.bugpatterns.PrivateConstructorForUtilityClass;
@@ -583,6 +583,7 @@ public class BuiltInCheckerSuppliers {
           InvalidPatternSyntax.class,
           InvalidTimeZoneID.class,
           InvalidZoneId.class,
+          IsInstanceIncompatibleType.class,
           IsInstanceOfClass.class,
           IsLoggableTagLength.class,
           JUnit3TestNotRun.class,
@@ -624,7 +625,6 @@ public class BuiltInCheckerSuppliers {
           PeriodGetTemporalUnit.class,
           PeriodTimeMath.class,
           PreconditionsInvalidPlaceholder.class,
-          PredicateIncompatibleType.class,
           PrivateSecurityContractProtoAccess.class,
           ProtoFieldNullComparison.class,
           ProtoStringFieldReferenceEquality.class,

commit 1be1f94419023da10ba243b2d8bd13ad25ecda6e

Fix a crash introduced by https://github.com/google/error-prone/commit/005372ab0fe90ac2b55565ee2c339f1cd2ec5930
PiperOrigin-RevId: 334406196

core/src/main/java/com/google/errorprone/bugpatterns/IsInstanceIncompatibleType.java
@@ -16,6 +16,7 @@
 
 package com.google.errorprone.bugpatterns;
 
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
 import static com.google.errorprone.matchers.Description.NO_MATCH;
 import static com.google.errorprone.matchers.Matchers.instanceMethod;
@@ -36,6 +37,7 @@ import com.sun.source.tree.MemberReferenceTree;
 import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.code.Type;
+import java.util.Collection;
 
 /**
  * @author cushon@google.com (Liam Miller-Cushon)
@@ -56,7 +58,10 @@ public final class IsInstanceIncompatibleType extends BugChecker
       return NO_MATCH;
     }
 
-    Type receiverType = getType(getReceiver(tree)).getTypeArguments().get(0);
+    Type receiverType = classTypeArgument(tree);
+    if (receiverType == null) {
+      return NO_MATCH;
+    }
     Type argumentType = getType(tree.getArguments().get(0));
 
     return isCastable(argumentType, receiverType, state)
@@ -71,12 +76,20 @@ public final class IsInstanceIncompatibleType extends BugChecker
     }
 
     Type type = state.getTypes().findDescriptorType(getType(tree));
-    Type receiverType = getType(getReceiver(tree)).getTypeArguments().get(0);
+    Type receiverType = classTypeArgument(tree);
+    if (receiverType == null) {
+      return NO_MATCH;
+    }
     Type argumentType = ASTHelpers.getUpperBound(type.getParameterTypes().get(0), state.getTypes());
 
     return isCastable(argumentType, receiverType, state)
         ? NO_MATCH
         : buildMessage(argumentType, receiverType, tree, state);
+  }
+
+  private static Type classTypeArgument(ExpressionTree tree) {
+    Collection<Type> receiverTypeArguments = getType(getReceiver(tree)).getTypeArguments();
+    return !receiverTypeArguments.isEmpty() ? getOnlyElement(receiverTypeArguments) : null;
   }
 
   private Description buildMessage(Type type1, Type type2, Tree tree, VisitorState state) {

core/src/test/java/com/google/errorprone/bugpatterns/IsInstanceIncompatibleTypeTest.java
@@ -151,4 +151,21 @@ public class IsInstanceIncompatibleTypeTest {
             "}")
         .doTest();
   }
+
+  @Test
+  public void rawTypes() {
+    testHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Optional;",
+            "class Test {",
+            "  boolean f(Object o, Class c) {",
+            "    return c.isInstance(o);",
+            "  }",
+            "  <T> Optional<T> f(Optional<T> t, Class c) {",
+            "    return t.filter(c::isInstance);",
+            "  }",
+            "}")
+        .doTest();
+  }
 }