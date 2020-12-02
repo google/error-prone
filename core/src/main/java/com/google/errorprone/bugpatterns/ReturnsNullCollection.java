google/error-prone

commit 51a95444aa1da2ff309e2e237219c047748909fe

Flag methods with collection return types which return null in some cases without annotating method as @Nullable
Flume hits : http://unknown commit

PiperOrigin-RevId: 345094169

core/src/main/java/com/google/errorprone/bugpatterns/ReturnsNullCollection.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright 2020 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
+import static com.google.errorprone.matchers.Matchers.allOf;
+import static com.google.errorprone.matchers.Matchers.anyOf;
+import static com.google.errorprone.matchers.Matchers.isSubtypeOf;
+import static com.google.errorprone.matchers.Matchers.methodReturns;
+import static com.google.errorprone.util.ASTHelpers.getSymbol;
+
+import com.google.errorprone.BugPattern;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.dataflow.nullnesspropagation.TrustingNullnessAnalysis;
+import com.google.errorprone.fixes.Fix;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ReturnTree;
+import java.util.Optional;
+
+/**
+ * Flags methods with collection return types which return {@link null} in some cases but don't
+ * annotate the method as @Nullable.
+ *
+ * @author bhagwani@google.com (Sumit Bhagwani)
+ */
+@BugPattern(
+    name = "ReturnsNullCollection",
+    summary =
+        "Methods has a collection return type and returns {@link null} in some cases but does not"
+            + " annotate the method as @Nullable. See Effective Java 3rd Edition Item 54.",
+    severity = SUGGESTION)
+public class ReturnsNullCollection extends AbstractMethodReturnsNull {
+
+  private static boolean methodWithoutNullable(MethodTree tree, VisitorState state) {
+    return !TrustingNullnessAnalysis.hasNullableAnnotation(getSymbol(tree));
+  }
+
+  private static final Matcher<MethodTree> METHOD_RETURNS_COLLECTION_WITHOUT_NULLABLE_ANNOTATION =
+      allOf(
+          anyOf(
+              methodReturns(isSubtypeOf("java.util.Collection")),
+              methodReturns(isSubtypeOf("java.util.Map"))),
+          ReturnsNullCollection::methodWithoutNullable);
+
+  public ReturnsNullCollection() {
+    super(METHOD_RETURNS_COLLECTION_WITHOUT_NULLABLE_ANNOTATION);
+  }
+
+  @Override
+  protected Optional<Fix> provideFix(ReturnTree tree) {
+    return Optional.empty();
+  }
+}

core/src/main/java/com/google/errorprone/bugpatterns/AbstractAsyncTypeReturnsNull.java
@@ -17,51 +17,32 @@
 package com.google.errorprone.bugpatterns;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.errorprone.matchers.Description.NO_MATCH;
 import static com.google.errorprone.util.ASTHelpers.findSuperMethods;
 import static com.google.errorprone.util.ASTHelpers.getSymbol;
-import static com.sun.source.tree.Tree.Kind.NULL_LITERAL;
 
 import com.google.common.util.concurrent.Futures;
 import com.google.errorprone.VisitorState;
-import com.google.errorprone.bugpatterns.BugChecker.ReturnTreeMatcher;
+import com.google.errorprone.fixes.Fix;
 import com.google.errorprone.fixes.SuggestedFix;
-import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.ReturnTree;
-import com.sun.source.tree.StatementTree;
-import com.sun.source.util.TreePath;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
+import java.util.Optional;
 
 /**
  * Superclass for checks that {@code AsyncCallable} and {@code AsyncFunction} implementations do not
  * directly {@code return null}.
  */
-abstract class AbstractAsyncTypeReturnsNull extends BugChecker implements ReturnTreeMatcher {
-  private final Matcher<MethodTree> implementsAsyncTypeMethod;
+abstract class AbstractAsyncTypeReturnsNull extends AbstractMethodReturnsNull {
 
   AbstractAsyncTypeReturnsNull(Class<?> asyncClass) {
-    this.implementsAsyncTypeMethod = overridesMethodOfClass(asyncClass);
+    super(overridesMethodOfClass(asyncClass));
   }
 
   @Override
-  public final Description matchReturn(ReturnTree tree, VisitorState state) {
-    if (tree.getExpression() == null || tree.getExpression().getKind() != NULL_LITERAL) {
-      return NO_MATCH;
-    }
-    TreePath path = state.getPath();
-    while (path != null && path.getLeaf() instanceof StatementTree) {
-      path = path.getParentPath();
-    }
-    if (path == null || !(path.getLeaf() instanceof MethodTree)) {
-      return NO_MATCH;
-    }
-    if (!implementsAsyncTypeMethod.matches((MethodTree) path.getLeaf(), state)) {
-      return NO_MATCH;
-    }
-    return describeMatch(
-        tree,
+  protected Optional<Fix> provideFix(ReturnTree tree) {
+    return Optional.of(
         SuggestedFix.builder()
             .replace(tree.getExpression(), "immediateFuture(null)")
             .addStaticImport(Futures.class.getName() + ".immediateFuture")

core/src/main/java/com/google/errorprone/bugpatterns/AbstractMethodReturnsNull.java
@@ -1,0 +1,62 @@
+/*
+ * Copyright 2020 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static com.sun.source.tree.Tree.Kind.NULL_LITERAL;
+
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.bugpatterns.BugChecker.ReturnTreeMatcher;
+import com.google.errorprone.fixes.Fix;
+import com.google.errorprone.matchers.Description;
+import com.google.errorprone.matchers.Matcher;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.util.TreePath;
+import java.util.Optional;
+
+/** Superclass for checks that method implementations that directly {@code return null}. */
+abstract class AbstractMethodReturnsNull extends BugChecker implements ReturnTreeMatcher {
+  private final Matcher<MethodTree> methodTreeMatcher;
+
+  AbstractMethodReturnsNull(Matcher<MethodTree> methodTreeMatcher) {
+    this.methodTreeMatcher = methodTreeMatcher;
+  }
+
+  protected abstract Optional<Fix> provideFix(ReturnTree tree);
+
+  @Override
+  public final Description matchReturn(ReturnTree tree, VisitorState state) {
+    if (tree.getExpression() == null || tree.getExpression().getKind() != NULL_LITERAL) {
+      return NO_MATCH;
+    }
+    TreePath path = state.getPath();
+    while (path != null && path.getLeaf() instanceof StatementTree) {
+      path = path.getParentPath();
+    }
+    if (path == null || !(path.getLeaf() instanceof MethodTree)) {
+      return NO_MATCH;
+    }
+    if (!methodTreeMatcher.matches((MethodTree) path.getLeaf(), state)) {
+      return NO_MATCH;
+    }
+    return provideFix(tree)
+        .map(fix -> describeMatch(tree, fix))
+        .orElseGet(() -> describeMatch(tree));
+  }
+}

core/src/main/java/com/google/errorprone/scanner/BuiltInCheckerSuppliers.java
@@ -275,6 +275,7 @@ import com.google.errorprone.bugpatterns.RemoveUnusedImports;
 import com.google.errorprone.bugpatterns.RequiredModifiersChecker;
 import com.google.errorprone.bugpatterns.RestrictedApiChecker;
 import com.google.errorprone.bugpatterns.ReturnValueIgnored;
+import com.google.errorprone.bugpatterns.ReturnsNullCollection;
 import com.google.errorprone.bugpatterns.RxReturnValueIgnored;
 import com.google.errorprone.bugpatterns.SameNameButDifferent;
 import com.google.errorprone.bugpatterns.SelfAssignment;
@@ -970,6 +971,7 @@ public class BuiltInCheckerSuppliers {
           RedundantThrows.class,
           RefersToDaggerCodegen.class,
           RemoveUnusedImports.class,
+          ReturnsNullCollection.class,
           ScopeOnModule.class,
           ScopeOrQualifierAnnotationRetention.class,
           StaticOrDefaultInterfaceMethod.class,

core/src/test/java/com/google/errorprone/bugpatterns/ReturnsNullCollectionTest.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright 2020 The Error Prone Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.errorprone.bugpatterns;
+
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** {@link ReturnsNullCollection}Test */
+@RunWith(JUnit4.class)
+public class ReturnsNullCollectionTest {
+
+  private final CompilationTestHelper compilationHelper =
+      CompilationTestHelper.newInstance(ReturnsNullCollection.class, getClass());
+
+  @Test
+  public void positive() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Collection;",
+            "import java.util.ArrayList;",
+            "import java.util.List;",
+            "import java.util.Map;",
+            "class Test {",
+            "  Collection<String> methodReturnsNullCollection() {",
+            "  // BUG: Diagnostic contains: ReturnsNullCollection",
+            "    return null;",
+            "  }",
+            "  List<String> methodReturnsNullList() {",
+            "  // BUG: Diagnostic contains: ReturnsNullCollection",
+            "    return null;",
+            "  }",
+            "  Map<String, String> methodReturnsNullMap() {",
+            "  // BUG: Diagnostic contains: ReturnsNullCollection",
+            "    return null;",
+            "  }",
+            "  List<String> methodReturnsNullListConditionally(boolean foo) {",
+            "    if (foo) {",
+            "      // BUG: Diagnostic contains: ReturnsNullCollection",
+            "      return null;",
+            "    }",
+            "    return new ArrayList();",
+            "  }",
+            "}")
+        .doTest();
+  }
+
+  @Test
+  public void negative() {
+    compilationHelper
+        .addSourceLines(
+            "Test.java",
+            "import java.util.Collection;",
+            "import java.util.HashMap;",
+            "import java.util.List;",
+            "import java.util.Map;",
+            "import javax.annotation.Nullable;",
+            "class Test {",
+            "  @Nullable",
+            "  Collection<String> methodReturnsNullCollection() {",
+            "    return null;",
+            "  }",
+            "  @Nullable",
+            "  List<String> methodReturnsNullList() {",
+            "    return null;",
+            "  }",
+            "  @Nullable",
+            "  Map<String, String> methodReturnsNullMap() {",
+            "    return null;",
+            "  }",
+            "  @Nullable",
+            "  HashMap<String, String> methodReturnsNullHashMap() {",
+            "    return null;",
+            "  }",
+            "}")
+        .doTest();
+  }
+}

commit 4f635d49e8ea4d3ed5cac6310cc2fa41570602fc

Fix javadoc issue introduced by https://github.com/google/error-prone/commit/51a95444aa1da2ff309e2e237219c047748909fe
PiperOrigin-RevId: 345147779

core/src/main/java/com/google/errorprone/bugpatterns/ReturnsNullCollection.java
@@ -33,7 +33,7 @@ import com.sun.source.tree.ReturnTree;
 import java.util.Optional;
 
 /**
- * Flags methods with collection return types which return {@link null} in some cases but don't
+ * Flags methods with collection return types which return {@code null} in some cases but don't
  * annotate the method as @Nullable.
  *
  * @author bhagwani@google.com (Sumit Bhagwani)
@@ -41,7 +41,7 @@ import java.util.Optional;
 @BugPattern(
     name = "ReturnsNullCollection",
     summary =
-        "Methods has a collection return type and returns {@link null} in some cases but does not"
+        "Methods has a collection return type and returns {@code null} in some cases but does not"
             + " annotate the method as @Nullable. See Effective Java 3rd Edition Item 54.",
     severity = SUGGESTION)
 public class ReturnsNullCollection extends AbstractMethodReturnsNull {