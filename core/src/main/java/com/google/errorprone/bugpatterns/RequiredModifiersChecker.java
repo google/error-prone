59b459f4847bbbbd750905160c36301c4f0a6ec3
Liam Miller-Cushon <cushon@google.com>
2021-05-24 15:44:12 -0700

    Update `IncompatibleModifiersChecker` and `RequiredModifiersChecker`
    in preparation for introducing a new `Modifier` enum.
    
    https://github.com/google/error-prone/issues/2122
    
    PiperOrigin-RevId: 375574262

--- core/src/main/java/com/google/errorprone/bugpatterns/RequiredModifiersChecker.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/RequiredModifiersChecker.java
@@ -18,12 +18,12 @@ package com.google.errorprone.bugpatterns;
 
 import static com.google.errorprone.BugPattern.LinkType.NONE;
 import static com.google.errorprone.BugPattern.SeverityLevel.ERROR;
+import static com.google.errorprone.matchers.Description.NO_MATCH;
+import static com.google.errorprone.util.MoreAnnotations.getValue;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
-import com.google.errorprone.annotations.RequiredModifiers;
 import com.google.errorprone.bugpatterns.BugChecker.AnnotationTreeMatcher;
 import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.Description;
@@ -31,8 +31,16 @@ import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ModifiersTree;
 import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Attribute;
+import com.sun.tools.javac.code.Symbol;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Modifier;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.SimpleAnnotationValueVisitor8;
 
 /** @author sgoldfeder@google.com (Steven Goldfeder) */
 @BugPattern(
@@ -46,28 +54,40 @@ public class RequiredModifiersChecker extends BugChecker implements AnnotationTr
 
   private static final String MESSAGE_TEMPLATE =
       "%s has specified that it must be used together with the following modifiers: %s";
+  private static final String REQUIRED_MODIFIERS =
+      "com.google.errorprone.annotations.RequiredModifiers";
 
   @Override
   public Description matchAnnotation(AnnotationTree tree, VisitorState state) {
-    RequiredModifiers annotation = ASTHelpers.getAnnotation(tree, RequiredModifiers.class);
-    if (annotation == null) {
-      return Description.NO_MATCH;
+    Symbol sym = ASTHelpers.getSymbol(tree);
+    if (sym == null) {
+      return NO_MATCH;
     }
-    Set<Modifier> requiredModifiers = ImmutableSet.copyOf(annotation.value());
+    Attribute.Compound annotation =
+        sym.getRawAttributes().stream()
+            .filter(a -> a.type.tsym.getQualifiedName().contentEquals(REQUIRED_MODIFIERS))
+            .findAny()
+            .orElse(null);
+    if (annotation == null) {
+      return NO_MATCH;
+    }
+    Set<Modifier> requiredModifiers = new LinkedHashSet<>();
+    getValue(annotation, "value").ifPresent(a -> getModifiers(requiredModifiers, a));
+    getValue(annotation, "modifier").ifPresent(a -> getModifiers(requiredModifiers, a));
     if (requiredModifiers.isEmpty()) {
-      return Description.NO_MATCH;
+      return NO_MATCH;
     }
 
     Tree parent = state.getPath().getParentPath().getLeaf();
     if (!(parent instanceof ModifiersTree)) {
       // e.g. An annotated package name
-      return Description.NO_MATCH;
+      return NO_MATCH;
     }
 
     Set<Modifier> missing = Sets.difference(requiredModifiers, ((ModifiersTree) parent).getFlags());
 
     if (missing.isEmpty()) {
-      return Description.NO_MATCH;
+      return NO_MATCH;
     }
 
     String annotationName = ASTHelpers.getAnnotationName(tree);
@@ -85,5 +105,22 @@ public class RequiredModifiersChecker extends BugChecker implements AnnotationTr
                 missing))
         .setMessage(customMessage)
         .build();
+  }
+
+  private static void getModifiers(Collection<Modifier> modifiers, Attribute attribute) {
+    class Visitor extends SimpleAnnotationValueVisitor8<Void, Void> {
+      @Override
+      public Void visitEnumConstant(VariableElement c, Void unused) {
+        modifiers.add(Modifier.valueOf(c.getSimpleName().toString()));
+        return null;
+      }
+
+      @Override
+      public Void visitArray(List<? extends AnnotationValue> vals, Void unused) {
+        vals.forEach(val -> val.accept(this, null));
+        return null;
+      }
+    }
+    attribute.accept(new Visitor(), null);
   }
 }
