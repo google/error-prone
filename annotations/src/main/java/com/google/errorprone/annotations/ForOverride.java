9236509091d028ebb96e583966068a3de6370cdb
Liam Miller-Cushon <cushon@google.com>
2021-06-02 17:44:51 -0700

    Migrate uses of `@IncompatibleModifiers` and `@RequiredModifiers`
    to use `com.google.errorprone.annotations.Modifier`
    instead of `javax.lang.model.element.Modifier`.
    
    #2122
    
    Follow-up to https://github.com/google/error-prone/commit/26f1f5498321c9e2c25f5546dc9aa026affde16a
    
    PiperOrigin-RevId: 377184897

--- annotations/src/main/java/com/google/errorprone/annotations/ForOverride.java
+++ annotations/src/main/java/com/google/errorprone/annotations/ForOverride.java
@@ -18,10 +18,6 @@ package com.google.errorprone.annotations;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.CLASS;
-import static javax.lang.model.element.Modifier.FINAL;
-import static javax.lang.model.element.Modifier.PRIVATE;
-import static javax.lang.model.element.Modifier.PUBLIC;
-import static javax.lang.model.element.Modifier.STATIC;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -39,7 +35,8 @@ import java.lang.annotation.Target;
  * protected or package-private visibility, although their effective visibility is actually "none".
  */
 @Documented
-@IncompatibleModifiers({PUBLIC, PRIVATE, STATIC, FINAL})
+@IncompatibleModifiers(
+    modifier = {Modifier.PUBLIC, Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL})
 @Retention(CLASS) // Parent source might not be available while compiling subclass
 @Target(METHOD)
 public @interface ForOverride {}
