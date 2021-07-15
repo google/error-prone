02916d1da093f0af405e8ee9331e3d67fe58dfbf
ghm <ghm@google.com>
2021-07-14 17:01:32 -0700

    Automated rollback of commit e08936be2785e39f640f01a69b921b6598ee6ca8.
    *** Reason for rollback ***
    
    This seems to have dramatically _increased_ the false positives.
    
    Absolutely no idea why before writing a repro, but seems sensible to roll this back in the mean time.
    
    []
    
    *** Original change description ***
    
    Fix a false positive when comparing protos.
    
    ***
    
    PiperOrigin-RevId: 384816650

--- core/src/main/java/com/google/errorprone/bugpatterns/TypeCompatibilityUtils.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/TypeCompatibilityUtils.java
@@ -82,8 +82,7 @@ public final class TypeCompatibilityUtils {
     }
 
     // If they're the exact same type, they are definitely compatible.
-    Types types = state.getTypes();
-    if (types.isSameType(upperBound(leftType), upperBound(rightType))) {
+    if (state.getTypes().isSameType(upperBound(leftType), upperBound(rightType))) {
       return TypeCompatibilityReport.compatible();
     }
 
@@ -104,7 +103,7 @@ public final class TypeCompatibilityUtils {
     // class Bar extends Super<String>
     // class Foo extends Super<Integer>
     // Bar and Foo would least-upper-bound to Super, and we compare String and Integer to each-other
-    Type commonSupertype = types.lub(types.erasure(rightType), types.erasure(leftType));
+    Type commonSupertype = state.getTypes().lub(rightType, leftType);
     // primitives, etc. can't have a common superclass.
     if (commonSupertype.getTag().equals(TypeTag.BOT)
         || commonSupertype.getTag().equals(TypeTag.ERROR)) {
