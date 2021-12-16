72ac2aa27c43e3da954c4e9d731facbecb5fc8cc
ghm <ghm@google.com>
2021-12-16 14:45:34 -0800

    Automated rollback of commit 7f4b2204432a34373c2dcee524060c3ec4858ce7.
    *** Reason for rollback ***
    
    Unbork the next Javabuilder release
    
    *** Original change description ***
    
    Make UnicodeDirectionalityCharacters non-disableable.
    
    ***
    
    PiperOrigin-RevId: 416899966

--- core/src/main/java/com/google/errorprone/bugpatterns/UnicodeDirectionalityCharacters.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/UnicodeDirectionalityCharacters.java
@@ -31,8 +31,10 @@ import com.sun.source.tree.CompilationUnitTree;
 @BugPattern(
     name = "UnicodeDirectionalityCharacters",
     severity = ERROR,
-    summary = "Unicode directionality modifiers can be used to conceal code in many editors.",
-    disableable = false)
+    summary = "Unicode directionality modifiers can be used to conceal code in many editors."
+    // This has to be non-disableable, otherwise the suppression could be hidden.
+    // TODO(b/205609703): Make this non-disableable after the depot is clean.
+    )
 public final class UnicodeDirectionalityCharacters extends BugChecker
     implements CompilationUnitTreeMatcher {
   @Override
