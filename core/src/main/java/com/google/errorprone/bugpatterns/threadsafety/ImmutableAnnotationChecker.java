437fc067bf08fde1694a5b7970f280441fb19bf7
ghm <ghm@google.com>
2022-01-24 03:19:02 -0800

    Remove unused constructor for ImmutableAnnotationChecker.
    It's not used anywhere (and the ErrorProneFlags one is used reflectively).
    
    PiperOrigin-RevId: 423770390

--- core/src/main/java/com/google/errorprone/bugpatterns/threadsafety/ImmutableAnnotationChecker.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/threadsafety/ImmutableAnnotationChecker.java
@@ -59,11 +59,6 @@ public class ImmutableAnnotationChecker extends BugChecker implements ClassTreeM
 
   private final WellKnownMutability wellKnownMutability;
 
-  @Deprecated // Used reflectively, but you should pass in ErrorProneFlags to get custom mutability
-  public ImmutableAnnotationChecker() {
-    this(ErrorProneFlags.empty());
-  }
-
   public ImmutableAnnotationChecker(ErrorProneFlags flags) {
     this.wellKnownMutability = WellKnownMutability.fromFlags(flags);
   }
