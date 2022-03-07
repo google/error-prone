8202ebeea15213f233774595f6650e44d0fdd39e
charmed-quark <martenssoncs@gmail.com>
2022-03-07 10:55:22 +0100

    (google#184) test: Add additional positive and negative cases
    * Positive: add a case where a class passes "this" to a method belonging to another class.
    * Negative: add a case where a class has a nonempty constructor which never uses "this".

--- core/src/test/java/com/google/errorprone/bugpatterns/testdata/ThisEscapesConstructorNegativeCases.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/testdata/ThisEscapesConstructorNegativeCases.java
@@ -1,6 +1,13 @@
 package com.google.errorprone.bugpatterns.testdata;
 
 public class ThisEscapesConstructorNegativeCases {
+
+    /**
+     * Legal usage of "this" in the constructor is on the left hand side of
+     * an assignment expression, often to assign fields.
+     * "this" can also be used in a non-constructor method, since the object
+     * will have been initialized by then.
+     */
 	static class ThisDotAssign {
         public ThisDotAssign at;
         ThisDotAssign() {
@@ -12,7 +19,21 @@ public class ThisEscapesConstructorNegativeCases {
         }
     }
 	
+    /**
+     * Classes whose constructors do not use "this" at all should throw no
+     * errors.
+     */
 	static class Receiver {
 		public Receiver(ThisDotAssign var) {}
 	}
+
+    static class ConstructorDoesNotUseThis {
+        public String s;
+        public int i;
+
+        ConstructorDoesNotUseThis() {
+            s = "000";
+            i = 100;
+        }
+    }
 }
--- core/src/test/java/com/google/errorprone/bugpatterns/testdata/ThisEscapesConstructorPositiveCases.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/testdata/ThisEscapesConstructorPositiveCases.java
@@ -2,6 +2,10 @@ package com.google.errorprone.bugpatterns.testdata;
 
 public class ThisEscapesConstructorPositiveCases {
 
+    /**
+     * "this" should never appear as the right hand side of an assignment in a
+     * constructor.
+     */
     static class AssignsThis {
         public AssignsThis at;
         AssignsThis() {
@@ -10,12 +14,30 @@ public class ThisEscapesConstructorPositiveCases {
         }
     }
 
-    static class PassesThis {
-        PassesThis() {
+    /**
+     * "this" should never be passed as an argument to a method in a constructor.
+     */
+    static class PassesThisToSelf {
+        PassesThisToSelf() {
             // BUG: Diagnostic contains:
             receivesThis(this);
         }
 
-        private void receivesThis(PassesThis t) {}
+        private void receivesThis(PassesThisToSelf t) {}
+    }
+
+    static class PassesThisToOther {
+        public int x;
+        PassesThisToOther() {
+            x = 6;
+            // BUG: Diagnostic contains:
+            Other.someMethod(this);
+        }
+    }
+
+    static class Other {
+        public static void someMethod(PassesThisToOther p) {
+            p.x++;
+        }
     }
 }
