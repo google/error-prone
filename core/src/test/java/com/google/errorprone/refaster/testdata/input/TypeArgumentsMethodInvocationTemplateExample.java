da1413ab1af27356a7fbffcdcdd02d3fc8e8a16b
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2023-01-05 22:27:47 +0100

    Update the year to 2023 in license header

--- core/src/test/java/com/google/errorprone/refaster/testdata/input/TypeArgumentsMethodInvocationTemplateExample.java
+++ core/src/test/java/com/google/errorprone/refaster/testdata/input/TypeArgumentsMethodInvocationTemplateExample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Error Prone Authors.
+ * Copyright 2023 The Error Prone Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
