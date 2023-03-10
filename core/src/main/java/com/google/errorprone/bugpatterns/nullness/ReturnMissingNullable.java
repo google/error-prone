216595493d6b0f7430ff8ae79f5648ecd8efecca
Error Prone Team <java-team-github-bot@google.com>
2023-03-10 14:32:57 -0800

    Specify which `@Nullable` to use.
    PiperOrigin-RevId: 515740479

--- core/src/main/java/com/google/errorprone/bugpatterns/nullness/ReturnMissingNullable.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/nullness/ReturnMissingNullable.java
@@ -84,7 +84,11 @@ import javax.lang.model.element.Name;
             + " possibly-null value. This can happen for example with Kotlin callers, or with Java"
             + " callers that are checked for null-safety by static-analysis tools. Alternatively,"
             + " depending on the tool, it may be possible to annotate Java callers temporarily with"
-            + " @SuppressWarnings(\"nullness\").",
+            + " @SuppressWarnings(\"nullness\").\n\n"
+            + "We recommend using JSpecify @Nullable for all new build units,"
+            + " org.jspecify.nullness.Nullable (//third_party/java/jspecify_annotations), but see"
+            + " https://engdoc.corp.google.com/eng/doc/devguide/java/practices/null.md#annotations"
+            + " for more information.\n\n",
     severity = SUGGESTION)
 public class ReturnMissingNullable extends BugChecker implements CompilationUnitTreeMatcher {
   private static final Matcher<StatementTree> METHODS_THAT_NEVER_RETURN =
