3816046982d37fdad45684adc30b0d9a259edfe3
Liam Miller-Cushon <cushon@google.com>
2023-03-20 08:23:15 -0700

    Prepare for an upcoming change to TypeMetadata
    https://github.com/openjdk/jdk/commit/b9951dd63997b6330001311c925e171f4645a28b
    
    PiperOrigin-RevId: 517974362

--- core/src/main/java/com/google/errorprone/bugpatterns/checkreturnvalue/ApiFactory.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/checkreturnvalue/ApiFactory.java
@@ -23,6 +23,7 @@ import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.code.Type.StructuralTypeMapping;
 import com.sun.tools.javac.code.TypeMetadata;
 import com.sun.tools.javac.code.Types;
+import java.lang.reflect.Method;
 
 /** Utility method to produce {@link Api} objects from javac {@link MethodSymbol}. */
 public final class ApiFactory {
@@ -58,15 +59,30 @@ public final class ApiFactory {
 
         @Override
         public Type visitClassType(Type.ClassType t, Void unused) {
-          return super.visitClassType(t.cloneWithMetadata(TypeMetadata.EMPTY), unused);
+          return super.visitClassType((Type.ClassType) cloneWithoutMetadata(t), unused);
         }
 
         // Remove annotations from all enclosing containers
         @Override
         public Type visitArrayType(Type.ArrayType t, Void unused) {
-          return super.visitArrayType(t.cloneWithMetadata(TypeMetadata.EMPTY), unused);
+          return super.visitArrayType((Type.ArrayType) cloneWithoutMetadata(t), unused);
         }
       };
+
+  public static Type cloneWithoutMetadata(Type type) {
+    try {
+      try {
+        Method method = Type.class.getMethod("cloneWithMetadata", TypeMetadata.class);
+        return (Type) method.invoke(type, TypeMetadata.class.getField("EMPTY").get(null));
+      } catch (NoSuchMethodException e) {
+        Class<?> annotations = Class.forName("com.sun.tools.javac.code.TypeMetadata$Annotations");
+        Method method = Type.class.getMethod("dropMetadata", Class.class);
+        return (Type) method.invoke(type, annotations);
+      }
+    } catch (ReflectiveOperationException e) {
+      throw new LinkageError(e.getMessage(), e);
+    }
+  }
 
   private ApiFactory() {}
 }
