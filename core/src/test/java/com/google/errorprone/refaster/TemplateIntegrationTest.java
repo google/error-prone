5c391b49009a1315bfbfedaf0faacd910ed07d82
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2023-09-14 14:36:07 -0700

    Prevent Refaster `UMemberSelect` from matching method parameters
    Fixes #2456
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/error-prone/pull/2456 from PicnicSupermarket:bugfix/member-select-method-parameter-disambiguation 160ec59ca613380af0050422af6b9f3a9f0f81c9
    PiperOrigin-RevId: 565480268

--- core/src/test/java/com/google/errorprone/refaster/TemplateIntegrationTest.java
+++ core/src/test/java/com/google/errorprone/refaster/TemplateIntegrationTest.java
@@ -374,4 +374,9 @@ public class TemplateIntegrationTest extends CompilerBasedTest {
   public void typeArgumentsMethodInvocation() throws IOException {
     runTest("TypeArgumentsMethodInvocationTemplate");
   }
+
+  @Test
+  public void memberSelectAndMethodParameterDisambiguation() throws IOException {
+    runTest("MemberSelectAndMethodParameterDisambiguationTemplate");
+  }
 }
