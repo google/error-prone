b052f3034e4b4b3a8aacb287549d02c22bf985c1
Andrew Kreimer <algonell@gmail.com>
2024-09-11 22:34:29 +0300

    Fix a typo

--- core/src/main/java/com/google/errorprone/bugpatterns/formatstring/FormatStringValidation.java
+++ core/src/main/java/com/google/errorprone/bugpatterns/formatstring/FormatStringValidation.java
@@ -145,7 +145,7 @@ public final class FormatStringValidation {
 
   /**
    * Return an instance of the given type if it receives special handling by {@code String.format}.
-   * For example, an intance of {@link Integer} will be returned for an input of type {@code int} or
+   * For example, an instance of {@link Integer} will be returned for an input of type {@code int} or
    * {@link Integer}.
    */
   private static @Nullable Object getInstance(Tree tree, VisitorState state) {
