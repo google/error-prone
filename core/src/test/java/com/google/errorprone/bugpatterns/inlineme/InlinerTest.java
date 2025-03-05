785b4b8549ad176460f28e0e2eec2c9770f72bed
ghm <ghm@google.com>
2025-03-05 07:42:30 -0800

    Add another broken test where the substituted code requires parens.
    This feels like a nice complementary test to the other precedence-backed ones.
    
    PiperOrigin-RevId: 733730048

--- core/src/test/java/com/google/errorprone/bugpatterns/inlineme/InlinerTest.java
+++ core/src/test/java/com/google/errorprone/bugpatterns/inlineme/InlinerTest.java
@@ -1193,6 +1193,88 @@ public final class Caller {
   }
 
   @Test
+  public void orderOfOperationsWhenInliningAddition() {
+    refactoringTestHelper
+        .addInputLines(
+            "Client.java",
+            """
+            import com.google.errorprone.annotations.InlineMe;
+
+            public final class Client {
+              @Deprecated
+              @InlineMe(replacement = "x + y")
+              public int add(int x, int y) {
+                return x + y;
+              }
+            }
+            """)
+        .expectUnchanged()
+        .addInputLines(
+            "Caller.java",
+            """
+            public final class Caller {
+              public void doTest() {
+                Client client = new Client();
+                int x = client.add(1, 2) * 3;
+                int y = client.add(1, 2) + 3;
+              }
+            }
+            """)
+        .addOutputLines(
+            "out/Caller.java",
+            """
+            public final class Caller {
+              public void doTest() {
+                Client client = new Client();
+                int x = 1 + 2 * 3;
+                int y = 1 + 2 + 3;
+              }
+            }
+            """)
+        .doTest();
+  }
+
+  @Test
+  public void orderOfOperationsWhenInliningCasts() {
+    refactoringTestHelper
+        .addInputLines(
+            "Client.java",
+            """
+            import com.google.errorprone.annotations.InlineMe;
+
+            public final class Client {
+              @Deprecated
+              @InlineMe(replacement = "(int) x")
+              public int cast(long x) {
+                return (int) x;
+              }
+            }
+            """)
+        .expectUnchanged()
+        .addInputLines(
+            "Caller.java",
+            """
+            public final class Caller {
+              public void doTest() {
+                Client client = new Client();
+                int x = client.cast(1) * 10;
+              }
+            }
+            """)
+        .addOutputLines(
+            "out/Caller.java",
+            """
+            public final class Caller {
+              public void doTest() {
+                Client client = new Client();
+                int x = (int) 1 * 10;
+              }
+            }
+            """)
+        .doTest();
+  }
+
+  @Test
   public void orderOfOperationsWithTrailingOperand() {
     refactoringTestHelper
         .addInputLines(
