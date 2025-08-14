e66fc4b2c9cb3523b01ec5c9350dbae23c5a2706
Éamonn McManus <emcmanus@google.com>
2025-08-14 14:29:47 -0700

    Change the return type of `ASTHelpers.asFlagSet`.
    RELNOTES=The return type of `ASTHelpers.asFlagSet` has changed. The previous type was `EnumSet<Flags.Flag>`, where `Flags.Flag` is an enum in the javac class `Flags`. A recent JDK has replaced that enum with a new top-level enum called `FlagsEnum`. It is not possible to change `ASTHelpers.asFlagSet` in a way that would be compatible with the enums from JDKs both before and after the change. Instead, the method now returns `ImmutableSet<String>`, where the strings come from the `toString()` of the enum constants. That means they are `"native"`, `"abstract"`, etc.
    PiperOrigin-RevId: 795200642

--- check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
+++ check_api/src/main/java/com/google/errorprone/util/ASTHelpers.java
@@ -2007,9 +2007,16 @@ public class ASTHelpers {
     return method.getModifiers().contains(Modifier.ABSTRACT);
   }
 
-  public static EnumSet<Flags.Flag> asFlagSet(long flags) {
+  public static ImmutableSet<String> asFlagSet(long flags) {
     flags &= ~(Flags.ANONCONSTR_BASED | POTENTIALLY_AMBIGUOUS);
-    return Flags.asFlagSet(flags);
+    // The cast to EnumSet<?> is because Flags.asFlagSet() returns an EnumSet<Flags.Flag> in some
+    // versions of JDK, and an EnumSet<FlagsEnum> in others. Without the cast, whichever enum type
+    // it is gets referenced by the stream, so the code won't work if compiled with a JDK that has
+    // the change and executed with one that doesn't, or vice versa. Erasure means that the call to
+    // Flags.asFlagSet doesn't itself cause problems because EnumSet<Flags.Flag> gets erased to just
+    // EnumSet in the bytecode.
+    return ((EnumSet<?>) Flags.asFlagSet(flags))
+        .stream().map(Object::toString).collect(toImmutableSet());
   }
 
   // Removed in JDK 21 by JDK-8026369
