cdec91afb1bb464fd51a7095d365e1430b356fcf
ghm <ghm@google.com>
2025-10-20 14:55:30 -0700

    Remove reflection for accessing `DocCommentTable.getCommentTree`.
    PiperOrigin-RevId: 821818199

--- core/src/test/java/com/google/errorprone/fixes/SuggestedFixesTest.java
+++ core/src/test/java/com/google/errorprone/fixes/SuggestedFixesTest.java
@@ -65,7 +65,6 @@ import com.sun.source.util.DocTreePathScanner;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.tree.DCTree;
 import com.sun.tools.javac.tree.DCTree.DCDocComment;
-import com.sun.tools.javac.tree.DocCommentTable;
 import com.sun.tools.javac.tree.JCTree;
 import java.io.IOException;
 import java.lang.annotation.Retention;
@@ -1130,9 +1129,9 @@ public class SuggestedFixesTest {
     @Override
     public Description matchClass(ClassTree tree, VisitorState state) {
       DCTree.DCDocComment comment =
-          getCommentTree(
-              ((JCTree.JCCompilationUnit) state.getPath().getCompilationUnit()).docComments,
-              (JCTree) tree);
+          (DCDocComment)
+              ((JCTree.JCCompilationUnit) state.getPath().getCompilationUnit())
+                  .docComments.getCommentTree((JCTree) tree);
       if (comment == null) {
         return Description.NO_MATCH;
       }
@@ -1149,17 +1148,6 @@ public class SuggestedFixesTest {
         return Description.NO_MATCH;
       }
       return describeMatch(tree, fix.build());
-    }
-  }
-
-  private static DCDocComment getCommentTree(DocCommentTable docCommentTable, JCTree tree) {
-    try {
-      return (DCDocComment)
-          DocCommentTable.class
-              .getMethod("getCommentTree", JCTree.class)
-              .invoke(docCommentTable, tree);
-    } catch (ReflectiveOperationException e) {
-      throw new LinkageError(e.getMessage(), e);
     }
   }
 
