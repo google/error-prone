91fca91dc962e1fa4ee726c07baa3f53a1144a95
ghm <ghm@google.com>
2025-12-02 06:23:02 -0800

    Remove `JUnit4TestNotRun:RemoveAssertionRequirement` flag.
    PiperOrigin-RevId: 839238956

--- check_api/src/main/java/com/google/errorprone/matchers/JUnitMatchers.java
+++ check_api/src/main/java/com/google/errorprone/matchers/JUnitMatchers.java
@@ -16,7 +16,6 @@
 
 package com.google.errorprone.matchers;
 
-import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.collect.Iterables.getOnlyElement;
 import static com.google.errorprone.matchers.ChildMultiMatcher.MatchType.AT_LEAST_ONE;
 import static com.google.errorprone.matchers.Matchers.allOf;
@@ -48,10 +47,7 @@ import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.Tree;
-import com.sun.source.util.TreeScanner;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Symbol.MethodSymbol;
 import com.sun.tools.javac.code.Type;
@@ -280,31 +276,6 @@ public final class JUnitMatchers {
    */
   public static final Matcher<ClassTree> isAmbiguousJUnitVersion =
       allOf(isTestCaseDescendant, anyOf(hasJUnit4TestRunner, hasJUnit4TestCases));
-
-  /** Returns true if the tree contains a method invocation that looks like a test assertion. */
-  public static boolean containsTestMethod(Tree tree) {
-    return firstNonNull(
-        tree.accept(
-            new TreeScanner<Boolean, Void>() {
-              @Override
-              public Boolean visitMethodInvocation(MethodInvocationTree node, Void unused) {
-                String name = getSymbol(node).getSimpleName().toString();
-                return name.contains("assert")
-                    || name.contains("verify")
-                    || name.contains("check")
-                    || name.contains("fail")
-                    || name.contains("expect")
-                    || firstNonNull(super.visitMethodInvocation(node, null), false);
-              }
-
-              @Override
-              public Boolean reduce(Boolean a, Boolean b) {
-                return firstNonNull(a, false) || firstNonNull(b, false);
-              }
-            },
-            null),
-        false);
-  }
 
   private JUnitMatchers() {}
 }
