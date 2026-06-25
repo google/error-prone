google/error-prone

commit 8f1c9c73d5704885520b4da8438bdcc6489b3173

Migrate Error Prone to SafeRE
PiperOrigin-RevId: 935576289

check_api/src/main/java/com/google/errorprone/matchers/Matchers.java
@@ -99,13 +99,13 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.NestingKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
+import org.safere.Pattern;
 
 /**
  * Static factory methods which make the DSL read more fluently. Since matchers are run in a tight
@@ -598,21 +598,37 @@ public final class Matchers {
     };
   }
 
+  static Matcher<ExpressionTree> stringLiteral(Predicate<String> matcher) {
+    return (tree, state) ->
+        tree instanceof LiteralTree literalTree
+            && literalTree.getValue() instanceof String string
+            && matcher.test(string);
+  }
+
   /**
    * Matches a Literal AST node if it is a string literal with the given value. For example, {@code
    * stringLiteral("thing")} matches the literal {@code "thing"}
    */
   public static Matcher<ExpressionTree> stringLiteral(String value) {
-    return new StringLiteral(value);
+    return stringLiteral(value::equals);
   }
 
   /**
-   * Matches a Literal AST node if it is a string literal which matches the given {@link Pattern}.
+   * Matches a Literal AST node if it is a string literal which matches the given {@code Pattern}.
    *
    * @see #stringLiteral(String)
    */
   public static Matcher<ExpressionTree> stringLiteral(Pattern pattern) {
-    return new StringLiteral(pattern);
+    return stringLiteral(pattern.asMatchPredicate());
+  }
+
+  /**
+   * Matches a Literal AST node if it is a string literal which matches the given {@code Pattern}.
+   *
+   * @see #stringLiteral(String)
+   */
+  public static Matcher<ExpressionTree> stringLiteral(java.util.regex.Pattern pattern) {
+    return stringLiteral(pattern.asMatchPredicate());
   }
 
   public static Matcher<ExpressionTree> booleanLiteral(boolean value) {

core/src/main/java/com/google/errorprone/bugpatterns/argumentselectiondefects/Matchers.java
@@ -39,7 +39,7 @@ import com.sun.tools.javac.code.Symbol.MethodSymbol;
 import com.sun.tools.javac.code.Symbol.VarSymbol;
 import com.sun.tools.javac.code.Type;
 import java.util.List;
-import java.util.regex.Pattern;
+import org.safere.Pattern;
 
 /**
  * Matchers for the various checkers in this package. They are factored out into this class so that

commit 3ef67994c280c9e6051d3552578e597ffda60b88

Restore partial match behavior for `stringLiteral(Pattern)`
The migration to SafeRE in https://github.com/google/error-prone/commit/8f1c9c73d5704885520b4da8438bdcc6489b3173 accidentally changed behaviour, to use `pattern.asMatchPredicate()` (full-string matching) instead of `pattern.asPredicate()` (performs partial substring matching). This restores the previous behavior.

PiperOrigin-RevId: 938168187

check_api/src/main/java/com/google/errorprone/matchers/Matchers.java
@@ -619,7 +619,7 @@ public final class Matchers {
    * @see #stringLiteral(String)
    */
   public static Matcher<ExpressionTree> stringLiteral(Pattern pattern) {
-    return stringLiteral(pattern.asMatchPredicate());
+    return stringLiteral(pattern.asPredicate());
   }
 
   /**
@@ -628,7 +628,7 @@ public final class Matchers {
    * @see #stringLiteral(String)
    */
   public static Matcher<ExpressionTree> stringLiteral(java.util.regex.Pattern pattern) {
-    return stringLiteral(pattern.asMatchPredicate());
+    return stringLiteral(pattern.asPredicate());
   }
 
   public static Matcher<ExpressionTree> booleanLiteral(boolean value) {